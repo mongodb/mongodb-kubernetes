5c8c048406783b363a82756258a093794698e3f0
Ben Elgar <me@benelgar.com>
2018-11-14 15:40:03 +0000

    CLOUDP-34616: Add support for mongos to probe.sh (#132)
    * Add support for mongos to probe.sh
    
    * Use pgrep to check mongod liveliness

--- docker/mongodb-enterprise-database/content/probe.sh
+++ docker/mongodb-enterprise-database/content/probe.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-
 agent_pid=/mongodb-automation/mongodb-mms-automation-agent.pid
-mongod_pid=/data/mongod.lock
 
 check_agent_pid () {
     # the agent PID must exists always
@@ -22,22 +20,23 @@ baby_container () {
     [ $pid1_alive_mins -lt 60 ]
 }
 
-check_mongod_pid () {
-    # The mongod pid might not always exists, specially when the container
-    # has been recently created and the automation agent is still
-    # downloading.
-    [ -f $mongod_pid ]
+check_mongod_alive () {
+    pgrep --exact 'mongod'
 }
 
-check_mongod_alive () {
-    # the mongod pid might not always exist
+check_mongos_alive () {
+    pgrep --exact 'mongos'
+}
+
+check_mongo_process_alive () {
+    # the mongod process pid might not always exist
     # 1. when the container is being created the mongod package needs to be
     #    downloaded. the agent will wait for 1 hour before giving up.
     # 2. the mongod process might be getting updated, we'll set a
     #    failureThreshold on the livenessProbe to a few minutes before we
     #    give up.
 
-    baby_container || check_mongod_pid
+    baby_container || check_mongod_alive || check_mongos_alive
 }
 
-check_agent_pid && check_mongod_alive
+check_agent_pid && check_mongo_process_alive
