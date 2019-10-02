080df914df9677c7dbcbcbabb1308df0d0191e02
Rodrigo Valin <rodrigo.valin@mongodb.com>
2019-10-02 12:06:30 +0100

    CLOUDP-50268: Removes old builder pods (#370)

---
+++ docker/cluster-cleaner/scripts/delete-old-builder-pods.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+NAMESPACE=construction-site
+
+if [ -z ${DELETE_OLDER_THAN_AMOUNT+x} ] || [ -z ${DELETE_OLDER_THAN_UNIT+x} ]; then
+    echo "Need to set both 'DELETE_OLDER_THAN_AMOUNT' and 'DELETE_OLDER_THAN_UNIT' environment variables."
+    exit 1
+fi
+
+for pod in $(kubectl -n $NAMESPACE get pods -o name); do
+    creation_time=$(kubectl -n $NAMESPACE get "${pod}" -o jsonpath='{.metadata.creationTimestamp}')
+
+    if ! ./is_older_than.py "${creation_time}" "${DELETE_OLDER_THAN_AMOUNT}" "${DELETE_OLDER_THAN_UNIT}"; then
+        continue
+    fi
+
+    kubectl delete "${pod}"
+
+done
