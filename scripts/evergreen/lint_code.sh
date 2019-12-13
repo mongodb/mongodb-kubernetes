cc6e373b1d8dfbfb07eae96ed27274919faca7f0
Louis <louis.plissonneau@mongodb.com>
2019-12-13 15:21:38 +0000

    Fix go1.13.x error (#591)

--- scripts/evergreen/lint_code.sh
+++ scripts/evergreen/lint_code.sh
@@ -4,39 +4,17 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-GOPATH="${WORKDIR}"
-export GOPATH
-
-export GOROOT="/usr/lib/go"
-export GOBIN="${GOPATH}/bin"
-export PATH="${GOBIN}:${PATH}"
-
-export GO111MODULE=on
-export GOFLAGS="-mod=vendor"
-
-if ! [[ -x "$(command -v goimports)" ]]; then
-    echo "installing goimports..."
-    GOFLAGS="" go get golang.org/x/tools/cmd/goimports
-else
-    echo "goimports is already installed"
-fi
+export GOPATH="${WORKDIR}"
 
 if ! [[ -x "$(command -v staticcheck)" ]]; then
-    echo "installing goimports..."
-    GO111MODULE="off" GOFLAGS="" go get honnef.co/go/tools/...
+    echo "installing gotools..."
+    GOFLAGS="" go get -u honnef.co/go/tools/...
   else
     echo "go tools are already installed"
 fi
 
 # check for dead code
-staticcheck -checks U1000,SA4006,ST1019,S1005 ./pkg/controller/...
-
-# ensure all code has been formatted with goimports
-if [[ "$($GOPATH/bin/goimports -l ./pkg/controller ./pkg/util ./pkg/apis main.go)" ]]; then
-    echo "ERROR: Not all code has been formatted with goimports."
-    echo "Run: goimports -w ./pkg/controller ./pkg/util ./pkg/apis main.go"
-fi
-
+PATH=$GOPATH/bin:$PATH staticcheck -checks U1000,SA4006,ST1019,S1005 ./pkg/controller/...
 
 # some directories are excluded from vetting as they are auto-generated
 vet_exclusions="github.com/10gen/ops-manager-kubernetes/pkg/apis/mongodb.com/v1
