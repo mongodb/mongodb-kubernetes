d0030f035dc43372e65e6fcee9a557c79803f67e
Anton <anton.lisovenko@10gen.com>
2020-02-25 09:41:58 +0000

    CLOUDP-57870: om podspec samples (#746)

--- pkg/kube/statefulset/statefulset_builder.go
+++ pkg/kube/statefulset/statefulset_builder.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"sort"
 
-	"github.com/hashicorp/go-multierror"
+	multierror "github.com/hashicorp/go-multierror"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
