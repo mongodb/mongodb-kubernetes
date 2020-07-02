4eb722736b8111ae8e14469daa5f1cce421776a9
Anton <anton.lisovenko@10gen.com>
2020-07-02 20:14:20 +0100

    Some refactoring of apis package (moving appdb out and some renamings) (#979)

--- pkg/apis/mongodb.com/v1/om/opsmanager_types.go
+++ pkg/apis/mongodb.com/v1/om/opsmanager_types.go
@@ -14,8 +14,6 @@ import (
 	"github.com/10gen/ops-manager-kubernetes/pkg/util"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -449,96 +447,4 @@ func SchemePortFromAnnotation(annotation string) (corev1.URIScheme, int) {
 	}
 
 	return scheme, port
-}
-
-//=============== AppDB ===========================================
-
-// +k8s:deepcopy-gen=true
-type AppDB struct {
-	mdbv1.MongoDbSpec `json:",inline"`
-
-	// PasswordSecretKeyRef contains a reference to the secret which contains the password
-	// for the mongodb-ops-manager SCRAM-SHA user
-	PasswordSecretKeyRef *userv1.SecretKeyRef `json:"passwordSecretKeyRef,omitempty"`
-
-	// transient fields. These fields are cleaned before serialization, see 'MarshalJSON()'
-	// note, that we cannot include the 'OpsManager' instance here as this creates circular dependency and problems with
-	// 'DeepCopy'
-
-	OpsManagerName string `json:"-"`
-	Namespace      string `json:"-"`
-}
-
-type AppDbBuilder struct {
-	appDb *AppDB
-}
-
-func DefaultAppDbBuilder() *AppDbBuilder {
-	appDb := &AppDB{
-		MongoDbSpec:          mdbv1.MongoDbSpec{Version: "", Members: 3, PodSpec: &mdbv1.MongoDbPodSpec{}},
-		PasswordSecretKeyRef: &userv1.SecretKeyRef{},
-	}
-	return &AppDbBuilder{appDb: appDb}
-}
-
-func (b *AppDbBuilder) Build() *AppDB {
-	return b.appDb.DeepCopy()
-}
-
-func (m AppDB) GetSecretName() string {
-	return m.Name() + "-password"
-}
-
-func (m *AppDB) UnmarshalJSON(data []byte) error {
-	type MongoDBJSON *AppDB
-	if err := json.Unmarshal(data, (MongoDBJSON)(m)); err != nil {
-		return err
-	}
-
-	// if a reference is specified without a key, we will default to "password"
-	if m.PasswordSecretKeyRef != nil && m.PasswordSecretKeyRef.Key == "" {
-		m.PasswordSecretKeyRef.Key = util.DefaultAppDbPasswordKey
-	}
-
-	m.ConnectionSpec.Credentials = ""
-	m.ConnectionSpec.CloudManagerConfig = nil
-	m.ConnectionSpec.OpsManagerConfig = nil
-	m.ConnectionSpec.Project = ""
-	// all resources have a pod spec
-	if m.PodSpec == nil {
-		m.PodSpec = mdbv1.NewMongoDbPodSpec()
-	}
-	return nil
-}
-
-func (m AppDB) Name() string {
-	return m.OpsManagerName + "-db"
-}
-
-func (m AppDB) ProjectIDConfigMapName() string {
-	return m.Name() + "-project-id"
-}
-
-func (m AppDB) ServiceName() string {
-	if m.Service == "" {
-		return m.Name() + "-svc"
-	}
-	return m.Service
-}
-
-func (m AppDB) AutomationConfigSecretName() string {
-	return m.Name() + "-config"
-}
-
-// ConnectionURL returns the connection url to the AppDB
-func (m AppDB) ConnectionURL(userName, password string, connectionParams map[string]string) string {
-	return mdbv1.BuildConnectionUrl(m.Name(), m.ServiceName(), m.Namespace, userName, password, m.MongoDbSpec, connectionParams)
-}
-
-// todo these two methods are added only to make AppDB implement runtime.Object
-func (m *AppDB) GetObjectKind() schema.ObjectKind {
-	return nil
-}
-func (m *AppDB) DeepCopyObject() runtime.Object {
-	return nil
 }
