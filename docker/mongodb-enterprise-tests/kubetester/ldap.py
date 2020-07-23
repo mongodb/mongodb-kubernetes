074d62a5fb767091aeb509d2eae34d51404294ac
Rodrigo Valin <rodrigo.valin@mongodb.com>
2020-07-23 10:44:58 +0100

    CLOUDP-67531: better user/group/org creation for LDAP. (#1016)

--- docker/mongodb-enterprise-tests/kubetester/ldap.py
+++ docker/mongodb-enterprise-tests/kubetester/ldap.py
@@ -31,29 +31,87 @@ class LDAPUser:
         return "uid={},{}".format(self.uid, self.ldap_base)
 
 
-def create_user(server: OpenLDAP, user: LDAPUser, ca_path: Optional[str] = None):
+def create_user(
+    server: OpenLDAP,
+    user: LDAPUser,
+    ca_path: Optional[str] = None,
+    ou: Optional[str] = None,
+):
+    """Creates a new user in the LDAP database. It might include an optional organizational unit (ou)."""
+    con = ldap_initialize(server, ca_path)
+
+    modlist = {
+        "objectClass": [b"top", b"account", b"simpleSecurityObject"],
+        "userPassword": [str.encode(user.password)],
+        "uid": [str.encode(user.uid)],
+    }
+    ldapmodlist = ldap.modlist.addModlist(modlist)
+
+    dn = build_dn(uid=user.uid, ou=ou, base=server.ldap_base)
+    con.add_s(dn, ldapmodlist)
+
+
+def create_organizational_unit(
+    server: OpenLDAP, ou: str, ca_path: Optional[str] = None
+):
+    """Creates an organizational unit."""
+    con = ldap_initialize(server, ca_path)
+
+    modlist = {"objectClass": [b"top", b"organizationalUnit"], "ou": [str.encode(ou)]}
+
+    ldapmodlist = ldap.modlist.addModlist(modlist)
+
+    dn = build_dn(ou=ou, base=server.ldap_base)
+    con.add_s(dn, ldapmodlist)
+
+
+def create_group(server: OpenLDAP, cn: str, ou: str, ca_path: Optional[str] = None):
+    """Creates a group in the LDAP database, that also belongs to an organizational unit. By default, it adds
+    the admin user to it."""
+    con = ldap_initialize(server, ca_path)
+
+    unique_member = build_dn(base=server.ldap_base, uid="admin", ou=ou)
+    modlist = {
+        "objectClass": [b"top", b"groupOfUniqueNames"],
+        "cn": str.encode(cn),
+        "uniqueMember": str.encode(unique_member),
+    }
+    ldapmodlist = ldap.modlist.addModlist(modlist)
+
+    dn = build_dn(base=server.ldap_base, cn=cn, ou=ou)
+    con.add_s(dn, ldapmodlist)
+
+
+def add_user_to_group(
+    server: OpenLDAP, user: str, group_cn: str, ou: str, ca_path: Optional[str] = None
+):
+    """Adds a new uniqueMember to a group, this is equivalent to add a user to the group."""
+    con = ldap_initialize(server, ca_path)
+
+    unique_member = build_dn(uid=user, ou=ou, base=server.ldap_base)
+    modlist = {"uniqueMember": [str.encode(unique_member)]}
+    ldapmodlist = ldap.modlist.modifyModlist({}, modlist)
+
+    dn = build_dn(cn=group_cn, ou=ou, base=server.ldap_base)
+    con.modify_s(dn, ldapmodlist)
+
+
+def ldap_initialize(server: OpenLDAP, ca_path: Optional[str] = None):
     con = ldap.initialize(server.host)
 
     if server.host.startswith("ldaps://") and ca_path is not None:
         con.set_option(ldap.OPT_X_TLS_CACERTFILE, ca_path)
         con.set_option(ldap.OPT_X_TLS_NEWCTX, 0)
 
-    dn_admin = "cn=admin," + server.ldap_base
+    dn_admin = build_dn(cn="admin", base=server.ldap_base)
     con.simple_bind_s(dn_admin, server.admin_password)
+    return con
 
-    modlist = {
-        "objectClass": [b"inetOrgPerson", b"posixAccount", b"shadowAccount"],
-        "userPassword": [str.encode(user.password)],
-        "uid": [str.encode(user.uid)],
-        "sn": [b"tests"],
-        "cn": [b"Tests"],
-        "displayName": [b"Tests"],
-        "uidNumber": [b"5000"],
-        "gidNumber": [b"10000"],
-        "loginShell": [b"/bin/false"],
-        "homeDirectory": [b"/home/auto"],
-    }
 
-    ldapmodlist = ldap.modlist.addModlist(modlist)
+def build_dn(base: Optional[str] = None, **kwargs):
+    """Builds a distinguished name from arguments."""
+    dn = ",".join("{}={}".format(k, v) for k, v in kwargs.items() if v is not None)
+    if base is not None:
+        dn += "," + base
 
-    con.add_s(user.username, ldapmodlist)
+    return dn
