6b150dd91b27459a6e36e6b7180b488b473738b6
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-11-24 13:48:50 +0100

    Fix to operator checkout in tests (#1245)

--- docker/mongodb-enterprise-tests/kubetester/git.py
+++ docker/mongodb-enterprise-tests/kubetester/git.py
@@ -3,5 +3,4 @@ from git import Repo
 
 def clone_and_checkout(url: str, fs_path: str, branch_name: str):
     repo = Repo.clone_from(url, fs_path)
-    branch = repo.create_head(branch_name)
-    branch.checkout()
+    repo.git.checkout(branch_name)
