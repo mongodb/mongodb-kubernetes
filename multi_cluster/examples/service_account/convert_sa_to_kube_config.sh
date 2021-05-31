2b8a7fc9c69df98b60e9fbe3732b9d192919efec
Rajdeep Das <rajdeep.das@mongodb.com>
2021-05-31 11:21:40 +0200

    Use kubectl command to set context. (#1571)

--- multi_cluster/examples/service_account/convert_sa_to_kube_config.sh
+++ multi_cluster/examples/service_account/convert_sa_to_kube_config.sh
@@ -7,20 +7,18 @@ NAMESPACE=chatton
 SA_NAME="can-read-pods"
 #SA_NAME="cannot-read-pods"
 
-kubectx ${CLUSTER2}
+kubectl --context ${CLUSTER2} delete ns ${NAMESPACE} --ignore-not-found
+kubectl --context ${CLUSTER2} create ns ${NAMESPACE}
 
-kubectl delete ns ${NAMESPACE} --ignore-not-found
-kubectl create ns ${NAMESPACE}
+kubectl --context ${CLUSTER2} delete serviceaccount ${SA_NAME} --ignore-not-found
+kubectl --context ${CLUSTER2} create serviceaccount ${SA_NAME} -n ${NAMESPACE} || true
+SA2_SECRET_NAME="$(kubectl get --context ${CLUSTER2} secret -n ${NAMESPACE} | grep ${SA_NAME} | awk '{ print $ 1}')"
 
-kubectl delete serviceaccount ${SA_NAME} --ignore-not-found
-kubectl create serviceaccount ${SA_NAME} -n ${NAMESPACE} || true
-SA2_SECRET_NAME="$(kubectl get secret -n ${NAMESPACE} | grep ${SA_NAME} | awk '{ print $ 1}')"
+kubectl --context ${CLUSTER2} apply -f cluster2_resources -n ${NAMESPACE}
 
-kubectl apply -f cluster2_resources -n ${NAMESPACE}
-
-ca=$(kubectl get secret/${SA2_SECRET_NAME} -n ${NAMESPACE} -o jsonpath='{.data.ca\.crt}')
-token=$(kubectl get secret/${SA2_SECRET_NAME} -n ${NAMESPACE} -o jsonpath='{.data.token}' | base64 --decode)
-namespace=$(kubectl get secret/${SA2_SECRET_NAME}  -n ${NAMESPACE} -o jsonpath='{.data.namespace}' | base64 --decode)
+ca=$(kubectl get --context ${CLUSTER2} secret/${SA2_SECRET_NAME} -n ${NAMESPACE} -o jsonpath='{.data.ca\.crt}')
+token=$(kubectl get --context ${CLUSTER2} secret/${SA2_SECRET_NAME} -n ${NAMESPACE} -o jsonpath='{.data.token}' | base64 --decode)
+namespace=$(kubectl get --context ${CLUSTER2} secret/${SA2_SECRET_NAME}  -n ${NAMESPACE} -o jsonpath='{.data.namespace}' | base64 --decode)
 
 echo "
 apiVersion: v1
@@ -44,15 +42,14 @@ users:
 " > kubeconfig
 
 
-kubectx ${CLUSTER1}
-kubectl delete ns ${NAMESPACE} --ignore-not-found
-kubectl create ns ${NAMESPACE}
+kubectl --context ${CLUSTER1} delete ns ${NAMESPACE} --ignore-not-found
+kubectl --context ${CLUSTER1} create ns ${NAMESPACE}
 
 
-kubectl delete configmap -n ${NAMESPACE} kubeconfig --ignore-not-found
-kubectl create configmap -n ${NAMESPACE} kubeconfig --from-file=kubeconfig || true
+kubectl delete --context ${CLUSTER1} configmap -n ${NAMESPACE} kubeconfig --ignore-not-found
+kubectl create --context ${CLUSTER1} configmap -n ${NAMESPACE} kubeconfig --from-file=kubeconfig || true
 
-kubectl apply -f cluster1_resources -n ${NAMESPACE}
+kubectl apply --context ${CLUSTER1} -f cluster1_resources -n ${NAMESPACE}
 
 rm kubeconfig
 
