037781890d94678125c5689b4def5a99947997b2
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2021-06-04 16:01:27 +0100

    Fix vanilla cluster (#1586)

--- docker/cluster-cleaner/scripts/clean-ops-manager.sh
+++ docker/cluster-cleaner/scripts/clean-ops-manager.sh
@@ -7,10 +7,5 @@ fi
 
 echo "Removing Ops Manager in ${OM_NAMESPACE}"
 
-# We sleep for a bit between stages to make sure that the underlying Persistent Volume
-# is unbound and then deallocated by Kubernetes.
-kubectl --namespace "${OM_NAMESPACE}" scale sts/mongodb-enterprise-ops-manager --replicas=0
-sleep 20
-kubectl --namespace "${OM_NAMESPACE}" delete pvc --all
-sleep 20
-kubectl --namespace "${OM_NAMESPACE}" scale sts/mongodb-enterprise-ops-manager --replicas=1
+kubectl --namespace "${OM_NAMESPACE}" delete om ops-manager
+
