37a50d68ceaf85787490dfbb65b38b1c4bd0c0ca
Rodrigo Valin <rodrigo.valin@mongodb.com>
2021-06-18 14:27:10 +0100

    CLOUDP-91788: Release OM 4.4.14 (#1624)

--- docker/mongodb-enterprise-tests/tests/conftest.py
+++ docker/mongodb-enterprise-tests/tests/conftest.py
@@ -183,7 +183,7 @@ def custom_mdb_version() -> str:
 def custom_version() -> str:
     """Returns a CUSTOM_OM_VERSION for OM.
     Defaults to 4.4+ (for development)"""
-    return os.getenv("CUSTOM_OM_VERSION", "4.4.13")
+    return os.getenv("CUSTOM_OM_VERSION", "4.4.14")
 
 
 @fixture(scope="module")
