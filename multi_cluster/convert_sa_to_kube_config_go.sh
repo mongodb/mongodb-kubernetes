eb3ec3509c0097b63bdf7f439fa2ea003abcd8a6
Rajdeep Das <rajdeep.das@mongodb.com>
2021-06-25 17:32:19 +0200

    Deploy operator with the script (#1649)

--- multi_cluster/convert_sa_to_kube_config_go.sh
+++ multi_cluster/convert_sa_to_kube_config_go.sh
@@ -9,7 +9,13 @@ NAMESPACE=$1
 OPERATOR_NAMESPACE=$2
 
 
-go run tools/cmd/main.go -member-clusters e2e.cluster1.mongokubernetes.com,e2e.cluster2.mongokubernetes.com -central-cluster ${CENTRAL_CLUSTER} -member-cluster-namespace ${NAMESPACE} -central-cluster-namespace ${OPERATOR_NAMESPACE} -cleanup # -cluster-scoped
+go run tools/cmd/main.go -member-clusters ${CLUSTER1},${CLUSTER2} -central-cluster ${CENTRAL_CLUSTER} -member-cluster-namespace ${NAMESPACE} -central-cluster-namespace ${OPERATOR_NAMESPACE} -cleanup # -cluster-scoped
 
-kubectl --context ${CENTRAL_CLUSTER} apply -f examples/service_account_using_go/central_cluster_resources/deployment.yaml --namespace ${OPERATOR_NAMESPACE}
+# deploy the multi-cluster CRD
+kubectl --context ${CENTRAL_CLUSTER} apply -f ../config/crd/bases/mongodb.com_mongodbmulti.yaml
 
+# deploy the operator pod
+kubectl --context ${CENTRAL_CLUSTER} apply -f operator-deployment.yaml --namespace ${OPERATOR_NAMESPACE}
+
+# deploy the CR
+kubectl --context ${CENTRAL_CLUSTER} apply -f ./multi-cluster-CR.yaml
