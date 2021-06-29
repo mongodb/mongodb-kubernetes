6444c8c78e6356779dd041356770ce37c256572b
Rajdeep Das <rajdeep.das@mongodb.com>
2021-06-29 10:52:19 +0200

    CLOUDP-93418: Deploy external OM in central cluster (#1651)

--- multi_cluster/convert_sa_to_kube_config_go.sh
+++ multi_cluster/convert_sa_to_kube_config_go.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -u
-
+# usage: sh ./convert_sa_to_kube_config_go.sh tmp operator
 CLUSTER1="e2e.cluster1.mongokubernetes.com"
 CLUSTER2="e2e.cluster2.mongokubernetes.com"
 CENTRAL_CLUSTER="e2e.operator.mongokubernetes.com"
@@ -11,6 +11,9 @@ OPERATOR_NAMESPACE=$2
 
 go run tools/cmd/main.go -member-clusters ${CLUSTER1},${CLUSTER2} -central-cluster ${CENTRAL_CLUSTER} -member-cluster-namespace ${NAMESPACE} -central-cluster-namespace ${OPERATOR_NAMESPACE} -cleanup # -cluster-scoped
 
+# deploy the MDB CRD in central cluster -- OM reconciler watches it
+kubectl --context ${CENTRAL_CLUSTER} apply -f ../config/crd/bases/mongodb.com_mongodb.yaml
+
 # deploy the multi-cluster CRD
 kubectl --context ${CENTRAL_CLUSTER} apply -f ../config/crd/bases/mongodb.com_mongodbmulti.yaml
 
@@ -19,3 +22,9 @@ kubectl --context ${CENTRAL_CLUSTER} apply -f operator-deployment.yaml --namespa
 
 # deploy the CR
 kubectl --context ${CENTRAL_CLUSTER} apply -f ./multi-cluster-CR.yaml
+
+# create OM admin secret
+ kubectl create secret generic ops-manager-admin-secret  --from-literal=Username="user.name@example.com" --from-literal=Password="Passw0rd."  --from-literal=FirstName="User" --from-literal=LastName="Name"
+
+# deploy OM in central cluster
+kubectl --context ${CENTRAL_CLUSTER} apply -f ./om.yaml
