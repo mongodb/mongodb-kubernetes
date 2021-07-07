646600e6f31b14c5d9c86fda2f2e2d26c49ea241
Rajdeep Das <rajdeep.das@mongodb.com>
2021-07-07 09:31:15 +0200

    CLOUDP-93813: Release OM 4.4.15 in Quay (#1671)

--- docker/mongodb-enterprise-tests/tests/conftest.py
+++ docker/mongodb-enterprise-tests/tests/conftest.py
@@ -188,7 +188,7 @@ def custom_mdb_version() -> str:
 def custom_version() -> str:
     """Returns a CUSTOM_OM_VERSION for OM.
     Defaults to 4.4+ (for development)"""
-    return os.getenv("CUSTOM_OM_VERSION", "4.4.14")
+    return os.getenv("CUSTOM_OM_VERSION", "4.4.15")
 
 
 @fixture(scope="module")
