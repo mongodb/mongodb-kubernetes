203be3c4bb947e3acff6b93e123ae783077e6937
Rajdeep Das <rajdeep.das@mongodb.com>
2021-09-06 11:36:53 +0200

    CLOUDP-99468: E2E test TLS + SCRAM (#1864)

--- docker/mongodb-enterprise-tests/tests/multicluster/multi_cluster_tls_with_scram.py
+++ docker/mongodb-enterprise-tests/tests/multicluster/multi_cluster_tls_with_scram.py
@@ -9,14 +9,21 @@ from kubetester.mongotester import with_tls
 from kubetester.operator import Operator
 from kubetester.kubetester import fixture as yaml_fixture
 from kubetester.mongodb import Phase
+from kubetester.mongodb_user import MongoDBUser
+from kubetester import create_secret
+from kubetester.mongotester import with_scram
 
 BUNDLE_SECRET_NAME = "prefix-multi-cluster-replica-set-cert"
+MDB_RESOURCE = "multi-cluster-replica-set"
+USER_NAME = "my-user-1"
+PASSWORD_SECRET_NAME = "mms-user-1-password"
+USER_PASSWORD = "my-password"
 
 
 @fixture(scope="module")
 def mongodb_multi_unmarshalled(namespace: str) -> MongoDBMulti:
     resource = MongoDBMulti.from_yaml(
-        yaml_fixture("mongodb-multi.yaml"), "multi-cluster-replica-set", namespace
+        yaml_fixture("mongodb-multi.yaml"), MDB_RESOURCE, namespace
     )
     return resource
 
@@ -56,12 +63,30 @@ def mongodb_multi(
     return resource.create()
 
 
-@mark.e2e_multi_cluster_tls
+@fixture(scope="module")
+def mongodb_user(
+    central_cluster_client: kubernetes.client.ApiClient, namespace: str
+) -> MongoDBUser:
+    resource = MongoDBUser.from_yaml(
+        yaml_fixture("mongodb-user.yaml"), "multi-replica-set-scram-user", namespace
+    )
+
+    resource["spec"]["username"] = USER_NAME
+    resource["spec"]["passwordSecretKeyRef"] = {
+        "name": PASSWORD_SECRET_NAME,
+        "key": "password",
+    }
+    resource["spec"]["mongodbResourceRef"]["name"] = MDB_RESOURCE
+    resource.api = kubernetes.client.CustomObjectsApi(central_cluster_client)
+    return resource.create()
+
+
+@mark.e2e_multi_cluster_tls_with_scram
 def test_deploy_operator(multi_cluster_operator: Operator):
     multi_cluster_operator.assert_is_running()
 
 
-@mark.e2e_multi_cluster_tls
+@mark.e2e_multi_cluster_tls_with_scram
 def test_deploy_mongodb_multi_with_tls(
     mongodb_multi: MongoDBMulti,
     namespace: str,
@@ -77,8 +102,52 @@ def test_deploy_mongodb_multi_with_tls(
     mongodb_multi.assert_reaches_phase(Phase.Running, timeout=900)
 
 
+@mark.e2e_multi_cluster_tls_with_scram
+def test_update_mongodb_multi_tls_with_scram(
+    mongodb_multi: MongoDBMulti,
+    namespace: str,
+):
+    mongodb_multi.load()
+    mongodb_multi["spec"]["security"] = {
+        "authentication": {"enabled": True, "modes": ["SCRAM"]}
+    }
+    mongodb_multi.update()
+    mongodb_multi.assert_reaches_phase(Phase.Running, timeout=700)
+
+
+@mark.e2e_multi_cluster_tls_with_scram
+def test_create_mongodb_user(
+    central_cluster_client: kubernetes.client.ApiClient,
+    mongodb_user: MongoDBUser,
+    namespace: str,
+):
+    # create user secret first
+    create_secret(
+        namespace=namespace,
+        name=PASSWORD_SECRET_NAME,
+        data={"password": USER_PASSWORD},
+        api_client=central_cluster_client,
+    )
+    mongodb_user.assert_reaches_phase(Phase.Updated, timeout=100)
+
+
 @skip_if_local
-@mark.e2e_multi_cluster_tls
+@mark.e2e_multi_cluster_tls_with_scram
 def test_tls_connectivity(mongodb_multi: MongoDBMulti, ca_path: str):
     tester = mongodb_multi.tester()
     tester.assert_connectivity(opts=[with_tls(use_tls=True, ca_path=ca_path)])
+
+
+@skip_if_local
+@mark.e2e_multi_cluster_tls_with_scram
+def test_replica_set_connectivity_with_scram_and_tls(
+    mongodb_multi: MongoDBMulti, ca_path: str
+):
+    tester = mongodb_multi.tester()
+    tester.assert_connectivity(
+        db="admin",
+        opts=[
+            with_scram(USER_NAME, USER_PASSWORD),
+            with_tls(use_tls=True, ca_path=ca_path),
+        ],
+    )
