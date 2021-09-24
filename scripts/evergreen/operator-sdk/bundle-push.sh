f45c19ed306092cdc1503e23e0b88f42ed39b9f5
Rodrigo Valin <rodrigo.valin@mongodb.com>
2021-09-24 09:51:09 +0100

    CLOUDP-98806: Bundle image in Evergreen. (#1903)

--- scripts/evergreen/operator-sdk/bundle-push.sh
+++ scripts/evergreen/operator-sdk/bundle-push.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -Eeou pipefail
 
+mkdir -p "${workdir:?}/bin"
+operator_sdk_bin="${workdir:?}/bin/operator-sdk"
+version="v1.12.0"
+curl -L "https://github.com/operator-framework/operator-sdk/releases/download/${version}/operator-sdk_linux_amd64" -o "${operator_sdk_bin}"
+chmod +x "${operator_sdk_bin}"
+
+echo "Installed operator-sdk ${version} in ${operator_sdk_bin}"
+
 VERSION="$(git describe --tags --abbrev=0)"
 BUNDLE_IMG="scan.connect.redhat.com/ospid-52d1c6df-b3f6-432b-9646-adb7f689e581/operator-bundle:${VERSION}"
 
@@ -8,3 +16,7 @@ BUNDLE_IMG="scan.connect.redhat.com/ospid-52d1c6df-b3f6-432b-9646-adb7f689e581/o
 make bundle-annotated "VERSION=${VERSION}" IMG="registry.connect.redhat.com/mongodb/enterprise-operator:${VERSION}"
 make bundle-build EXPIRES= "VERSION=${VERSION}" "BUNDLE_IMG=${BUNDLE_IMG}"
 make docker-push "VERSION=${VERSION}" "IMG=${BUNDLE_IMG}"
+
+
+echo "Bundle has been pushed to Openshift Marketplace"
+echo "You can review publishing state in https://connect.redhat.com/projects/5f7dcbbd9208171f730fbd03/images"
