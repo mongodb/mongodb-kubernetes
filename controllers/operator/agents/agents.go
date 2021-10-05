a67da3eae966d9b1f626bfbf2efbf16cf956be4c
Rajdeep Das <rajdeep.das@mongodb.com>
2021-10-05 09:51:58 +0200

    CLOUDP-101819: Configure agent register time, increase the timeout for MC (#1920)

--- controllers/operator/agents/agents.go
+++ controllers/operator/agents/agents.go
@@ -22,6 +22,11 @@ type SecretGetCreator interface {
 	secret.Creator
 }
 
+type retryParams struct {
+	waitSeconds int
+	retrials    int
+}
+
 // ensureAgentKeySecretExists checks if the Secret with specified name (<groupId>-group-secret) exists, otherwise tries to
 // generate agent key using OM public API and create Secret containing this key. Generation of a key is expected to be
 // a rare operation as the group creation api generates agent key already (so the only possible situation is when the group
@@ -71,7 +76,7 @@ func WaitForRsAgentsToRegisterReplicasSpecified(set appsv1.StatefulSet, members 
 	hostnames, _ := dns.GetDnsForStatefulSetReplicasSpecified(set, clusterName, members)
 	log = log.With("statefulset", set.Name)
 
-	if !waitUntilRegistered(omConnection, log, hostnames...) {
+	if !waitUntilRegistered(omConnection, log, retryParams{retrials: 5, waitSeconds: 3}, hostnames...) {
 		return errors.New("Some agents failed to register or the Operator is using the wrong host names for the pods. " +
 			"Make sure the 'spec.clusterDomain' is set if it's different from the default Kubernetes cluster " +
 			"name ('cluster.local') ")
@@ -81,7 +86,7 @@ func WaitForRsAgentsToRegisterReplicasSpecified(set appsv1.StatefulSet, members 
 
 // WaitForRsAgentsToRegisterReplicasSpecifiedMultiCluster waits for the specified agents to registry with Ops Manager.
 func WaitForRsAgentsToRegisterReplicasSpecifiedMultiCluster(omConnection om.Connection, hostnames []string, log *zap.SugaredLogger) error {
-	if !waitUntilRegistered(omConnection, log, hostnames...) {
+	if !waitUntilRegistered(omConnection, log, retryParams{retrials: 10, waitSeconds: 9}, hostnames...) {
 		return errors.New("Some agents failed to register or the Operator is using the wrong host names for the pods. " +
 			"Make sure the 'spec.clusterDomain' is set if it's different from the default Kubernetes cluster " +
 			"name ('cluster.local') ")
@@ -91,11 +96,11 @@ func WaitForRsAgentsToRegisterReplicasSpecifiedMultiCluster(omConnection om.Conn
 
 // waitUntilRegistered waits until all agents with 'agentHostnames' are registered in OM. Note, that wait
 // happens after retrial - this allows to skip waiting in case agents are already registered
-func waitUntilRegistered(omConnection om.Connection, log *zap.SugaredLogger, agentHostnames ...string) bool {
+func waitUntilRegistered(omConnection om.Connection, log *zap.SugaredLogger, r retryParams, agentHostnames ...string) bool {
 	log.Infow("Waiting for agents to register with OM", "agent hosts", agentHostnames)
 	// environment variables are used only for tests
-	waitSeconds := env.ReadIntOrDefault(util.PodWaitSecondsEnv, 3)
-	retrials := env.ReadIntOrDefault(util.PodWaitRetriesEnv, 5)
+	waitSeconds := env.ReadIntOrDefault(util.PodWaitSecondsEnv, r.waitSeconds)
+	retrials := env.ReadIntOrDefault(util.PodWaitRetriesEnv, r.retrials)
 
 	agentsCheckFunc := func() (string, bool) {
 		registeredCount := 0
