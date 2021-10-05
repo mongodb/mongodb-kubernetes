3fd622c99d875904309ccf83d51b59da283249a3
Rajdeep Das <rajdeep.das@mongodb.com>
2021-10-05 18:04:03 +0200

    Fix service account creation for GKE (#1924)

--- multi_cluster/tools/cmd/main.go
+++ multi_cluster/tools/cmd/main.go
@@ -15,6 +15,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
