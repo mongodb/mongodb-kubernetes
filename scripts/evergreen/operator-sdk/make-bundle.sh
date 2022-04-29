d8d3b83be856eec372308edbb022e1f7577f3206
mircea-cosbuc <mircea-cosbuc@users.noreply.github.com>
2022-04-29 17:07:23 +0200

    CLOUDP-113391: Upload operator bundle in evergreen task (#2316)
    * Upload operator bundle in evergreen task
    
    * Pushes a tgz file instead of directory contents.
    
    * Remove outdated comments from make-bundle script
    
    Co-authored-by: Rodrigo Valin <rodrigo.valin@mongodb.com>

--- scripts/evergreen/operator-sdk/make-bundle.sh
+++ scripts/evergreen/operator-sdk/make-bundle.sh
@@ -8,20 +8,6 @@ chmod +x "${operator_sdk_bin}"
 
 echo "Installed operator-sdk ${version} in ${operator_sdk_bin}"
 
-operator-sdk olm install --version 0.18.3
+tag="${triggered_by_git_tag:-$(git describe --tags)}"
 
-echo "Installed Operator Lifecycle Management (olm) into cluster."
-
-# The operator-bundle image will go to a public quay repo, because operator-sdk
-# can't be configured to use pivate registries yet.
-echo "${quay_prod_robot_token:?}" | docker login quay.io/mongodb --password-stdin --username "${quay_prod_username:?}"
-
-docker pull "${operator_img:?}"
-kind load docker-image "${operator_img}"
-
-tag="$(git describe --tags)"
-bundle_img="quay.io/mongodb/operator-bundle:${tag}"
-
-make bundle-annotated VERSION="${tag}" IMG="${operator_img:?}"
-make bundle-build VERSION="${tag}" BUNDLE_IMG="${bundle_img}"
-make docker-push IMG="${bundle_img}"
+make bundle-annotated VERSION="${tag}" IMG="quay.io/mongodb/mongodb-enterprise-operator:${tag}"
