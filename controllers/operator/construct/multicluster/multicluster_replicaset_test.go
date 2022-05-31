38ec13410c6a5f463c35345f9e0f5c0c31b18ed6
Rajdeep Das <rajdeep.das@mongodb.com>
2022-05-31 17:24:25 +0530

    CLOUDP-124543: Configure storage size for data persistent volume (#2359)

--- controllers/operator/construct/multicluster/multicluster_replicaset_test.go
+++ controllers/operator/construct/multicluster/multicluster_replicaset_test.go
@@ -7,17 +7,18 @@ import (
 	mdbv1 "github.com/10gen/ops-manager-kubernetes/api/v1/mdb"
 	"github.com/10gen/ops-manager-kubernetes/api/v1/mdbmulti"
 	"github.com/10gen/ops-manager-kubernetes/controllers/om"
+	"github.com/10gen/ops-manager-kubernetes/controllers/operator/construct"
 	"github.com/10gen/ops-manager-kubernetes/controllers/operator/mock"
 	"github.com/10gen/ops-manager-kubernetes/pkg/util"
 	"github.com/stretchr/testify/assert"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func getMultiClusterMongoDB() mdbmulti.MongoDBMulti {
 	spec := mdbmulti.MongoDBMultiSpec{
-		Version:    "5.0.0",
-		Persistent: util.BooleanRef(false),
+		Version: "5.0.0",
 		ConnectionSpec: mdbv1.ConnectionSpec{
 			OpsManagerConfig: &mdbv1.PrivateCloudConfig{
 				ConfigMapRef: mdbv1.ConfigMapRef{
@@ -97,5 +98,77 @@ func TestMultiClusterStatefulSet(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, *sts.Spec.Replicas, tt.outReplicas)
 		assert.Equal(t, sts.Spec.Selector.MatchLabels, tt.outLabelSelector)
+	}
+}
+
+func TestPVCOverride(t *testing.T) {
+
+	tests := []struct {
+		inp appsv1.StatefulSetSpec
+		out struct {
+			Storage    int64
+			AccessMode []corev1.PersistentVolumeAccessMode
+		}
+	}{
+		{
+			inp: appsv1.StatefulSetSpec{
+				VolumeClaimTemplates: []corev1.PersistentVolumeClaim{
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "data",
+						},
+						Spec: corev1.PersistentVolumeClaimSpec{
+							Resources: corev1.ResourceRequirements{
+								Requests: corev1.ResourceList{
+									corev1.ResourceStorage: construct.ParseQuantityOrZero("20"),
+								},
+							},
+							AccessModes: []corev1.PersistentVolumeAccessMode{},
+						},
+					},
+				},
+			},
+			out: struct {
+				Storage    int64
+				AccessMode []corev1.PersistentVolumeAccessMode
+			}{
+				Storage:    20,
+				AccessMode: []corev1.PersistentVolumeAccessMode{"ReadWriteOnce"},
+			},
+		},
+		{
+			inp: appsv1.StatefulSetSpec{
+				VolumeClaimTemplates: []corev1.PersistentVolumeClaim{
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "data",
+						},
+						Spec: corev1.PersistentVolumeClaimSpec{
+							AccessModes: []corev1.PersistentVolumeAccessMode{"ReadWriteMany"},
+						},
+					},
+				},
+			},
+			out: struct {
+				Storage    int64
+				AccessMode []corev1.PersistentVolumeAccessMode
+			}{
+				Storage:    16000000000,
+				AccessMode: []corev1.PersistentVolumeAccessMode{"ReadWriteOnce", "ReadWriteMany"},
+			},
+		},
+	}
+
+	os.Setenv(util.AutomationAgentImage, "some-registry")
+	os.Setenv(util.InitDatabaseImageUrlEnv, "some-registry")
+
+	for _, tt := range tests {
+		mdbm := getMultiClusterMongoDB()
+		mdbm.Spec.ClusterSpecList.ClusterSpecs[0].StatefulSetConfiguration.SpecWrapper.Spec = tt.inp
+		sts, err := MultiClusterStatefulSet(mdbm, 0, 3, om.NewEmptyMockedOmConnection(&om.OMContext{}), "")
+		assert.NoError(t, err)
+		assert.Equal(t, tt.out.AccessMode, sts.Spec.VolumeClaimTemplates[0].Spec.AccessModes)
+		storage, _ := sts.Spec.VolumeClaimTemplates[0].Spec.Resources.Requests.Storage().AsInt64()
+		assert.Equal(t, tt.out.Storage, storage)
 	}
 }
