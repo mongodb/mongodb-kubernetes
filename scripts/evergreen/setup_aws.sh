23bede60587725839e149b56ed5661bfe817e1b0
mircea-cosbuc <mircea-cosbuc@users.noreply.github.com>
2022-06-01 13:51:25 +0200

    CLOUDP-122683: Use task_group with max_hosts for daily builds (#2361)

--- scripts/evergreen/setup_aws.sh
+++ scripts/evergreen/setup_aws.sh
@@ -13,4 +13,4 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip &> /dev/null
 sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
 sudo chmod g+rwx "/home/$USER/.docker" -R
-sudo ./aws/install --bin-dir "${BIN_LOCATION}" --install-dir "${INSTALL_DIR}"
+sudo ./aws/install --bin-dir "${BIN_LOCATION}" --install-dir "${INSTALL_DIR}" --update
