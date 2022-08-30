feacaf9ab473587ee4d23074b03e92725c8bc711
Sebastian Łaskawiec <slaskawi@users.noreply.github.com>
2022-08-30 14:50:40 +0200

    dependencies update (#2481)

--- scripts/evergreen/build_multi_cluster_kubeconfig_creator.sh
+++ scripts/evergreen/build_multi_cluster_kubeconfig_creator.sh
@@ -2,5 +2,7 @@
 set -Eeou pipefail
 
 echo "Building multi cluster kube config creation tool."
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -buildvcs=false -o docker/mongodb-enterprise-tests/multi-cluster-kube-config-creator public/tools/multicluster/main.go
+pushd public/tools/multicluster
+GOOS=linux GOARCH=amd64 go build -buildvcs=false -o ../../../docker/mongodb-enterprise-tests/multi-cluster-kube-config-creator main.go
+popd
 chmod +x docker/mongodb-enterprise-tests/multi-cluster-kube-config-creator
