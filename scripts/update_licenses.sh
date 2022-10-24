67ddc79af8430c46add0275d9e743f882886d5be
Sebastian Łaskawiec <slaskawi@users.noreply.github.com>
2022-10-24 09:28:42 +0200

    CLOUDP-139972 Don't omit Google dependencies when generating license (#2592)

--- scripts/update_licenses.sh
+++ scripts/update_licenses.sh
@@ -10,4 +10,4 @@ go install github.com/google/go-licenses@master
 SCRIPTS_DIR=$(dirname $(readlink -f $0))
 
 go-licenses report . --template "$SCRIPTS_DIR/update_licenses.tpl" > licenses_full.csv 2> licenses_stderr
-cat licenses_full.csv | grep -v 10gen | grep -v "github.com/mongodb" | grep -v "golang.org"  | sort > licenses.csv
+cat licenses_full.csv | grep -v 10gen | grep -v "github.com/mongodb" | grep -v "^golang.org"  | sort > licenses.csv
