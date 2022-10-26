13151a9b173775fce509d34e0a5eb09310849161
Rajdeep Das <rajdeep.das@mongodb.com>
2022-10-26 15:07:16 +0530

    CLOUDP-141467: Fix e2e_multi_cluster_disaster_recovery E2E test (#2594)

--- api/v1/mdbmulti/mongodbmulti_validation.go
+++ api/v1/mdbmulti/mongodbmulti_validation.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	v1 "github.com/10gen/ops-manager-kubernetes/api/v1"
-	"github.com/10gen/ops-manager-kubernetes/pkg/multicluster"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
@@ -35,7 +34,6 @@ func (m *MongoDBMulti) ProcessValidationsOnReconcile(old *MongoDBMulti) error {
 func (m *MongoDBMulti) RunValidations(old *MongoDBMulti) []v1.ValidationResult {
 	validators := []func(ms MongoDBMultiSpec) v1.ValidationResult{
 		validateUniqueClusterNames,
-		validateSpecifiedClusterNames,
 	}
 	var validationResults []v1.ValidationResult
 
@@ -57,31 +55,5 @@ func validateUniqueClusterNames(ms MongoDBMultiSpec) v1.ValidationResult {
 		}
 		present[e.ClusterName] = struct{}{}
 	}
-	return v1.ValidationSuccess()
-}
-
-func validateSpecifiedClusterNames(ms MongoDBMultiSpec) v1.ValidationResult {
-	kubeConfigFile, err := multicluster.NewKubeConfigFile()
-	if err != nil {
-		return v1.ValidationError(fmt.Sprintf("failed to open kubeconfig file: %s, err: %s", multicluster.KubeConfigPath, err))
-	}
-
-	kubeConfig, err := kubeConfigFile.LoadKubeConfigFile()
-	if err != nil {
-		msg := fmt.Sprintf("Couldn't load kubeconfig file from the path: %s", multicluster.KubeConfigPath)
-		return v1.ValidationError(msg)
-	}
-
-	clusters := make(map[string]struct{})
-	for _, context := range kubeConfig.Contexts {
-		clusters[context.Context.Cluster] = struct{}{}
-	}
-
-	for _, cluster := range ms.ClusterSpecList.ClusterSpecs {
-		if _, ok := clusters[cluster.ClusterName]; !ok {
-			return v1.ValidationError("Cluster %s credentials is not specified in Kubeconfig", cluster.ClusterName)
-		}
-	}
-
 	return v1.ValidationSuccess()
 }
