eadee6611f3d2657129aa7483e91ba5ca17e23d5
mircea-cosbuc <mircea-cosbuc@users.noreply.github.com>
2022-11-11 19:39:33 +0100

    Fix central cluster istion installation script (#2609)

--- multi_cluster/tools/install_istio_central.sh
+++ multi_cluster/tools/install_istio_central.sh
@@ -4,10 +4,10 @@ set -eux
 
 export VERSION=1.14.2
 
-export CTX_CLUSTER1=e2e.operator.mongokubernetes.com
+export CTX_CLUSTER=e2e.operator.mongokubernetes.com
 
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=${VERSION} sh -
 cd istio-${VERSION}
 
-
-bin/istioctl install --context="${CTX_CLUSTER}" --set profile profile=default --set meshConfig.outboundTrafficPolicy.mode=REGISTRY_ONLY -y
+bin/istioctl x uninstall --context="${CTX_CLUSTER}" --purge
+bin/istioctl install --context="${CTX_CLUSTER}" --set profile=default --set meshConfig.outboundTrafficPolicy.mode=REGISTRY_ONLY -y
