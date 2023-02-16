e21410531f6a301bfe6a795a03fe6b19eba8e414
mircea-cosbuc <mircea-cosbuc@users.noreply.github.com>
2023-02-16 08:17:10 +0100

    Use data-root instead of graph for docker daemon (#2735)

--- scripts/evergreen/configure-docker-datadir.sh
+++ scripts/evergreen/configure-docker-datadir.sh
@@ -9,24 +9,23 @@ if docker info 2>/dev/null | grep "Docker Root Dir" | grep -q "/var/lib/docker";
     echo "Trying with /etc/docker/daemon.json file"
     sudo mkdir -p /etc/docker
     sudo chmod o+w /etc/docker
-    cat <<EOF > "${HOME}/daemon.json"
+    cat <<EOF >"${HOME}/daemon.json"
 {
-    "graph": "/data/docker",
+    "data-root": "/data/docker",
     "storage-driver": "overlay2"
 }
 EOF
     sudo mv "${HOME}/daemon.json" /etc/docker/daemon.json
 
-
     sudo systemctl restart docker
-    if docker info 2>/dev/null | grep "Docker Root Dir"| grep -q "/data/docker"; then
+    if docker info 2>/dev/null | grep "Docker Root Dir" | grep -q "/data/docker"; then
         echo "Docker storage configured correctly"
     else
         # The change didn't went through, we are not failing the test, but it might
         # fail because of no free space left in device.
         echo "Docker storage was not configured properly"
-  fi
+    fi
 fi
 
 mkdir -p logs/
-docker info > logs/docker-info.text
+docker info >logs/docker-info.text
