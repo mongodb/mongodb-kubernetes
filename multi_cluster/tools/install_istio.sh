0d649d0b6ec8b197a11956fccf9822c8fd180eda
pierwill <19642016+pierwill@users.noreply.github.com>
2023-03-21 03:57:46 -0500

    CLOUDP-165860: Add checksum verification for Istio installer script (#2777)
    Co-authored-by: pierwill <pierwill@users.noreply.github.com>

--- multi_cluster/tools/install_istio.sh
+++ multi_cluster/tools/install_istio.sh
@@ -13,7 +13,11 @@ then
   IS_KIND="true"
 fi
 
-[ ! -d "istio-${VERSION}" ] && curl -L https://istio.io/downloadIstio | ISTIO_VERSION=${VERSION} sh -
+ISTIO_SCRIPT_CHECKSUM="254c6bd6aa5b8ac8c552561c84d8e9b3a101d9e613e2a8edd6db1f19c1871dbf"
+[ ! -d "istio-${VERSION}" ] && curl -O https://raw.githubusercontent.com/istio/istio/d710dfc2f95adb9399e1656165fa5ac22f6e1a16/release/downloadIstioCandidate.sh
+echo "${ISTIO_SCRIPT_CHECKSUM}  downloadIstioCandidate.sh" | sha256sum --check
+ISTIO_VERSION=${VERSION} sh downloadIstioCandidate.sh
+
 #
 cd istio-${VERSION}
 ## perform cleanup prior to install
