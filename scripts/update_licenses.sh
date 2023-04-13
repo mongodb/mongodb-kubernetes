c3f9662d75e9db5e57680f0fdfdbad4ce5aa6fd7
Nam Nguyen <nam.nguyen@mongodb.com>
2023-04-13 10:49:45 +0200

    make license check work (#2843)

--- scripts/update_licenses.sh
+++ scripts/update_licenses.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-set -xeou pipefail
+set -Eeou pipefail
 
-# Here we rely on https://github.com/google/go-licenses/pull/152
-# At the time of implementing this, it hasn't been tagged yet.
-# In a few months, it's safe to switch to the `latest`
-go install github.com/google/go-licenses@master
+export GOPATH="${workdir:-}"
 
-SCRIPTS_DIR=$(dirname $(readlink -f $0))
+go install github.com/google/go-licenses@v1.6.0
 
-go-licenses report . --template "$SCRIPTS_DIR/update_licenses.tpl" > licenses_full.csv 2> licenses_stderr
+SCRIPTS_DIR=$(dirname "$(readlink -f "$0")")
+
+PATH=$GOPATH/bin:$PATH go-licenses report . --template "$SCRIPTS_DIR/update_licenses.tpl" > licenses_full.csv 2> licenses_stderr
+
+# shellcheck disable=SC2002
 cat licenses_full.csv | grep -v 10gen | grep -v "github.com/mongodb" | grep -v "^golang.org"  | sort > licenses.csv
