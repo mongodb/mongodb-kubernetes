3daa6719d86cb4ce47a1fbfbd3eea272a57bf2e1
Łukasz Sierant <lukasz.sierant@mongodb.com>
2023-07-25 12:33:19 +0100

    CLOUDP-180049: Use diagnostic tool to gather logs in EVG (#2983)

--- scripts/evergreen/build_multi_cluster_kubeconfig_creator.sh
+++ scripts/evergreen/build_multi_cluster_kubeconfig_creator.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 set -Eeou pipefail
 
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
+if [[ "${ARCH}" == "x86_64" ]]; then
+  ARCH="amd64"
+fi
+
 echo "Building multi cluster kube config creation tool."
+
+project_dir="$(pwd)"
 pushd public/tools/multicluster
-GOOS=linux GOARCH=amd64 go build -buildvcs=false -o ../../../docker/mongodb-enterprise-tests/multi-cluster-kube-config-creator main.go
+GOOS="${OS}" GOARCH="${ARCH}" go build -buildvcs=false -o "${project_dir}/docker/mongodb-enterprise-tests/multi-cluster-kube-config-creator" main.go
 popd
 chmod +x docker/mongodb-enterprise-tests/multi-cluster-kube-config-creator
+
+mkdir -p bin || true
+cp docker/mongodb-enterprise-tests/multi-cluster-kube-config-creator bin/kubectl-mongodb
+
