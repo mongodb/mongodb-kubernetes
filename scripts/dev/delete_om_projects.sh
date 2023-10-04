58344c88f4bd32e7293f2a34deffe40abe28a39b
mircea-cosbuc <mircea-cosbuc@users.noreply.github.com>
2023-10-04 16:11:24 +0200

    CLOUDP-203228: Remove processes from OM projects before deletion (#3150)

--- scripts/dev/delete_om_projects.sh
+++ scripts/dev/delete_om_projects.sh
@@ -14,6 +14,12 @@ delete_project() {
   echo "Deleting project id of ${project_name} from ${OM_HOST}"
   project_id=$(curl -s -u "${OM_USER}:${OM_API_KEY}" --digest "${OM_HOST}/api/public/v1.0/groups/byName/${project_name}" | jq -r .id)
   if [[ "${project_id}" != "" && "${project_id}" != "null" ]]; then
+    echo "Removing controlledFeature policies for project ${project_name} (${project_id})"
+    curl -X PUT --digest -u "${OM_USER}:${OM_API_KEY}" "${OM_HOST}/api/public/v1.0/groups/${project_id}/controlledFeature" -H 'Content-Type: application/json' -d '{"externalManagementSystem": {"name": "mongodb-enterprise-operator"},"policies": []}'
+    echo
+    echo "Removing any existing automationConfig for project ${project_name} (${project_id})"
+    curl -X PUT --digest -u "${OM_USER}:${OM_API_KEY}" "${OM_HOST}/api/public/v1.0/groups/${project_id}/automationConfig" -H 'Content-Type: application/json' -d '{}'
+    echo
     echo "Deleting project ${project_name} (${project_id})"
     curl -X DELETE --digest -u "${OM_USER}:${OM_API_KEY}" "${OM_HOST}/api/public/v1.0/groups/${project_id}"
     echo
