a2f4a37b0313ecea422471693c29ff74cb3a5291
mircea-cosbuc <mircea-cosbuc@users.noreply.github.com>
2023-10-12 09:25:46 +0200

    Fix running python commands from $workdir folder (#3168)

---
+++ scripts/evergreen/run_python.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -Eeou pipefail
+
+source scripts/dev/set_env_context.sh
+
+# shellcheck disable=SC2154
+if [ -f "${workdir}"/venv/bin/activate ]; then
+    # shellcheck disable=SC1091
+    source "${workdir}"/venv/bin/activate
+fi
+
+python "$@"
