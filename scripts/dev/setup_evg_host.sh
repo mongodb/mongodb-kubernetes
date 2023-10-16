02a1b8588348e55982cc3219a81b5fe9486dd387
Lucian Tosa <49226451+lucian-tosa@users.noreply.github.com>
2023-10-16 10:28:15 +0200

    Allow multi-arch images to be rebuilt in daily builds (#3094)
    * Allow multi-arch images to be rebuilt in daily builds, solve merge conflicts
    
    * Improve documentation
    
    * Fix ubi suffix usage
    
    * Automatic PEP formatting
    
    * Configure architectures for daily build with command line argument
    
    * pre-commit hook
    
    * PR feedback and PEP formatting
    
    * pre-commit hook
    
    * evg_host configure now supports arm64
    
    * fix useless &
    
    * add quotes around variables
    
    * fix check for multi-arch rebuild
    
    ---------
    
    Co-authored-by: Julien Benhaim <julien.benhaim@outlook.com>

--- scripts/dev/setup_evg_host.sh
+++ scripts/dev/setup_evg_host.sh
@@ -7,27 +7,30 @@ set -Eeou pipefail
 echo "Increasing fs.inotify.max_user_instances"
 sudo sysctl -w fs.inotify.max_user_instances=8192
 
+# retrieve arch variable off the shell command line
+ARCH=${1-"amd64"}
+
 download_kind() {
   echo "Downloading kind..."
-  curl -s -o ./kind -L https://kind.sigs.k8s.io/dl/v0.19.0/kind-linux-amd64
+  curl -s -o ./kind -L https://kind.sigs.k8s.io/dl/v0.19.0/kind-linux-"${ARCH}"
   chmod +x ./kind
   sudo mv ./kind /usr/local/bin/kind
 }
 
 download_curl() {
   echo "Downloading curl..."
-  curl -s -o kubectl -L https://dl.k8s.io/release/"$(curl -L -s https://dl.k8s.io/release/stable.txt)"/bin/linux/amd64/kubectl
+  curl -s -o kubectl -L https://dl.k8s.io/release/"$(curl -L -s https://dl.k8s.io/release/stable.txt)"/bin/linux/"${ARCH}"/kubectl
   chmod +x kubectl
   sudo mv kubectl /usr/local/bin/kubectl
 }
 
 download_helm() {
   echo "Downloading helm..."
-  curl -s -o helm.tar.gz -L https://get.helm.sh/helm-v3.10.3-linux-amd64.tar.gz
+  curl -s -o helm.tar.gz -L https://get.helm.sh/helm-v3.10.3-linux-"${ARCH}"tar.gz
   tar -xf helm.tar.gz 2>/dev/null
-  sudo mv linux-amd64/helm /usr/local/bin/helm
+  sudo mv linux-"${ARCH}"helm /usr/local/bin/helm
   rm helm.tar.gz
-  rm -rf linux-amd64/
+  rm -rf linux-"${ARCH}/"
 }
 
 download_kind &
