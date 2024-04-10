b98fa55368e1b7ba99255e417fa6e5442d155773
Julien-Ben <33035980+Julien-Ben@users.noreply.github.com>
2024-04-10 12:58:37 +0200

    Refactor daily builds in pipeline (#3468)
    # Summary
    
    - Separate the logic of the `inner` function, which was almost 100 lines
    long, into small and readable chunks.
    - Refactor the `daily.yaml` inventory file.

--- pipeline_test.py
+++ pipeline_test.py
@@ -1,6 +1,6 @@
 from unittest import mock
 
-from pipeline import operator_build_configuration
+from pipeline import is_version_in_range, operator_build_configuration
 
 
 @mock.patch("builtins.open")
@@ -37,3 +37,20 @@ export NAMESPACE="something"
         config = operator_build_configuration("builder", True)
 
     assert config.skip_tags() == ["ubuntu"]
+
+
+def test_is_version_in_range():
+    # When one bound is empty or None, always return True
+    assert is_version_in_range("7.0.0", min_version="8.0.0", max_version="")
+    assert is_version_in_range("7.0.0", min_version="8.0.0", max_version=None)
+    assert is_version_in_range("9.0.0", min_version="", max_version="8.0.0")
+
+    # Upper bound is excluded
+    assert not is_version_in_range("8.1.1", min_version="8.0.0", max_version="8.1.1")
+
+    # Lower bound is included
+    assert is_version_in_range("8.0.0", min_version="8.0.0", max_version="8.1.1")
+
+    # Test some values
+    assert is_version_in_range("8.5.2", min_version="8.5.1", max_version="8.5.3")
+    assert not is_version_in_range("8.5.2", min_version="8.5.3", max_version="8.4.2")
