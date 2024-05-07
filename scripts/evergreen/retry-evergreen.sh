54a7e3bff5b23620f971ee0b66be2443723897a9
Sebastian Łaskawiec <slaskawi@users.noreply.github.com>
2024-05-07 15:46:31 +0200

    CLOUDP-245407 OpenShift retrigger (#3568)
    # Summary
    
    This Pull Request introduces a debugging switch to track down why
    OpenShift tests are retriggered.

--- scripts/evergreen/retry-evergreen.sh
+++ scripts/evergreen/retry-evergreen.sh
@@ -2,6 +2,8 @@
 
 set -Eeou pipefail
 
+set -x
+
 ###
 ## This script automatically retriggers failed tasks from Evergreen based on the `version_id` passed in from Evergreen.
 ##
@@ -38,14 +40,16 @@ fi
 EVERGREEN_API="https://evergreen.mongodb.com/api"
 MAX_RETRIES="${EVERGREEN_MAX_RETRIES:-3}"
 
-BUILD_IDS=($(curl -s -H "Api-User: $EVERGREEN_USER" -H "Api-Key: $EVERGREEN_API_KEY" $EVERGREEN_API/rest/v2/versions/$VERSION | jq -r '.build_variants_status[] | .build_id'))
+# shellcheck disable=SC2207
+BUILD_IDS=($(curl -s -H "Api-User: $EVERGREEN_USER" -H "Api-Key: $EVERGREEN_API_KEY" $EVERGREEN_API/rest/v2/versions/"$VERSION" | jq -r '.build_variants_status[] | .build_id'))
 
-for BUILD_ID in ${BUILD_IDS[@]}; do
+for BUILD_ID in "${BUILD_IDS[@]}"; do
   echo "Finding failed tasks in BUILD ID: $BUILD_ID"
-  TASK_IDS=($(curl -s -H "Api-User: $EVERGREEN_USER" -H "Api-Key: $EVERGREEN_API_KEY" $EVERGREEN_API/rest/v2/builds/$BUILD_ID/tasks | jq ".[] | select(.status == \"failed\" and .execution <= $MAX_RETRIES)" | jq -r '.task_id'))
+  # shellcheck disable=SC2207
+  TASK_IDS=($(curl -s -H "Api-User: $EVERGREEN_USER" -H "Api-Key: $EVERGREEN_API_KEY" $EVERGREEN_API/rest/v2/builds/"$BUILD_ID"/tasks | jq ".[] | select(.status == \"failed\" and .execution <= $MAX_RETRIES)" | jq -r '.task_id'))
 
-  for TASK_ID in ${TASK_IDS[@]}; do
+  for TASK_ID in "${TASK_IDS[@]}"; do
     echo "Retriggering TASK ID: $TASK_ID"
-    curl -H "Api-User: $EVERGREEN_USER" -H "Api-Key: $EVERGREEN_API_KEY" -X POST $EVERGREEN_API/rest/v2/tasks/$TASK_ID/restart
+    curl -H "Api-User: $EVERGREEN_USER" -H "Api-Key: $EVERGREEN_API_KEY" -X POST $EVERGREEN_API/rest/v2/tasks/"$TASK_ID"/restart
   done
 done
