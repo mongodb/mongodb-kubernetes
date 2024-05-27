a829860b2536e289172ec46d7a235f9346ebbc0c
Julien-Ben <33035980+Julien-Ben@users.noreply.github.com>
2024-05-27 15:51:06 +0200

    Improve readability (#3611)
    Improve readability of logger definition, and use a better format for logs

--- scripts/evergreen/release/base_logger.py
+++ scripts/evergreen/release/base_logger.py
@@ -3,24 +3,30 @@ import os
 import sys
 
 LOGLEVEL = os.environ.get("LOGLEVEL", "DEBUG").upper()
-logger = logging.getLogger("pipeline")
-logger.setLevel(LOGLEVEL)
-logger.propagate = False
-sonar_logger = logging.getLogger("sonar")
-sonar_logger.setLevel(LOGLEVEL)
-sonar_logger.propagate = False
 
-# Output Debug and Info logs to stdout, and above to stderr
+# Create handlers to output Debug and Info logs to stdout, and above to stderr
+# They are attached to each logger below
 stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setLevel(logging.DEBUG)
 stdout_handler.addFilter(lambda record: record.levelno <= logging.INFO)
 stderr_handler = logging.StreamHandler(sys.stderr)
 stderr_handler.setLevel(logging.WARNING)
 
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+# Format the logs
+formatter = logging.Formatter("%(levelname)-8s %(asctime)s [%(name)s]  %(message)s")
 stdout_handler.setFormatter(formatter)
 stderr_handler.setFormatter(formatter)
+
+# Pipeline logger
+logger = logging.getLogger("pipeline")
+logger.setLevel(LOGLEVEL)
+logger.propagate = False
 logger.addHandler(stdout_handler)
 logger.addHandler(stderr_handler)
+
+# Sonar logger
+sonar_logger = logging.getLogger("sonar")
+sonar_logger.setLevel(LOGLEVEL)
+sonar_logger.propagate = False
 sonar_logger.addHandler(stdout_handler)
 sonar_logger.addHandler(stderr_handler)
