8fcb44fcfc5113273f3c6531baf21782fa98034d
Sebastian Łaskawiec <slaskawi@users.noreply.github.com>
2024-05-31 13:55:59 +0200

    CLOUDP-212060 Static analysis for all files (#3602)
    # Summary
    
    During SSDLC Review meeting, Salman highlighted that Static Analysis
    tools should always check all the files, no just delta. This Pull
    Request put this into life.

--- docker/cluster-cleaner/scripts/delete-old-builder-pods.sh
+++ docker/cluster-cleaner/scripts/delete-old-builder-pods.sh
@@ -9,7 +9,7 @@ fi
 
 for pod in $(kubectl -n $NAMESPACE get pods -o name); do
     creation_time=$(kubectl -n $NAMESPACE get "${pod}" -o jsonpath='{.metadata.creationTimestamp}')
-    status=$(kubectl get $pod -o jsonpath='{.status.phase}' -n "${NAMESPACE}")
+    status=$(kubectl get "$pod" -o jsonpath='{.status.phase}' -n "${NAMESPACE}")
 
     if [[ "$status" != "Succeeded" ]] && [[ "$status" != "Failed" ]]; then
         # we don't remove pending tasks
