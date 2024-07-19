5e28ed0c2859982372cc0a01ead282a4a6200c7b
Nam Nguyen <nam.nguyen@mongodb.com>
2024-07-19 13:17:12 +0200

    fix error messages in docker configuration (#3691)
    # Summary
    
    That gets rid of those red herrings:
    ```
    [2024/07/18 09:46:39.328] find: '/home/ubuntu/.docker/config.json': No such file or directory
    
    [2024/07/18 09:46:39.328] grep: /home/ubuntu/.docker/config.json: No such file or directory
    ```
    
    ## Documentation changes
    
    * [ ] Add an entry to [release notes](.../RELEASE_NOTES.md).
    * [ ] When needed, make sure you create a new [DOCSP
    ticket](https://jira.mongodb.org/projects/DOCSP) that documents your
    change.
    
    ## Changes to CRDs
    
    * [ ] Add `slaskawi`(Sebastian) and `@giohan` (George) as reviewers.
    * [ ] Make sure any changes are reflected on `/public/samples`
    directory.

--- scripts/dev/configure_docker_auth.sh
+++ scripts/dev/configure_docker_auth.sh
@@ -18,24 +18,27 @@ remove_element() {
 # This is the script which performs docker authentication to different registries that we use (so far ECR and RedHat)
 # As the result of this login the ~/.docker/config.json will have all the 'auth' information necessary to work with docker registries
 
-if [[ "${RUNNING_IN_EVG:-""}" == "true" ]]; then
-  # when running locally we don't need to docker login all the time - we can do it once in 11 hours (ECR tokens expire each 12 hours)
-  if [[ -n "$(find ~/.docker/config.json -mmin -360 -type f)" ]] &&
-    grep "268558157000" -q ~/.docker/config.json; then
-    echo "Docker credentials are up to date - not performing the new login!"
-    exit
+if [[ -f ~/.docker/config.json ]]; then
+  if [[ "${RUNNING_IN_EVG:-""}" == "true" ]]; then
+    # when running locally we don't need to docker login all the time - we can do it once in 11 hours (ECR tokens expire each 12 hours)
+    if [[ -n "$(find ~/.docker/config.json -mmin -360 -type f)" ]] &&
+      grep "268558157000" -q ~/.docker/config.json; then
+      echo "Docker credentials are up to date - not performing the new login!"
+      exit
+    fi
+  fi
+
+  title "Performing docker login to ECR registries"
+
+  # There could be some leftovers on Evergreen
+  if grep -q "credsStore" ~/.docker/config.json; then
+    remove_element "credsStore"
+  fi
+  if grep -q "credHelpers" ~/.docker/config.json; then
+    remove_element "credHelpers"
   fi
 fi
 
-title "Performing docker login to ECR registries"
-
-# There could be some leftovers on Evergreen
-if grep -q "credsStore" ~/.docker/config.json; then
-  remove_element "credsStore"
-fi
-if grep -q "credHelpers" ~/.docker/config.json; then
-  remove_element "credHelpers"
-fi
 
 echo "$(aws --version)}"
 
@@ -49,6 +52,7 @@ if grep -q "credsStore" ~/.docker/config.json; then
   # login again to store the credentials into the config.json
   aws ecr get-login-password --region "us-east-1" | docker login --username AWS --password-stdin 268558157000.dkr.ecr.us-east-1.amazonaws.com
 fi
+
 aws ecr get-login-password --region "eu-west-1" | docker login --username AWS --password-stdin 268558157000.dkr.ecr.eu-west-1.amazonaws.com
 
 
