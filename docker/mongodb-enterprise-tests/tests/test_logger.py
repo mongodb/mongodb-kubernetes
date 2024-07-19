84fe051ca9e8b8e8ff85e6e9e1045c63cf9450b4
Julien-Ben <33035980+Julien-Ben@users.noreply.github.com>
2024-07-19 16:05:31 +0200

    CLOUDP-242980: Activate upgrade test for OM 6 (#3683)
    # Summary
    
    Following the previous PR for OM 7 :
    https://github.com/10gen/ops-manager-kubernetes/pull/3632
    
    Now that OM 6.0.24 is out we can activate the test for static containers
    with OM6 as well.

---
+++ docker/mongodb-enterprise-tests/tests/test_logger.py
@@ -0,0 +1,27 @@
+import logging
+import os
+import sys
+
+LOGLEVEL = os.environ.get("LOGLEVEL", "DEBUG").upper()
+
+# Create handlers to output Debug and Info logs to stdout, and above to stderr
+# They are attached to each logger below
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.addFilter(lambda record: record.levelno <= logging.INFO)
+stderr_handler = logging.StreamHandler(sys.stderr)
+stderr_handler.setLevel(logging.WARNING)
+
+# Format the logs
+formatter = logging.Formatter("%(levelname)-8s %(asctime)s [%(module)s]  %(message)s")
+stdout_handler.setFormatter(formatter)
+stderr_handler.setFormatter(formatter)
+
+
+def get_test_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(LOGLEVEL)
+    logger.propagate = False
+    logger.addHandler(stdout_handler)
+    logger.addHandler(stderr_handler)
+    return logger
