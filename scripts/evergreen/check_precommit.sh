feb49f7da7dc4a66a5bf4781ac5b12594fb64764
Julien-Ben <33035980+Julien-Ben@users.noreply.github.com>
2024-08-12 18:14:03 +0200

    Fix and improve check precommit (#3707)
    # Summary
    
    I noticed that recently we were pushing changes to master that were not
    formatted with the pre commit hook, meaning that the `lint_repo` step of
    the pipeline was broken, it was not failing when expected.
    For example, on this patch (on master), Black reformats 4 files, but the
    task succeeds:
    
    https://spruce.mongodb.com/task/ops_manager_kubernetes_go_unit_tests_lint_repo_e209a7c60b973a1c2a9e94ea1b2ffe55784f94f8_24_07_30_12_53_39/logs?execution=0
    
    This PR fixes this behaviour
    
    Example of the task failing as expected, on an improperly formatted
    code:
    
    https://spruce.mongodb.com/task/ops_manager_kubernetes_go_unit_tests_lint_repo_patch_de551ea1f8b4431a6999564e5674572e69b5877b_66b2075c1c4cce0007e0942f_24_08_06_11_22_09/logs?execution=0
    
    Execution after formatting is fixed (success):
    
    https://spruce.mongodb.com/task/ops_manager_kubernetes_go_unit_tests_lint_repo_patch_de551ea1f8b4431a6999564e5674572e69b5877b_66b208d8062e7d0007082479_24_08_06_11_28_38/logs?execution=0
    
    Note: Changes to files other than `check_precommit.sh` are only
    formatting.

--- scripts/evergreen/check_precommit.sh
+++ scripts/evergreen/check_precommit.sh
@@ -9,16 +9,37 @@ if [ -n "$(git diff --name-only --cached --diff-filter=AM)" ]; then
   exit 0
 fi
 
-export EVERGREEN_MODE=true
-source .githooks/pre-commit
+# Store the current state of the index and working directory
+initial_index_state=$(git diff --name-only --cached --diff-filter=AM)
 
-# shellcheck disable=SC2317
-if [ -z "$(git diff --name-only --cached --diff-filter=AM)" ]; then
+export EVERGREEN_MODE=true
+
+# Capturing the output avoids to stop if an error occurs
+pre_commit_output=$(source .githooks/pre-commit) || true
+echo "$pre_commit_output"
+echo "Pre-commit hook has completed."
+
+# Stage any changes made by the pre-commit hook
+git add -u
+
+# Store the new state of the index and working directory
+final_index_state=$(git diff --name-only --cached --diff-filter=AM)
+
+# Check if there are differences between the initial and final states
+if [ "$initial_index_state" != "$final_index_state" ]; then
+  echo "Initial index state:"
+  echo "$initial_index_state"
+
+  echo "Final index state:"
+  echo "$final_index_state"
+
+  echo "We have files that differ after running pre-commit, please run the pre-commit locally"
+  echo "Full diff: "
+  git diff --cached --diff-filter=AM
+  echo "The following files differ: "
+  git diff --name-only --cached --diff-filter=AM
+  exit 1
+else
   echo "No changes detected, clean state"
   exit 0
-else
-  echo "We have files to be committed, please run the pre-commit locally"
-  echo "The following files differ: "
-  git diff --cached --diff-filter=AM
-  exit 1
 fi
