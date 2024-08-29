d34abd3b3b1379b8b4eb863d223ed262d514ec0d
Nam Nguyen <nam.nguyen@mongodb.com>
2024-08-29 11:05:29 +0200

    CLOUDP-269317 - process licenses for multicluster tool as well and refactor code (#3739)
    # Summary
    
    stderr of the current run
    
    `multicluster`
    ```
    W0828 18:04:24.743183   61125 library.go:101] "github.com/modern-go/reflect2" contains non-Go code that can't be inspected for further dependencies:
    /Users/nam.nguyen/go/pkg/mod/github.com/modern-go/reflect2@v1.0.2/reflect2_amd64.s
    /Users/nam.nguyen/go/pkg/mod/github.com/modern-go/reflect2@v1.0.2/relfect2_mips64x.s
    /Users/nam.nguyen/go/pkg/mod/github.com/modern-go/reflect2@v1.0.2/relfect2_mipsx.s
    /Users/nam.nguyen/go/pkg/mod/github.com/modern-go/reflect2@v1.0.2/relfect2_ppc64x.s
    W0828 18:04:26.655236   61125 library.go:101] "golang.org/x/sys/unix" contains non-Go code that can't be inspected for further dependencies:
    /Users/nam.nguyen/go/pkg/mod/golang.org/x/sys@v0.20.0/unix/asm_linux_amd64.s
    W0828 18:04:29.403164   61125 library.go:276] module github.com/10gen/ops-manager-kubernetes/multi has empty version, defaults to HEAD. The license URL may be incorrect. Please verify!
    ```
    
    ## Documentation changes
    
    * [ ] Add an entry to [release notes](.../RELEASE_NOTES.md).
    * [ ] When needed, make sure you create a new [DOCSP
    ticket](https://jira.mongodb.org/projects/DOCSP) that documents your
    change.
    
    ## Changes to CRDs
    
    * [ ] Add `slaskawi`(Sebastian) and `@giohan` (George) as reviewers.
    * [ ] Make sure any changes are reflected on `/public/samples`
    directory.

--- scripts/evergreen/update_licenses.sh
+++ scripts/evergreen/update_licenses.sh
@@ -5,9 +5,31 @@ source scripts/dev/set_env_context.sh
 
 go install github.com/google/go-licenses@v1.6.0
 
+# Define the root of the repo and the scripts directory
+REPO_DIR=$(dirname "$(dirname "$(dirname "$(readlink -f "$0")")")")
 SCRIPTS_DIR=$(dirname "$(readlink -f "$0")")
 
-PATH=$GOPATH/bin:$PATH GOOS=linux GOARCH=amd64 go-licenses report . --template "$SCRIPTS_DIR/update_licenses.tpl" > licenses_full.csv 2> licenses_stderr
+# Function to process licenses in a given directory
+process_licenses() {
+    local DIR="$1"
 
-# shellcheck disable=SC2002
-cat licenses_full.csv | grep -v 10gen | grep -v "github.com/mongodb" | grep -v "^golang.org"  | sort > licenses.csv
+    echo "Processing licenses for module: $DIR"
+
+    if ! cd "$DIR"; then
+        echo "Failed to change directory to $DIR"
+        return 1
+    fi
+
+    PATH=$GOPATH/bin:$PATH GOOS=linux GOARCH=amd64 go-licenses report . --template "$SCRIPTS_DIR/update_licenses.tpl" > licenses_full.csv 2> licenses_stderr  || true
+
+    # Filter and sort the licenses report
+    grep -v 10gen licenses_full.csv | grep -v "github.com/mongodb" | grep -v "^golang.org" | sort > licenses.csv || true
+
+    # Return to the repo root directory
+    cd "$REPO_DIR" || exit
+}
+
+process_licenses "$REPO_DIR"
+process_licenses "$REPO_DIR/public/tools/multicluster"
+
+echo "License processing complete for all modules."
