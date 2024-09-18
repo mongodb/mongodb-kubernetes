2662b86945a1b6692a9fbe566b962860e399fe46
Nam Nguyen <nam.nguyen@mongodb.com>
2024-09-18 15:52:27 +0200

    CLOUDP-272269 - remove ubuntu from appdb leftovers (#3797)
    # Summary
    
    We don't use or support ubuntu images.

--- docker/mongodb-enterprise-appdb-database/build_and_push_appdb_database_images.sh
+++ docker/mongodb-enterprise-appdb-database/build_and_push_appdb_database_images.sh
@@ -19,19 +19,6 @@ append_missing_version() {
 }
 
 for version in $_44_versions; do
-    echo "Building version ${version}"
-    docker build \
-        -f 4.4/ubuntu/Dockerfile \
-        --build-arg MONGO_PACKAGE=mongodb-enterprise \
-        --build-arg "MONGO_VERSION=${version}" \
-        --build-arg MONGO_REPO=repo.mongodb.com \
-        -t "quay.io/mongodb/mongodb-enterprise-appdb-database:${version}-ent-${build_id}" \
-        -t "quay.io/mongodb/mongodb-enterprise-appdb-database:${version}-ent" .
-    append_missing_version "${version}" "ubuntu"
-
-    docker push "quay.io/mongodb/mongodb-enterprise-appdb-database:${version}-ent-${build_id}"
-    docker push "quay.io/mongodb/mongodb-enterprise-appdb-database:${version}-ent"
-
     docker build \
         -f 4.4/ubi/Dockerfile \
         --build-arg MONGO_PACKAGE=mongodb-enterprise \
@@ -46,19 +33,6 @@ for version in $_44_versions; do
 done
 
 for version in $_50_versions; do
-    echo "Building version ${version}"
-    docker build \
-        -f 5.0/ubuntu/Dockerfile \
-        --build-arg MONGO_PACKAGE=mongodb-enterprise \
-        --build-arg "MONGO_VERSION=${version}" \
-        --build-arg MONGO_REPO=repo.mongodb.com \
-        -t "quay.io/mongodb/mongodb-enterprise-appdb-database:${version}-ent-${build_id}" \
-        -t "quay.io/mongodb/mongodb-enterprise-appdb-database:${version}-ent" .
-    append_missing_version "${version}" "ubuntu"
-
-    docker push "quay.io/mongodb/mongodb-enterprise-appdb-database:${version}-ent-${build_id}"
-    docker push "quay.io/mongodb/mongodb-enterprise-appdb-database:${version}-ent"
-
     docker build \
         -f 5.0/ubi/Dockerfile \
         --build-arg MONGO_PACKAGE=mongodb-enterprise \
