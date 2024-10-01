7e844992a85952873102db472104c6e6159d9204
mms-build-account <mms-admin+pullonly@10gen.com>
2024-10-01 15:56:36 -0400

    Kubernetes Enterprise Operator Release 1.28.0 (#3829)
    ## Kubernetes Enterprise Operator Release 1.28.0
    
    This release patch has been created and it should be reviewed now.
    
    You can add new commits to this patch if needed. After changes have been
    reviewed, merge this PR for PCT to continue the release process.
    
    ### Next steps
    
    1. Find the SHA of the merge commit; resulting of merging this patch. 2.
    Execute `REDACTED
    k8s ok-to-publish CLOUDP-265113`
    
    ---------
    
    Co-authored-by: Mircea Cosbuc <mircea.cosbuc@mongodb.com>

--- docker/mongodb-enterprise-tests/tests/tls/tls_requiressl_upgrade.py
+++ docker/mongodb-enterprise-tests/tests/tls/tls_requiressl_upgrade.py
@@ -5,7 +5,7 @@ from kubetester.certs import (
     create_agent_tls_certs,
     create_mongodb_tls_certs,
 )
-from kubetester.kubetester import KubernetesTester
+from kubetester.kubetester import KubernetesTester, ensure_ent_version
 from kubetester.kubetester import fixture as load_fixture
 from kubetester.kubetester import skip_if_local
 from kubetester.mongodb import MongoDB, Phase
@@ -20,15 +20,16 @@ def server_certs(issuer: str, namespace: str):
 
 
 @pytest.fixture(scope="module")
-def mdb(namespace: str, server_certs: str, issuer_ca_configmap: str) -> MongoDB:
+def mdb(namespace: str, server_certs: str, issuer_ca_configmap: str, custom_mdb_version: str) -> MongoDB:
     res = MongoDB.from_yaml(load_fixture("test-tls-base-rs-require-ssl-upgrade.yaml"), namespace=namespace)
+    res.set_version(ensure_ent_version(custom_mdb_version))
     res["spec"]["security"] = {"tls": {"ca": issuer_ca_configmap}}
     return res.create()
 
 
 @pytest.mark.e2e_replica_set_tls_require_upgrade
 def test_replica_set_running(mdb: MongoDB):
-    mdb.assert_reaches_phase(Phase.Running, timeout=400)
+    mdb.assert_reaches_phase(Phase.Running)
 
 
 @pytest.mark.e2e_replica_set_tls_require_upgrade
@@ -41,7 +42,7 @@ def test_enables_TLS_replica_set(mdb: MongoDB, server_certs: str, issuer_ca_conf
     mdb.load()
     mdb["spec"]["security"] = {"tls": {"enabled": True}, "ca": issuer_ca_configmap}
     mdb.update()
-    mdb.assert_reaches_phase(Phase.Running, timeout=400)
+    mdb.assert_reaches_phase(Phase.Running)
 
 
 @pytest.mark.e2e_replica_set_tls_require_upgrade
