f9a986dcf40003252067cbf517869872f4240123
Maciej Karaś <6159874+MaciejKaras@users.noreply.github.com>
2024-11-05 14:46:19 +0100

    CLOUDP-278184 - e2e_sharded_cluster_statefulset_status test multi-cluster adoption (#3901)
    # Summary
    
    CLOUDP-278184 - e2e_sharded_cluster_statefulset_status test
    multi-cluster adoption
    
    Additional fixes:
    - merged statuses for statefulsets in controller so that we will have
    multiple resources in `status.resourcesNotReady` array
    
    ## Documentation changes
    
    * [ ] Add an entry to [release notes](.../RELEASE_NOTES.md).
    * [ ] When needed, make sure you create a new [DOCSP
    ticket](https://jira.mongodb.org/projects/DOCSP) that documents your
    change.
    
    ## Changes to CRDs
    
    * [ ] Add `slaskawi`(Sebastian) and `@giohan` (George) as reviewers.
    * [ ] Make sure any changes are reflected on `/public/samples`
    directory.

--- docker/mongodb-enterprise-tests/tests/shardedcluster/sharded_cluster_statefulset_status.py
+++ docker/mongodb-enterprise-tests/tests/shardedcluster/sharded_cluster_statefulset_status.py
@@ -1,20 +1,16 @@
+import re
+
+from kubetester import try_load
+from kubetester.kubetester import ensure_ent_version
 from kubetester.kubetester import fixture as yaml_fixture
 from kubetester.mongodb import MongoDB, Phase
 from kubetester.operator import Operator
 from pytest import fixture, mark
-
-
-@fixture(scope="module")
-def sharded_cluster(namespace: str, custom_mdb_version: str) -> MongoDB:
-    resource = MongoDB.from_yaml(
-        yaml_fixture("sharded-cluster-single.yaml"),
-        namespace=namespace,
-        name="sharded-cluster-status",
-    )
-    resource.set_version(custom_mdb_version)
-    resource["spec"]["shardCount"] = 2
-    return resource.create()
-
+from tests.conftest import is_multi_cluster
+from tests.shardedcluster.conftest import (
+    enable_multi_cluster_deployment,
+    get_member_cluster_clients_using_cluster_mapping,
+)
 
 """
 This test checks the 'status.resourcesNotReady' element during sharded cluster reconciliation. It's expected to 
@@ -23,49 +19,97 @@ shard 1, mongos.
 """
 
 
-@mark.e2e_sharded_cluster_statefulset_status
-def test_install_operator(default_operator: Operator):
-    default_operator.assert_is_running()
+@fixture(scope="function")
+def sc(namespace: str, custom_mdb_version: str) -> MongoDB:
+    resource = MongoDB.from_yaml(
+        yaml_fixture("sharded-cluster-single.yaml"),
+        namespace=namespace,
+        name="sharded-cluster-status",
+    )
+
+    if try_load(resource):
+        return resource
+
+    resource.set_version(ensure_ent_version(custom_mdb_version))
+    resource.set_architecture_annotation()
+
+    resource["spec"]["shardCount"] = 2
+
+    if is_multi_cluster():
+        enable_multi_cluster_deployment(
+            resource=resource,
+            shard_members_array=[1, 1, 1],
+            mongos_members_array=[1, 1, None],
+            configsrv_members_array=[None, 1, 1],
+        )
+
+    return resource.update()
 
 
 @mark.e2e_sharded_cluster_statefulset_status
-def test_config_srv_reaches_pending_phase(sharded_cluster: MongoDB):
-    cluster_reaches_not_ready(sharded_cluster, sharded_cluster.name + "-config")
+def test_install_operator(operator: Operator):
+    operator.assert_is_running()
 
 
 @mark.e2e_sharded_cluster_statefulset_status
-def test_first_shard_reaches_pending_phase(sharded_cluster: MongoDB):
-    cluster_reaches_not_ready(sharded_cluster, sharded_cluster.name + "-0")
+def test_config_srv_reaches_pending_phase(sc: MongoDB):
+    for cluster_member_client in get_member_cluster_clients_using_cluster_mapping(sc.name, sc.namespace):
+        if sc.config_srv_members_in_cluster(cluster_member_client.cluster_name) > 0:
+            cluster_idx = cluster_member_client.cluster_index
+            configsrv_sts_name = sc.config_srv_statefulset_name(cluster_idx)
+            cluster_reaches_not_ready(sc, configsrv_sts_name)
 
 
 @mark.e2e_sharded_cluster_statefulset_status
-def test_second_shard_reaches_pending_phase(sharded_cluster: MongoDB):
-    cluster_reaches_not_ready(sharded_cluster, sharded_cluster.name + "-1")
+def test_first_shard_reaches_pending_phase(sc: MongoDB):
+    for cluster_member_client in get_member_cluster_clients_using_cluster_mapping(sc.name, sc.namespace):
+        if sc.shard_members_in_cluster(cluster_member_client.cluster_name) > 0:
+            cluster_idx = cluster_member_client.cluster_index
+            shard0_sts_name = sc.shard_statefulset_name(0, cluster_idx)
+            cluster_reaches_not_ready(sc, shard0_sts_name)
 
 
 @mark.e2e_sharded_cluster_statefulset_status
-def test_mongos_reaches_pending_phase(sharded_cluster: MongoDB):
-    cluster_reaches_not_ready(sharded_cluster, sharded_cluster.name + "-mongos")
+def test_second_shard_reaches_pending_phase(sc: MongoDB):
+    for cluster_member_client in get_member_cluster_clients_using_cluster_mapping(sc.name, sc.namespace):
+        if sc.shard_members_in_cluster(cluster_member_client.cluster_name) > 0:
+            cluster_idx = cluster_member_client.cluster_index
+            shard1_sts_name = sc.shard_statefulset_name(1, cluster_idx)
+            cluster_reaches_not_ready(sc, shard1_sts_name)
 
 
 @mark.e2e_sharded_cluster_statefulset_status
-def test_sharded_cluster_reaches_running_phase(sharded_cluster: MongoDB):
-    sharded_cluster.assert_reaches_phase(Phase.Running, timeout=100)
-    assert sharded_cluster.get_status_resources_not_ready() is None
+def test_mongos_reaches_pending_phase(sc: MongoDB):
+    for cluster_member_client in get_member_cluster_clients_using_cluster_mapping(sc.name, sc.namespace):
+        if sc.mongos_members_in_cluster(cluster_member_client.cluster_name) > 0:
+            cluster_idx = cluster_member_client.cluster_index
+            mongos_sts_name = sc.mongos_statefulset_name(cluster_idx)
+            cluster_reaches_not_ready(sc, mongos_sts_name)
 
 
-def cluster_reaches_not_ready(sharded_cluster: MongoDB, sts_name: str):
+@mark.e2e_sharded_cluster_statefulset_status
+def test_sharded_cluster_reaches_running_phase(sc: MongoDB):
+    sc.assert_reaches_phase(Phase.Running, timeout=1000)
+    assert sc.get_status_resources_not_ready() is None
+
+
+def cluster_reaches_not_ready(sc: MongoDB, sts_name: str):
     """This function waits until the sharded cluster status gets 'resource_not_ready' element for the specified
     StatefulSet"""
 
-    def resource_not_ready(s: MongoDB):
+    def resource_not_ready(s: MongoDB) -> bool:
         if s.get_status_resources_not_ready() is None:
             return False
-        return s.get_status_resources_not_ready()[0]["name"] == sts_name
 
-    sharded_cluster.wait_for(resource_not_ready, timeout=150, should_raise=True)
-    sharded_cluster.assert_status_resource_not_ready(
-        sts_name,
-        msg_regexp="Not all the Pods are ready \(wanted: 1.*\)",
-    )
-    assert sharded_cluster.get_status_phase() == Phase.Pending
+        for idx, resource in enumerate(s.get_status_resources_not_ready()):
+            if resource["name"] == sts_name:
+                assert resource["kind"] == "StatefulSet"
+                assert re.search("Not all the Pods are ready \(wanted: 1.*\)", resource["message"]) is not None
+
+                return True
+
+        return False
+
+    sc.wait_for(resource_not_ready, timeout=150, should_raise=True)
+
+    assert sc.get_status_phase() == Phase.Pending
