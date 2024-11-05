40d10a29c9c463defe1780aadec64203feada37e
Maciej Karaś <6159874+MaciejKaras@users.noreply.github.com>
2024-11-05 14:25:35 +0100

    CLOUDP-278184 - e2e_sharded_cluster_secret test multi-cluster adoption (#3893)
    # Summary
    
    CLOUDP-278184 - e2e_sharded_cluster_secret test multi-cluster adoption
    
    `e2e_sharded_cluster_secret` looks like a subset of
    `e2e_sharded_cluster_recovery` ->
    
    https://github.com/10gen/ops-manager-kubernetes/blob/c1e4e11773f4b47b69ec38a2ee013b92a3ab5a14/docker/mongodb-enterprise-tests/tests/shardedcluster/sharded_cluster_secret.py#L22-L39
    
    https://github.com/10gen/ops-manager-kubernetes/blob/c1e4e11773f4b47b69ec38a2ee013b92a3ab5a14/docker/mongodb-enterprise-tests/tests/shardedcluster/sharded_cluster_recovery.py#L22-L44
    
    ## Documentation changes
    
    * [ ] Add an entry to [release notes](.../RELEASE_NOTES.md).
    * [ ] When needed, make sure you create a new [DOCSP
    ticket](https://jira.mongodb.org/projects/DOCSP) that documents your
    change.
    
    ## Changes to CRDs
    
    * [ ] Add `slaskawi`(Sebastian) and `@giohan` (George) as reviewers.
    * [ ] Make sure any changes are reflected on `/public/samples`
    directory.

--- docker/mongodb-enterprise-tests/tests/shardedcluster/sharded_cluster_secret.py
+++ docker/mongodb-enterprise-tests/tests/shardedcluster/sharded_cluster_secret.py
@@ -1,25 +1,39 @@
-from kubernetes.client import V1Secret
-from kubetester.kubetester import KubernetesTester
-from kubetester.kubetester import fixture as load_fixture
+from kubetester import try_load
+from kubetester.kubetester import KubernetesTester, ensure_ent_version
+from kubetester.kubetester import fixture as yaml_fixture
 from kubetester.mongodb import MongoDB, Phase
 from kubetester.operator import Operator
 from pytest import fixture, mark
+from tests.conftest import is_multi_cluster
+from tests.shardedcluster.conftest import enable_multi_cluster_deployment
 
 
-@fixture(scope="module")
-def mdb(namespace: str, custom_mdb_version: str) -> MongoDB:
-    resource = MongoDB.from_yaml(load_fixture("sharded-cluster-single.yaml"), namespace=namespace)
-    resource.set_version(custom_mdb_version)
+@fixture(scope="function")
+def sc(namespace: str, custom_mdb_version: str) -> MongoDB:
+    resource = MongoDB.from_yaml(
+        yaml_fixture("sharded-cluster-single.yaml"),
+        namespace=namespace,
+    )
+
+    if try_load(resource):
+        return resource
+
+    resource.set_version(ensure_ent_version(custom_mdb_version))
+    resource.set_architecture_annotation()
+
+    if is_multi_cluster():
+        enable_multi_cluster_deployment(resource)
+
     return resource.update()
 
 
 @mark.e2e_sharded_cluster_secret
-def test_install_operator(default_operator: Operator):
-    default_operator.assert_is_running()
+def test_install_operator(operator: Operator):
+    operator.assert_is_running()
 
 
 @mark.e2e_sharded_cluster_secret
-class TestShardedClusterListensSecret(KubernetesTester):
+class TestShardedClusterListensSecret:
     """
     name: ShardedCluster tracks configmap changes
     description: |
@@ -28,12 +42,11 @@ class TestShardedClusterListensSecret(KubernetesTester):
       flag locally as secret must be recreated
     """
 
-    def test_create_sharded_cluster(self, mdb: MongoDB):
-        mdb.assert_reaches_phase(Phase.Running)
+    def test_create_sharded_cluster(self, sc: MongoDB):
+        sc.assert_reaches_phase(Phase.Running, timeout=1000)
 
-    def test_patch_config_map(self, mdb: MongoDB):
-        secret = V1Secret(string_data={"publicApiKey": "wrongKey"})
-        self.clients("corev1").patch_namespaced_secret("my-credentials", self.get_namespace(), secret)
+    def test_patch_config_map(self, sc: MongoDB):
+        KubernetesTester.update_secret(sc.namespace, "my-credentials", {"publicApiKey": "wrongKey"})
 
         print('Patched the Secret - changed publicApiKey to "wrongKey"')
-        mdb.assert_reaches_phase(Phase.Failed, timeout=20)
+        sc.assert_reaches_phase(Phase.Failed, timeout=20)
