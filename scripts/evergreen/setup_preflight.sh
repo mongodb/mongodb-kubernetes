3daa492a717936ac253fed1613d3e2a5158923eb
Yavor Georgiev <fealebenpae@users.noreply.github.com>
2024-11-08 15:10:43 +0100

    Bump openshift-preflight version (#3914)
    # Summary
    
    The Pyxis API doesn't accept submissions from preflight 1.10.0 anymore
    so the periodic build preflight wasn't actually uploading anything. See
    https://spruce.mongodb.com/task/ops_manager_kubernetes_preflight_images_preflight_images_patch_e20227cbf2fc05f07df83110d1d4f8047207884c_672cde61088a9e0007987f51_24_11_07_15_36_10/logs
    for the exact error output.
    
    ## Documentation changes
    
    * [ ] Add an entry to [release notes](.../RELEASE_NOTES.md).
    * [ ] When needed, make sure you create a new [DOCSP
    ticket](https://jira.mongodb.org/projects/DOCSP) that documents your
    change.
    
    ## Changes to CRDs
    
    * [ ] Add `slaskawi`(Sebastian) and `@giohan` (George) as reviewers.
    * [ ] Make sure any changes are reflected on `/public/samples`
    directory.

--- scripts/evergreen/setup_preflight.sh
+++ scripts/evergreen/setup_preflight.sh
@@ -8,7 +8,7 @@ bindir="${workdir:?}/bin"
 mkdir -p "${bindir}"
 
 echo "Downloading preflight binary"
-preflight_version="1.10.0"
+preflight_version="1.10.2"
 curl -s --retry 3 -o preflight -LO "https://github.com/redhat-openshift-ecosystem/openshift-preflight/releases/download/${preflight_version}/preflight-linux-amd64"
 chmod +x preflight
 mv preflight "${bindir}"
