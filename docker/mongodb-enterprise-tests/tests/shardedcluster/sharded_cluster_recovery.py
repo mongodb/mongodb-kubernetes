c794e0266db3d2429dba4f6dffa892244706517a
Maciej Karaś <6159874+MaciejKaras@users.noreply.github.com>
2024-11-11 21:58:09 +0100

    CLOUDP-278184 - e2e_sharded_cluster_recovery test multi-cluster adoption (#3889)
    # Summary
    
    CLOUDP-278184 - e2e_sharded_cluster_recovery test multi-cluster adoption
    
    ## Documentation changes
    
    * [ ] Add an entry to [release notes](.../RELEASE_NOTES.md).
    * [ ] When needed, make sure you create a new [DOCSP
    ticket](https://jira.mongodb.org/projects/DOCSP) that documents your
    change.
    
    ## Changes to CRDs
    
    * [ ] Add `slaskawi`(Sebastian) and `@giohan` (George) as reviewers.
    * [ ] Make sure any changes are reflected on `/public/samples`
    directory.

--- docker/mongodb-enterprise-tests/tests/shardedcluster/sharded_cluster_recovery.py
+++ docker/mongodb-enterprise-tests/tests/shardedcluster/sharded_cluster_recovery.py
@@ -1,25 +1,41 @@
-from kubernetes.client import V1Secret
-from kubetester.kubetester import KubernetesTester
-from kubetester.kubetester import fixture as load_fixture
+from kubetester import try_load
+from kubetester.kubetester import KubernetesTester, ensure_ent_version
+from kubetester.kubetester import fixture as yaml_fixture
+from kubetester.kubetester import is_multi_cluster
 from kubetester.mongodb import MongoDB, Phase
 from kubetester.operator import Operator
 from pytest import fixture, mark
+from tests.shardedcluster.conftest import enable_multi_cluster_deployment
 
 
-@fixture(scope="module")
-def mdb(namespace: str, custom_mdb_version: str) -> MongoDB:
-    resource = MongoDB.from_yaml(load_fixture("sharded-cluster-single.yaml"), namespace=namespace)
-    resource.set_version(custom_mdb_version)
+@fixture(scope="function")
+def sc(namespace: str, custom_mdb_version: str) -> MongoDB:
+    resource = MongoDB.from_yaml(yaml_fixture("sharded-cluster-single.yaml"), namespace=namespace)
+
+    if try_load(resource):
+        return resource
+
+    resource.set_version(ensure_ent_version(custom_mdb_version))
+    resource.set_architecture_annotation()
+
+    if is_multi_cluster():
+        enable_multi_cluster_deployment(
+            resource=resource,
+            shard_members_array=[1, 1, 1],
+            mongos_members_array=[1, None, None],
+            configsrv_members_array=[None, 1, None],
+        )
+
     return resource.update()
 
 
 @mark.e2e_sharded_cluster_recovery
-def test_install_operator(default_operator: Operator):
-    default_operator.assert_is_running()
+def test_install_operator(operator: Operator):
+    operator.assert_is_running()
 
 
 @mark.e2e_sharded_cluster_recovery
-class TestShardedClusterRecoversBadOmConfiguration(KubernetesTester):
+class TestShardedClusterRecoversBadOmConfiguration:
     """
     name: Sharded cluster broken OM connection
     description: |
@@ -27,18 +43,21 @@ class TestShardedClusterRecoversBadOmConfiguration(KubernetesTester):
       Then the secret is fixed and the standalone is expected to reach good state eventually
     """
 
-    def test_sharded_cluster_reaches_failed_state(self, mdb: MongoDB):
-        secret = V1Secret(string_data={"publicApiKey": "wrongKey"})
-        self.clients("corev1").patch_namespaced_secret("my-credentials", self.get_namespace(), secret)
+    def test_create_sharded_cluster(self, sc: MongoDB):
+        sc.assert_reaches_phase(Phase.Running, timeout=1000)
 
-        mdb.assert_reaches_phase(Phase.Failed, timeout=20)
+    def test_sharded_cluster_reaches_failed_state(self, sc: MongoDB):
+        secret_data = {"publicApiKey": "wrongKey"}
+        KubernetesTester.update_secret(sc.namespace, "my-credentials", secret_data)
 
-        mdb.load()
-        assert "You are not authorized for this resource" in mdb["status"]["message"]
+        sc.assert_reaches_phase(Phase.Failed, timeout=20)
 
-    def test_recovery(self, mdb: MongoDB):
-        secret = V1Secret(string_data={"publicApiKey": self.get_om_api_key()})
-        self.clients("corev1").patch_namespaced_secret("my-credentials", self.get_namespace(), secret)
+        sc.load()
+        assert "You are not authorized for this resource" in sc["status"]["message"]
+
+    def test_recovery(self, sc: MongoDB):
+        secret_data = {"publicApiKey": KubernetesTester.get_om_api_key()}
+        KubernetesTester.update_secret(sc.namespace, "my-credentials", secret_data)
 
         # We need to ignore errors here because the CM change can be faster than the check
-        mdb.assert_reaches_phase(Phase.Running, ignore_errors=True)
+        sc.assert_reaches_phase(Phase.Running, ignore_errors=True)
