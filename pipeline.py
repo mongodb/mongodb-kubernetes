mongodb/mongodb-kubernetes

commit 251a0313aa061e57a5c167dc8fcdb9a89d6daccb

Parallelize the daily images build (#3883)
# Summary

This brings down the `periodic_agent_build` task's runtime to just over
half an hour from more than six. We also drop the 1.25 agent image combo
because we no longer support it after releasing 1.28.

## Documentation changes

* [ ] Add an entry to [release notes](.../RELEASE_NOTES.md).
* [ ] When needed, make sure you create a new [DOCSP
ticket](https://jira.mongodb.org/projects/DOCSP) that documents your
change.

## Changes to CRDs

* [ ] Add `slaskawi`(Sebastian) and `@giohan` (George) as reviewers.
* [ ] Make sure any changes are reflected on `/public/samples`
directory.

pipeline.py
@@ -628,13 +628,7 @@ If the context image supports both ARM and AMD architectures, both will be built
 """
 
 
-def build_image_daily(
-    image_name: str,  # corresponds to the image_name in the release.json
-    min_version: str = None,
-    max_version: str = None,
-):
-    """Builds a daily image."""
-
+def build_image_daily_parallel_worker(build_configuration: BuildConfiguration, args: Dict[str, str]):
     def get_architectures_set(build_configuration, args):
         """Determine the set of architectures to build for"""
         arch_set = set(build_configuration.architecture) if build_configuration.architecture else set()
@@ -662,6 +656,50 @@ def build_image_daily(
             for tag in tags:
                 create_and_push_manifest(registry + args["ubi_suffix"], tag)
 
+    arch_set = get_architectures_set(build_configuration, args)
+    if arch_set == {"amd64", "arm64"}:
+        # We need to release the non context amd64 and arm64 images first before we can create the sbom
+        for arch in arch_set:
+            # Suffix to append to images name for multi-arch (see usage in daily.yaml inventory)
+            args["architecture_suffix"] = f"-{arch}"
+            args["platform"] = arch
+            sonar_build_image(
+                "image-daily-build",
+                build_configuration,
+                args,
+                inventory="inventories/daily.yaml",
+                with_sbom=False,
+            )
+            if build_configuration.sign:
+                sign_image_in_repositories(args, arch)
+        create_and_push_manifests(args)
+        for arch in arch_set:
+            args["architecture_suffix"] = f"-{arch}"
+            args["platform"] = arch
+            produce_sbom(build_configuration, args)
+        if build_configuration.sign:
+            sign_image_in_repositories(args)
+    else:
+        # No suffix for single arch images
+        args["architecture_suffix"] = ""
+        args["platform"] = "amd64"
+        sonar_build_image(
+            "image-daily-build",
+            build_configuration,
+            args,
+            inventory="inventories/daily.yaml",
+        )
+        if build_configuration.sign:
+            sign_image_in_repositories(args)
+
+
+def build_image_daily(
+    image_name: str,  # corresponds to the image_name in the release.json
+    min_version: str = None,
+    max_version: str = None,
+):
+    """Builds a daily image."""
+
     def inner(build_configuration: BuildConfiguration):
         supported_versions = get_supported_version_for_image_matrix_handling(image_name)
         variants = get_supported_variants_for_image(image_name)
@@ -670,56 +708,27 @@ def build_image_daily(
         args["build_id"] = build_id()
         logger.info("Supported Versions for {}: {}".format(image_name, supported_versions))
 
-        completed_versions = set()
         filtered_versions = get_versions_to_rebuild(supported_versions, min_version, max_version)
 
-        for version in filtered_versions:
-            build_configuration = copy.deepcopy(build_configuration)
-            if build_configuration.include_tags is None:
-                build_configuration.include_tags = []
-            build_configuration.include_tags.extend(variants)
+        tasks_queue = Queue()
+        max_workers = 1
+        if build_configuration.parallel:
+            max_workers = None
+            if build_configuration.parallel_factor > 0:
+                max_workers = build_configuration.parallel_factor
+        with ProcessPoolExecutor(max_workers=max_workers) as executor:
+            logger.info(f"running with factor of {max_workers}")
+            for version in filtered_versions:
+                build_configuration = copy.deepcopy(build_configuration)
+                if build_configuration.include_tags is None:
+                    build_configuration.include_tags = []
+                build_configuration.include_tags.extend(variants)
 
-            logger.info("Rebuilding {} with variants {}".format(version, variants))
-            args["release_version"] = version
+                logger.info("Rebuilding {} with variants {}".format(version, variants))
+                args["release_version"] = version
 
-            arch_set = get_architectures_set(build_configuration, args)
-
-            if version not in completed_versions:
-                if arch_set == {"amd64", "arm64"}:
-                    # We need to release the non context amd64 and arm64 images first before we can create the sbom
-                    for arch in arch_set:
-                        # Suffix to append to images name for multi-arch (see usage in daily.yaml inventory)
-                        args["architecture_suffix"] = f"-{arch}"
-                        args["platform"] = arch
-                        sonar_build_image(
-                            "image-daily-build",
-                            build_configuration,
-                            args,
-                            inventory="inventories/daily.yaml",
-                            with_sbom=False,
-                        )
-                        if build_configuration.sign:
-                            sign_image_in_repositories(args, arch)
-                    create_and_push_manifests(args)
-                    for arch in arch_set:
-                        args["architecture_suffix"] = f"-{arch}"
-                        args["platform"] = arch
-                        produce_sbom(build_configuration, args)
-                    if build_configuration.sign:
-                        sign_image_in_repositories(args)
-                else:
-                    # No suffix for single arch images
-                    args["architecture_suffix"] = ""
-                    args["platform"] = "amd64"
-                    sonar_build_image(
-                        "image-daily-build",
-                        build_configuration,
-                        args,
-                        inventory="inventories/daily.yaml",
-                    )
-                    if build_configuration.sign:
-                        sign_image_in_repositories(args)
-                completed_versions.add(version)
+                tasks_queue.put(executor.submit(build_image_daily_parallel_worker, build_configuration, args))
+        queue_exception_handling(tasks_queue)
 
     return inner
 

config/manager/manager.yaml
@@ -109,8 +109,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.0.8502-1"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_1_8507_1
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.1.8507-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_107_0_1_8507_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:107.0.1.8507-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_1_8507_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.1.8507-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_1_8507_1_1_27_0
@@ -119,8 +117,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.1.8507-1_1.28.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_10_8627_1
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.10.8627-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_107_0_10_8627_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:107.0.10.8627-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_10_8627_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.10.8627-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_10_8627_1_1_27_0
@@ -129,8 +125,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.10.8627-1_1.28.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_11_8645_1
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.11.8645-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_107_0_11_8645_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:107.0.11.8645-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_11_8645_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.11.8645-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_11_8645_1_1_27_0
@@ -139,8 +133,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.11.8645-1_1.28.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_2_8531_1
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.2.8531-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_107_0_2_8531_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:107.0.2.8531-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_2_8531_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.2.8531-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_2_8531_1_1_27_0
@@ -149,8 +141,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.2.8531-1_1.28.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_3_8550_1
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.3.8550-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_107_0_3_8550_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:107.0.3.8550-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_3_8550_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.3.8550-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_3_8550_1_1_27_0
@@ -159,8 +149,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.3.8550-1_1.28.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_4_8567_1
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.4.8567-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_107_0_4_8567_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:107.0.4.8567-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_4_8567_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.4.8567-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_4_8567_1_1_27_0
@@ -169,8 +157,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.4.8567-1_1.28.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_6_8587_1
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.6.8587-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_107_0_6_8587_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:107.0.6.8587-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_6_8587_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.6.8587-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_6_8587_1_1_27_0
@@ -179,8 +165,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.6.8587-1_1.28.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_7_8596_1
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.7.8596-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_107_0_7_8596_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:107.0.7.8596-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_7_8596_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.7.8596-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_7_8596_1_1_27_0
@@ -189,8 +173,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.7.8596-1_1.28.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_8_8615_1
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.8.8615-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_107_0_8_8615_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:107.0.8.8615-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_8_8615_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.8.8615-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_8_8615_1_1_27_0
@@ -199,8 +181,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.8.8615-1_1.28.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_9_8621_1
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.9.8621-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_107_0_9_8621_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:107.0.9.8621-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_9_8621_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.9.8621-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_107_0_9_8621_1_1_27_0
@@ -209,8 +189,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:107.0.9.8621-1_1.28.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_108_0_0_8694_1
               value: "quay.io/mongodb/mongodb-agent-ubi:108.0.0.8694-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_108_0_0_8694_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:108.0.0.8694-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_108_0_0_8694_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:108.0.0.8694-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_108_0_0_8694_1_1_27_0
@@ -221,8 +199,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:12.0.28.7763-1"
             - name: RELATED_IMAGE_AGENT_IMAGE_12_0_29_7785_1
               value: "quay.io/mongodb/mongodb-agent-ubi:12.0.29.7785-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_12_0_29_7785_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:12.0.29.7785-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_12_0_29_7785_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:12.0.29.7785-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_12_0_29_7785_1_1_27_0
@@ -231,8 +207,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:12.0.29.7785-1_1.28.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_12_0_30_7791_1
               value: "quay.io/mongodb/mongodb-agent-ubi:12.0.30.7791-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_12_0_30_7791_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:12.0.30.7791-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_12_0_30_7791_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:12.0.30.7791-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_12_0_30_7791_1_1_27_0
@@ -241,8 +215,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:12.0.30.7791-1_1.28.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_12_0_31_7825_1
               value: "quay.io/mongodb/mongodb-agent-ubi:12.0.31.7825-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_12_0_31_7825_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:12.0.31.7825-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_12_0_31_7825_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:12.0.31.7825-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_12_0_31_7825_1_1_27_0
@@ -251,8 +223,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:12.0.31.7825-1_1.28.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_12_0_32_7857_1
               value: "quay.io/mongodb/mongodb-agent-ubi:12.0.32.7857-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_12_0_32_7857_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:12.0.32.7857-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_12_0_32_7857_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:12.0.32.7857-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_12_0_32_7857_1_1_27_0
@@ -261,8 +231,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:12.0.32.7857-1_1.28.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_12_0_33_7866_1
               value: "quay.io/mongodb/mongodb-agent-ubi:12.0.33.7866-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_12_0_33_7866_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:12.0.33.7866-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_12_0_33_7866_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:12.0.33.7866-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_12_0_33_7866_1_1_27_0
@@ -273,8 +241,6 @@ spec:
               value: "quay.io/mongodb/mongodb-agent-ubi:13.10.0.8620-1"
             - name: RELATED_IMAGE_AGENT_IMAGE_13_25_0_9175_1
               value: "quay.io/mongodb/mongodb-agent-ubi:13.25.0.9175-1"
-            - name: RELATED_IMAGE_AGENT_IMAGE_13_25_0_9175_1_1_25_0
-              value: "quay.io/mongodb/mongodb-agent-ubi:13.25.0.9175-1_1.25.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_13_25_0_9175_1_1_26_0
               value: "quay.io/mongodb/mongodb-agent-ubi:13.25.0.9175-1_1.26.0"
             - name: RELATED_IMAGE_AGENT_IMAGE_13_25_0_9175_1_1_27_0

helm_chart/values-openshift.yaml
@@ -121,89 +121,72 @@ relatedImages:
   - 107.0.0.8465-1
   - 107.0.0.8502-1
   - 107.0.1.8507-1
-  - 107.0.1.8507-1_1.25.0
   - 107.0.1.8507-1_1.26.0
   - 107.0.1.8507-1_1.27.0
   - 107.0.1.8507-1_1.28.0
   - 107.0.10.8627-1
-  - 107.0.10.8627-1_1.25.0
   - 107.0.10.8627-1_1.26.0
   - 107.0.10.8627-1_1.27.0
   - 107.0.10.8627-1_1.28.0
   - 107.0.11.8645-1
-  - 107.0.11.8645-1_1.25.0
   - 107.0.11.8645-1_1.26.0
   - 107.0.11.8645-1_1.27.0
   - 107.0.11.8645-1_1.28.0
   - 107.0.2.8531-1
-  - 107.0.2.8531-1_1.25.0
   - 107.0.2.8531-1_1.26.0
   - 107.0.2.8531-1_1.27.0
   - 107.0.2.8531-1_1.28.0
   - 107.0.3.8550-1
-  - 107.0.3.8550-1_1.25.0
   - 107.0.3.8550-1_1.26.0
   - 107.0.3.8550-1_1.27.0
   - 107.0.3.8550-1_1.28.0
   - 107.0.4.8567-1
-  - 107.0.4.8567-1_1.25.0
   - 107.0.4.8567-1_1.26.0
   - 107.0.4.8567-1_1.27.0
   - 107.0.4.8567-1_1.28.0
   - 107.0.6.8587-1
-  - 107.0.6.8587-1_1.25.0
   - 107.0.6.8587-1_1.26.0
   - 107.0.6.8587-1_1.27.0
   - 107.0.6.8587-1_1.28.0
   - 107.0.7.8596-1
-  - 107.0.7.8596-1_1.25.0
   - 107.0.7.8596-1_1.26.0
   - 107.0.7.8596-1_1.27.0
   - 107.0.7.8596-1_1.28.0
   - 107.0.8.8615-1
-  - 107.0.8.8615-1_1.25.0
   - 107.0.8.8615-1_1.26.0
   - 107.0.8.8615-1_1.27.0
   - 107.0.8.8615-1_1.28.0
   - 107.0.9.8621-1
-  - 107.0.9.8621-1_1.25.0
   - 107.0.9.8621-1_1.26.0
   - 107.0.9.8621-1_1.27.0
   - 107.0.9.8621-1_1.28.0
   - 108.0.0.8694-1
-  - 108.0.0.8694-1_1.25.0
   - 108.0.0.8694-1_1.26.0
   - 108.0.0.8694-1_1.27.0
   - 108.0.0.8694-1_1.28.0
   - 12.0.28.7763-1
   - 12.0.29.7785-1
-  - 12.0.29.7785-1_1.25.0
   - 12.0.29.7785-1_1.26.0
   - 12.0.29.7785-1_1.27.0
   - 12.0.29.7785-1_1.28.0
   - 12.0.30.7791-1
-  - 12.0.30.7791-1_1.25.0
   - 12.0.30.7791-1_1.26.0
   - 12.0.30.7791-1_1.27.0
   - 12.0.30.7791-1_1.28.0
   - 12.0.31.7825-1
-  - 12.0.31.7825-1_1.25.0
   - 12.0.31.7825-1_1.26.0
   - 12.0.31.7825-1_1.27.0
   - 12.0.31.7825-1_1.28.0
   - 12.0.32.7857-1
-  - 12.0.32.7857-1_1.25.0
   - 12.0.32.7857-1_1.26.0
   - 12.0.32.7857-1_1.27.0
   - 12.0.32.7857-1_1.28.0
   - 12.0.33.7866-1
-  - 12.0.33.7866-1_1.25.0
   - 12.0.33.7866-1_1.26.0
   - 12.0.33.7866-1_1.27.0
   - 12.0.33.7866-1_1.28.0
   - 13.10.0.8620-1
   - 13.25.0.9175-1
-  - 13.25.0.9175-1_1.25.0
   - 13.25.0.9175-1_1.26.0
   - 13.25.0.9175-1_1.27.0
   - 13.25.0.9175-1_1.28.0

commit 4445f1214ff757a213aa25459924620cee2e680f

Fix daily build not building all images (#3917)
# Summary

Due to a bug introduced in #3883 Sonar was only actually building the
last image in the matrix, over and over. The fix is to always copy the
`args` dict that contains the requested version image before submitting
it to a concurrent Executor for pickling so that the next loop iteration
doesn't update it in place.

## Documentation changes

* [ ] Add an entry to [release notes](.../RELEASE_NOTES.md).
* [ ] When needed, make sure you create a new [DOCSP
ticket](https://jira.mongodb.org/projects/DOCSP) that documents your
change.

## Changes to CRDs

* [ ] Add `slaskawi`(Sebastian) and `@giohan` (George) as reviewers.
* [ ] Make sure any changes are reflected on `/public/samples`
directory.

pipeline.py
@@ -725,6 +725,7 @@ def build_image_daily(
                 build_configuration.include_tags.extend(variants)
 
                 logger.info("Rebuilding {} with variants {}".format(version, variants))
+                args = copy.deepcopy(args)
                 args["release_version"] = version
 
                 tasks_queue.put(executor.submit(build_image_daily_parallel_worker, build_configuration, args))