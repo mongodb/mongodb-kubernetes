fdfe6d5fad0006298f54cb94c9a2b34778c04fa2
Nam Nguyen <nam.nguyen@mongodb.com>
2025-02-04 10:56:29 +0100

    split unit test by language and parse golang unit test output (#4067)
    # Summary
    
    - split unit test code into task for python and golang
    - upload golang test result to evergreen
    
    ## Proof
    - new tasks in variant unit_tests:
    https://docs.google.com/document/d/1gWZQIj5CNwDXGsqiBNNFq-MWq1quyOBH6WZN5tUW0RQ/edit?tab=t.0#heading=h.7sj23lrlzcwt
    - test results are now uploaded:
    https://spruce.mongodb.com/task/ops_manager_kubernetes_unit_tests_unit_tests_golang_patch_9f86602fdd0f9865671b9babfe188733880ef8c7_67a0ea8717a8420008bb44bd_25_02_03_16_10_51?execution=0&page=0&sorts=STATUS%3AASC%3BDURATION%3ADESC
    
    ![Screenshot 2025-02-03 at 17 18
    03](https://github.com/user-attachments/assets/164cff90-2ce7-4f53-9bee-8d06baafdf90)
    ![Screenshot 2025-02-03 at 17 18
    18](https://github.com/user-attachments/assets/724091a5-e0b0-4a84-a642-430e98ff76cf)

--- scripts/evergreen/unit-tests.sh
+++ scripts/evergreen/unit-tests.sh
@@ -2,7 +2,7 @@
 
 set -Eeou pipefail
 
-rm -f ops-manager-kubernetes.suite
+rm -f result.suite
 
 # shellcheck disable=SC2038
 # shellcheck disable=SC2016
@@ -11,16 +11,16 @@ cd "$0"
 echo "testing $0"
 if [ "$USE_RACE" = "true" ]; then
   echo "running test with race enabled"
-  GO_TEST_CMD="go test -race -coverprofile cover.out ./..."
+  GO_TEST_CMD="go test -v -race -coverprofile cover.out ./..."
 else
   echo "running test without race enabled"
-  GO_TEST_CMD="go test -coverprofile cover.out ./..."
+  GO_TEST_CMD="go test -v -coverprofile cover.out ./..."
 fi
 echo "running $GO_TEST_CMD"
-eval "$GO_TEST_CMD" | tee -a ops-manager-kubernetes.suite
+eval "$GO_TEST_CMD" | tee -a result.suite
 if [ $? -ne 0 ]; then
   echo "Tests failed in directory $0"
-  grep "FAIL:" ops-manager-kubernetes.suite
+  grep "FAIL:" result.suite
   echo ""
   exit 1
 fi
