e1011f2b514bc4bd2590aabe47a4d7ec10c60dcd
Nam Nguyen <nam.nguyen@mongodb.com>
2025-02-13 14:00:08 +0100

    fix unit test reporting (#4095)
    # Summary
    
    This patch fixes unit test reporting by doing the following
    - not fail unit-test.sh if a sub go.mod file fails
    - load all result.suite files into evergreen
    - let evergreen decide whether we succeed or not based on the uploaded
    suites
    
    ## Proof of Work
    
    example from my other PR; this change has been cherry-picked:
    https://spruce.mongodb.com/task/ops_manager_kubernetes_unit_tests_unit_tests_golang_patch_fdfe6d5fad0006298f54cb94c9a2b34778c04fa2_67accf1b1acbc90007401d4e_25_02_12_16_41_00?execution=0&sortBy=STATUS&sortDir=ASC
    
    ## Checklist
    - [ ] Have you linked a jira ticket and/or is the ticket in the title?
    - [ ] Have you checked whether your jira ticket required DOCSP changes?
    - [x] Have you checked for release_note changes?
    
    ## Reminder (Please remove this when merging)
    - Please try to Approve or Reject Changes the PR, keep PRs in review as
    short as possible
    - Our Short Guide for PRs:
    [Link](https://docs.google.com/document/d/1T93KUtdvONq43vfTfUt8l92uo4e4SEEvFbIEKOxGr44/edit?tab=t.0)
    - Remember the following Communication Standards - use comment prefixes
    for clarity:
      * **blocking**: Must be addressed before approval.
      * **follow-up**: Can be addressed in a later PR or ticket.
      * **q**: Clarifying question.
      * **nit**: Non-blocking suggestions.
      * **note**: Side-note, non-actionable. Example: Praise
      * --> no prefix is considered a question

--- scripts/evergreen/unit-tests.sh
+++ scripts/evergreen/unit-tests.sh
@@ -2,13 +2,12 @@
 
 set -Eeou pipefail
 
-rm -f result.suite
-
 # shellcheck disable=SC2038
 # shellcheck disable=SC2016
-find . -name go.mod -exec dirname "{}" \+ | xargs -L 1 /bin/bash -o pipefail -c '
+find . -name go.mod -not -path "./docker/mongodb-enterprise-tests/*" -exec dirname "{}" \+ | xargs -L 1 /bin/bash -c '
 cd "$0"
 echo "testing $0"
+rm -f result.suite
 if [ "$USE_RACE" = "true" ]; then
   echo "running test with race enabled"
   GO_TEST_CMD="go test -v -race -coverprofile cover.out ./..."
@@ -18,10 +17,4 @@ else
 fi
 echo "running $GO_TEST_CMD"
 eval "$GO_TEST_CMD" | tee -a result.suite
-if [ $? -ne 0 ]; then
-  echo "Tests failed in directory $0"
-  grep "FAIL:" result.suite
-  echo ""
-  exit 1
-fi
 '
