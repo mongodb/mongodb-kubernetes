647ca535b9163e5e9eb9025516f3407bf5ad831a
Mikalai Radchuk <509198+m1kola@users.noreply.github.com>
2025-02-13 17:48:06 +0100

    Improve Istio download script (#4098)
    * Currently we are silently failing Istio download which can lead to
      network issues when working with multi cluster setups.
    * Improve Istio conditional download script

--- multi_cluster/tools/download_istio.sh
+++ multi_cluster/tools/download_istio.sh
@@ -2,9 +2,14 @@
 set -Eeou pipefail
 
 export VERSION=${VERSION:-1.16.1}
-
-echo "Downloading istio"
 ISTIO_SCRIPT_CHECKSUM="254c6bd6aa5b8ac8c552561c84d8e9b3a101d9e613e2a8edd6db1f19c1871dbf"
-[ ! -d "istio-${VERSION}" ] && curl -O https://raw.githubusercontent.com/istio/istio/d710dfc2f95adb9399e1656165fa5ac22f6e1a16/release/downloadIstioCandidate.sh
-echo "${ISTIO_SCRIPT_CHECKSUM}  downloadIstioCandidate.sh" | sha256sum --check
-[ ! -d "istio-${VERSION}" ] && ISTIO_VERSION=${VERSION} sh downloadIstioCandidate.sh
+
+echo "Checking if we need to download Istio ${VERSION}"
+if [ ! -d "istio-${VERSION}" ]; then
+    echo "Downloading Istio ${VERSION}"
+    curl -O https://raw.githubusercontent.com/istio/istio/d710dfc2f95adb9399e1656165fa5ac22f6e1a16/release/downloadIstioCandidate.sh
+    echo "${ISTIO_SCRIPT_CHECKSUM}  downloadIstioCandidate.sh" | sha256sum --check
+    ISTIO_VERSION=${VERSION} sh downloadIstioCandidate.sh
+else
+    echo "Istio ${VERSION} already downloaded... Skipping."
+fi
