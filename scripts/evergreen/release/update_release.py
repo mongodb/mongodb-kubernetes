1c99ab53a588f564cc607a122f43353072d2ce81
Maciej Karaś <6159874+MaciejKaras@users.noreply.github.com>
2025-02-14 16:07:41 +0100

    CLOUDP-295945 - stop calling mms repo in precommit hook (#4100)
    # Summary
    
    This PR removes calls to mms repository in precommit hook. 
    
    
    [update_agent_and_tools_version](https://github.com/10gen/ops-manager-kubernetes/blob/008c01523d5a1576a1ed2aaeecb08e208315c3a1/scripts/evergreen/release/update_release.py#L95)
    function is the only code that calls mms to get agent and mongo tools
    version for the most recent OM version in release.json
    (`["supportedImages"]["ops-manager"]["versions"][-1]`). This information
    is used for:
    - `update_om_mapping` -> it updates the
    `["supportedImages"]["mongodb-agent"]["opsManagerMapping"]["ops_manager"][latest_version]`
    agent and tools mapping. I see that those values are set [by PCT during
    OM
    release](https://github.com/10gen/ops-manager-kubernetes/pull/4063/commits/53b970cdaaefdbe8f450ce731794d1369c2fb85e)
    and precommit hook tries to update this path again with no changes
    - `update_mongodb_tools_bundle` -> this updates
    `["mongodbToolsBundle"]["ubi"]` bundle version that is used when
    building `init_appdb` and `init_database`. We can get this information
    from latest version in opsManagerMapping that PCT sets during OM release
    
    ## Followup tasks (after merging)
    
    - [ ] update docs
    [here](https://wiki.corp.mongodb.com/display/MMS/Setting+up+local+development+and+E2E+testing#SettinguplocaldevelopmentandE2Etesting-Preparelocalenvironment)
    and [here](https://wiki.corp.mongodb.com/display/MMS/Pre-Commit+Hook)
    after merging PR
    
    ## Proof of Work
    
    Run locally precommit hook with updated OM mapping in `release.json` and
    it updated `mongodbToolsBundle` entry. Without changes to release.json
    there are no updates - CI lint task
    [succeeded](https://spruce.mongodb.com/task/ops_manager_kubernetes_unit_tests_lint_repo_patch_f316028ac429a42806039a834ce78906b5e2c1f1_67ae19a200e9150007c7d8ab_25_02_13_16_11_16/logs?execution=0).
    
    ## Checklist
    - [x] Have you linked a jira ticket and/or is the ticket in the title?
    - [x] Have you checked whether your jira ticket required DOCSP changes?
    - [x] Have you checked for release_note changes?
    
    ## Reminder (Please remove this when merging)
    - Please try to Approve or Reject Changes the PR, keep PRs in review as
    short as possible
    - Our Short Guide for PRs:
    [Link](https://docs.google.com/document/d/1T93KUtdvONq43vfTfUt8l92uo4e4SEEvFbIEKOxGr44/edit?tab=t.0)
    - Remember the following Communication Standards - use comment prefixes
    for clarity:
      * **blocking**: Must be addressed before approval.
      * **follow-up**: Can be addressed in a later PR or ticket.
      * **q**: Clarifying question.
      * **nit**: Non-blocking suggestions.
      * **note**: Side-note, non-actionable. Example: Praise 
      * --> no prefix is considered a question

--- scripts/evergreen/release/update_release.py
+++ scripts/evergreen/release/update_release.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 
-import configparser
 import json
 import logging
 import os
-import sys
 
-import requests
 import yaml
-from packaging.version import Version
 
 logger = logging.getLogger(__name__)
 
@@ -21,22 +17,6 @@ def get_latest_om_versions_from_evergreen_yml():
     return data["variables"][0], data["variables"][1]
 
 
-def get_headers():
-    """
-    Returns an authentication header that can be used when accessing
-    the Github API. This is used to access private 10gen repos.
-    """
-
-    github_token = os.getenv("GITHUB_TOKEN_READ")
-    if github_token is None:
-        raise Exception(
-            "Missing GITHUB_TOKEN_READ environment variable; see https://wiki.corp.mongodb.com/display/MMS/Pre-Commit+Hook"
-        )
-    return {
-        "Authorization": f"token {github_token}",
-    }
-
-
 def update_release_json():
     # Define a custom constructor to preserve the anchors in the YAML file
     release = os.path.join(os.getcwd(), "release.json")
@@ -45,7 +25,7 @@ def update_release_json():
 
     # PCT already bumps the release.json, such that the last element contains the newest version, since they are sorted
     newest_om_version = data["supportedImages"]["ops-manager"]["versions"][-1]
-    update_agent_and_tools_version(data, newest_om_version)
+    update_mongodb_tools_bundle(data, newest_om_version)
 
     # PCT bumps this field, and we can use this as a base to set the version for everything else in release.json
     newest_operator_version = data["mongodbOperator"]
@@ -92,43 +72,12 @@ def update_operator_related_versions(release: dict, version: str):
     logger.debug(f"Updated content {release}")
 
 
-def update_agent_and_tools_version(data, missing_version):
-    repo_owner = "10gen"
-    repo_name = "mms"
-    file_path = "server/conf/conf-hosted.properties"
-    if missing_version.startswith("6."):
-        tag_to_search = f"on-prem-{missing_version}"
-    else:
-        # starting om 7 our tag starts with ops-manager-<version> instead
-        tag_to_search = f"ops-manager-{missing_version}"
+def update_mongodb_tools_bundle(data, newest_om_version):
+    om_mapping = data["supportedImages"]["mongodb-agent"]["opsManagerMapping"]["ops_manager"][newest_om_version]
+    mongo_tool_version = om_mapping["tools_version"]
 
-    url = f"https://raw.githubusercontent.com/{repo_owner}/{repo_name}/{tag_to_search}/{file_path}"
-    response = requests.get(url=url, headers=get_headers())
-    # Check if the request was successful
-    if response.status_code == 200:
-        config = configparser.ConfigParser()
-        input_data = (
-            "[DEFAULT]\n" + response.text
-        )  # configparser needs a section, but our properties do not contain one.
-        config.read_string(input_data)
-        mongo_tool_version = config.get("DEFAULT", "mongotools.version")
-        agent_version = config.get("DEFAULT", "automation.agent.version")
-        update_om_mapping(agent_version, data, missing_version, mongo_tool_version)
-
-        new_rhel_tools_version = "100.10.0"
-        arch = "rhel88" if Version(mongo_tool_version) >= Version(new_rhel_tools_version) else "rhel80"
-        version_name = f"mongodb-database-tools-{arch}-x86_64-{mongo_tool_version}.tgz"
-        data["mongodbToolsBundle"]["ubi"] = version_name
-    else:
-        print(f"was not able to request file from {url}: {response.text}")
-        sys.exit(1)
-
-
-def update_om_mapping(agent_version, data, missing_version, mongo_tool_version):
-    data["supportedImages"]["mongodb-agent"]["opsManagerMapping"]["ops_manager"][missing_version] = {
-        "agent_version": f"{agent_version}",
-        "tools_version": f"{mongo_tool_version}",
-    }
+    version_name = f"mongodb-database-tools-rhel88-x86_64-{mongo_tool_version}.tgz"
+    data["mongodbToolsBundle"]["ubi"] = version_name
 
 
 update_release_json()
