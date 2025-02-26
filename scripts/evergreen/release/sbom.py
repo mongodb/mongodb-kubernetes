5473538478abaa82761b3df92a6ea193886801d4
Evan Fetsko <evan.fetsko@mongodb.com>
2025-02-26 03:29:49 -0500

    CLOUDP-301995: use Silkbomb 2.0 (replaces Silk usage with Kondukto) (#4117)
    # Summary
    
    CLOUDP-301995
    
    # Description
    - make updates to use Silkbomb 2.0
      - assume IAM role to pull `KONDUKTO_TOKEN` from AWS Secrets Manager
    - assumption is that `kondukto_role_arn` variable is set in the
    Evergreen project with a value of
    `arn:aws:iam::119629040606:role/kondukto`
    - the concept of "asset groups" does not exist in Kondukto
      - SBOMs will not be uploaded to a Kondukto project's "branch"
    - the existing naming scheme for "asset groups" will now be used to
    determine the Kondukto "branch" to upload an SBOM to
    
    ## Proof of Work
    
    - TODO (may need some assistance here)
      - Q: can I simply trigger the `release_agent` variant to test?
    
    ## Checklist
    - [x] Have you linked a jira ticket and/or is the ticket in the title?
    - [x] Have you checked whether your jira ticket required DOCSP changes?
    - [x] Have you checked for release_note changes?

--- scripts/evergreen/release/sbom.py
+++ scripts/evergreen/release/sbom.py
@@ -9,19 +9,17 @@ report generation might be found in https://wiki.corp.mongodb.com/display/MMS/Ku
 
 On a typical daily run, the workflow is the following:
 
-- Generating the Silk Asset Group for the daily run
-- If an SBOM Lite is already present in the S3, which means this is not the first run for the release:
-  - Download Augmented SBOM from the previous day (Silk vulnerability scanning happens at night)
-  - Uploading Augmented SBOM to S3
-  - Uploading Augmented SBOM to the release path in S3
 - Generate SBOM Lite
-- Uploading it to Silk
-- Uploading it S3
+- Uploading SBOM Lite to Kondukto
+- Generate Augmented SBOM
+- Uploading SBOM Lite and Augmented SBOM to S3
 
 In addition to this, there are special steps done only for the initial upload of a newly released images:
 
-- Uploading the SBOM Lite to a special path to S3, it's never updated - we want it to stay the same
-- Uploading the SBOM Lite to a special Asset Group in Silk
+- Generate SBOM Lite
+- Uploading SBOM Lite to Kondukto
+- Generate Augmented SBOM
+- Uploading the SBOM Lite and Augmented SBOM to a special path to S3, it's never updated - we want it to stay the same
 
 """
 
@@ -36,10 +34,11 @@ import boto3
 import botocore
 
 from lib.base_logger import logger
-from scripts.evergreen.release.images_signing import mongodb_artifactory_login
 
 S3_BUCKET = "kubernetes-operators-sboms"
-SILK_BOMB_IMAGE = "artifactory.corp.mongodb.com/release-tools-container-registry-local/silkbomb:1.0"
+SILK_BOMB_IMAGE = "artifactory.corp.mongodb.com/release-tools-container-registry-public-local/silkbomb:2.0"
+KONDUKTO_REPO = "10gen/ops-manager-kubernetes"
+WORKDIR = os.getenv("workdir")
 
 
 def get_image_sha(image_pull_spec: str):
@@ -111,22 +110,25 @@ def upload_to_s3(directory: str, file_name: str, s3_bucket: str, s3_path: str):
     logger.debug(f"Uploading file done")
 
 
-def validate_environment():
-    if "ARTIFACTORY_USERNAME" not in os.environ:
-        raise ValueError("ARTIFACTORY_USERNAME not defined in environment")
-    if "ARTIFACTORY_PASSWORD" not in os.environ:
-        raise ValueError("ARTIFACTORY_PASSWORD not defined in environment")
-    if "SILK_CLIENT_ID" not in os.environ:
-        raise ValueError("SILK_CLIENT_ID not defined in environment")
-    if "SILK_CLIENT_SECRET" not in os.environ:
-        raise ValueError("SILK_CLIENT_SECRET not defined in environment")
+def get_silkbomb_env_file_path() -> str:
+    if not WORKDIR:
+        raise EnvironmentError("'workdir' environment variable is not set")
+
+    silkbomb_env_path = os.path.join(WORKDIR, "silkbomb.env")
+    if not os.path.exists(silkbomb_env_path):
+        raise FileNotFoundError(f"{silkbomb_env_path} does not exist")
+    return silkbomb_env_path
 
 
-def upload_sbom_lite_to_silk(directory: str, file_name: str, asset_group: str):
-    logger.debug(f"Uploading SBOM Lite {directory}/{file_name} to Silk")
-    mongodb_artifactory_login()
-    silk_client_id = os.getenv("SILK_CLIENT_ID")
-    silk_client_secret = os.getenv("SILK_CLIENT_SECRET")
+def augment_sbom(
+    silkbomb_env_file: str,
+    directory: str,
+    sbom_lite_file_name: str,
+    sbom_augmented_file_name: str,
+    kondukto_repo: str,
+    kondukto_branch: str,
+) -> bool:
+    logger.debug(f"Augmenting SBOM {directory}/{sbom_lite_file_name} with Kondukto scan results")
 
     command = [
         "docker",
@@ -136,55 +138,26 @@ def upload_sbom_lite_to_silk(directory: str, file_name: str, asset_group: str):
         "--rm",
         "-v",
         f"{directory}:/sboms",
-        "-e",
-        f"SILK_CLIENT_ID={silk_client_id}",
-        "-e",
-        f"SILK_CLIENT_SECRET={silk_client_secret}",
+        "--env-file",
+        silkbomb_env_file,
         SILK_BOMB_IMAGE,
-        "upload",
-        "--silk_asset_group",
-        asset_group,
+        "augment",
         "--sbom_in",
-        f"sboms/{file_name}",
-    ]
-
-    logger.debug(f"Calling Silk upload: {' '.join(command)}")
-    if retry(lambda: subprocess.run(command, check=True)):
-        logger.debug(f"Uploading SBOM Lite done")
-    else:
-        logger.error(f"Failed to upload SBOM Lite")
-
-
-def download_augmented_sbom_from_silk(directory: str, file_name: str, asset_group: str) -> bool:
-    logger.debug(f"Downloading Augmented SBOM {directory}/{file_name} from Silk")
-    silk_client_id = os.getenv("SILK_CLIENT_ID")
-    silk_client_secret = os.getenv("SILK_CLIENT_SECRET")
-    command = [
-        "docker",
-        "run",
-        "--platform",
-        "linux/amd64",
-        "--rm",
-        "-v",
-        f"{directory}:/sboms",
-        "-e",
-        f"SILK_CLIENT_ID={silk_client_id}",
-        "-e",
-        f"SILK_CLIENT_SECRET={silk_client_secret}",
-        SILK_BOMB_IMAGE,
-        "download",
-        "--silk_asset_group",
-        asset_group,
+        f"sboms/{sbom_lite_file_name}",
+        "--repo",
+        kondukto_repo,
+        "--branch",
+        kondukto_branch,
         "--sbom_out",
-        f"sboms/{file_name}",
+        f"sboms/{sbom_augmented_file_name}",
     ]
 
-    logger.debug(f"Calling Silk download: {' '.join(command)}")
+    logger.debug(f"Calling Silkbomb augment: {' '.join(command)}")
     if retry(lambda: subprocess.run(command, check=True)):
-        logger.debug(f"Downloading Augmented SBOM done")
+        logger.debug(f"Augmenting SBOM done")
         return True
     else:
-        logger.error(f"Failed to download Augmented SBOM")
+        logger.error(f"Failed to augment SBOM")
         return False
 
 
@@ -216,16 +189,14 @@ def unpack(directory: str, file_path: str):
     logger.info("Unpacking done")
 
 
-def create_sbom_light_for_binary(directory: str, file_path: str, sbom_light_path: str, platform: str):
-    logger.info(f"Creating SBOM Light for {directory}/{file_path}")
+def create_sbom_lite_for_binary(directory: str, file_path: str, sbom_light_path: str):
+    logger.info(f"Creating SBOM Lite for {directory}/{file_path}")
 
     purl_file_name = f"{sbom_light_path}.purl"
 
     subprocess.check_call(
         f"./scripts/evergreen/release/purl_creator.sh {directory}/{file_path} {directory}/{purl_file_name}", shell=True
     )
-
-    mongodb_artifactory_login()
 
     command = [
         "docker",
@@ -245,13 +216,13 @@ def create_sbom_light_for_binary(directory: str, file_path: str, sbom_light_path
     logger.debug(f"Calling update purls: {' '.join(command)}")
     subprocess.run(command, check=True)
 
-    logger.info(f"Creating SBOM Light done")
+    logger.info(f"Creating SBOM Lite done")
 
 
 def generate_sbom_for_cli(cli_version: str = "1.25.0", platform: str = "linux/amd64"):
     logger.info(f"Generating SBOM for CLI for version {cli_version} and platform {platform}")
     try:
-        validate_environment()
+        silkbomb_env_file = get_silkbomb_env_file_path()
         platform_sanitized = platform.replace("/", "-")
         platform_sanitized_with_underscores = platform.replace("/", "_")
 
@@ -259,7 +230,8 @@ def generate_sbom_for_cli(cli_version: str = "1.25.0", platform: str = "linux/am
             sbom_lite_file_name = f"kubectl-mongodb-{cli_version}-{platform_sanitized}.json"
             sbom_augmented_file_name = f"kubectl-mongodb-{cli_version}-{platform_sanitized}-augmented.json"
             product_name = "mongodb-enterprise-cli"
-            asset_group_release_id = f"{product_name}-release-{cli_version}-{platform_sanitized}"
+            kondukto_project_repo = "mongodb/mongodb-enterprise-kubernetes"
+            kondukto_branch_id = f"{product_name}-release-{cli_version}-{platform_sanitized}"
             s3_release_sbom_lite_path = f"sboms/release/lite/{product_name}/{cli_version}/{platform_sanitized}"
             s3_release_sbom_augmented_path = (
                 f"sboms/release/augmented/{product_name}/{cli_version}/{platform_sanitized}"
@@ -268,42 +240,35 @@ def generate_sbom_for_cli(cli_version: str = "1.25.0", platform: str = "linux/am
             download_binary_url = f"https://github.com/mongodb/mongodb-enterprise-kubernetes/releases/download/{cli_version}/{binary_file_name}"
             unpacked_binary_file_name = "kubectl-mongodb"
 
-            create_asset_group_in_silk_if_needed(
-                asset_group_release_id, "MongoDB Enterprise CLI", "mongodb/mongodb-enterprise-kubernetes"
-            )
-
-            if not s3_path_exists(s3_release_sbom_lite_path):
-                logger.info("Uploading SBOM Lite for the first release")
+            if not s3_path_exists(s3_release_sbom_augmented_path):
                 download_file(download_binary_url, directory, binary_file_name)
                 unpack(directory, binary_file_name)
-                create_sbom_light_for_binary(directory, unpacked_binary_file_name, sbom_lite_file_name, platform)
-                upload_to_s3(directory, sbom_lite_file_name, S3_BUCKET, s3_release_sbom_lite_path)
-                upload_sbom_lite_to_silk(directory, sbom_lite_file_name, asset_group_release_id)
-            elif not s3_path_exists(s3_release_sbom_augmented_path):
-                logger.info("Uploading Augmented SBOM for the first release")
-                if download_augmented_sbom_from_silk(directory, sbom_augmented_file_name, asset_group_release_id):
+                create_sbom_lite_for_binary(directory, unpacked_binary_file_name, sbom_lite_file_name)
+                logger.info("Augmenting SBOM Lite and uploading SBOM Lite and Augmented SBOM for the first release")
+                if augment_sbom(
+                    silkbomb_env_file,
+                    directory,
+                    sbom_lite_file_name,
+                    sbom_augmented_file_name,
+                    kondukto_project_repo,
+                    kondukto_branch_id,
+                ):
+                    upload_to_s3(directory, sbom_lite_file_name, S3_BUCKET, s3_release_sbom_lite_path)
                     upload_to_s3(directory, sbom_augmented_file_name, S3_BUCKET, s3_release_sbom_augmented_path)
                 else:
-                    logger.exception(f"Could not download release Augmented SBOM from Silk")
+                    logger.exception(f"Could not augment release SBOM with Kondukto scan results")
     except:
         logger.exception("Skipping SBOM Generation because of an error")
 
     logger.info(f"Generating SBOM done")
 
 
-def create_asset_group_in_silk_if_needed(asset_group: str, description: str, project: str):
-    logger.debug(f"Creating Asset Group in Silk for {asset_group}, {description} and {project}")
-    command = ["./scripts/evergreen/release/create_asset_group.sh", "-a", asset_group, "-d", description, "-p", project]
-    subprocess.run(command, check=True)
-    logger.debug(f"Created Asset Group")
-
-
-def get_asset_group_data(image_name: str, tag: str, platform_sanitized: str):
-    daily_asset_group_id = f"{image_name}-daily-{tag}-{platform_sanitized}"
-    release_asset_group_id = f"{image_name}-release-{tag}-{platform_sanitized}"
+def get_kondukto_sbom_data(image_name: str, tag: str, platform_sanitized: str):
+    daily_project_branch_id = f"{image_name}-daily-{tag}-{platform_sanitized}"
+    release_project_branch_id = f"{image_name}-release-{tag}-{platform_sanitized}"
     if image_name.startswith("mongodb-enterprise"):
-        return daily_asset_group_id, release_asset_group_id, f"MEKO {image_name}", "10gen/ops-manager-kubernetes"
-    return daily_asset_group_id, release_asset_group_id, f"MCO {image_name}", "mongodb/mongodb-kubernetes-operator"
+        return daily_project_branch_id, release_project_branch_id, "10gen/ops-manager-kubernetes"
+    return daily_project_branch_id, release_project_branch_id, "mongodb/mongodb-kubernetes-operator"
 
 
 def s3_path_exists(s3_path):
@@ -331,11 +296,11 @@ def generate_sbom(image_pull_spec: str, platform: str = "linux/amd64"):
     image_name: str
     tag: str
     try:
-        validate_environment()
+        silkbomb_env_file = get_silkbomb_env_file_path()
         registry, organization, image_name, tag, sha = parse_image_pull_spec(image_pull_spec)
         platform_sanitized = platform.replace("/", "-")
-        asset_group_daily_id, asset_group_release_id, asset_group_description, asset_group_project = (
-            get_asset_group_data(image_name, tag, platform_sanitized)
+        daily_project_branch_id, release_project_branch_id, kondukto_project_repo = get_kondukto_sbom_data(
+            image_name, tag, platform_sanitized
         )
 
         with tempfile.TemporaryDirectory() as directory:
@@ -345,9 +310,6 @@ def generate_sbom(image_pull_spec: str, platform: str = "linux/amd64"):
             create_sbom_lite_for_image(image_pull_spec, directory, sbom_lite_file_name, platform)
 
             ### Daily SBOM generation ###
-            s3_daily_path_for_specific_tag = (
-                f"sboms/daily/lite/{registry}/{organization}/{image_name}/{tag}/{platform_sanitized}"
-            )
             s3_daily_sbom_lite_path = (
                 f"sboms/daily/lite/{registry}/{organization}/{image_name}/{tag}/{platform_sanitized}/{sha}"
             )
@@ -355,26 +317,23 @@ def generate_sbom(image_pull_spec: str, platform: str = "linux/amd64"):
                 f"sboms/daily/augmented/{registry}/{organization}/{image_name}/{tag}/{platform_sanitized}/{sha}"
             )
 
-            create_asset_group_in_silk_if_needed(asset_group_daily_id, asset_group_description, asset_group_project)
-
-            # Silk augmentation happens at the fetch time, however the Snyk vuln. association happens asynchronously.
-            # This means we need to fetch the Augmented SBOMs and store them in our S3 to be ready for generating the
-            # report one day after the release.
-            if s3_path_exists(s3_daily_path_for_specific_tag):
-                if download_augmented_sbom_from_silk(directory, sbom_augmented_file_name, asset_group_daily_id):
-                    upload_to_s3(directory, sbom_augmented_file_name, S3_BUCKET, s3_daily_sbom_augmented_path)
-                else:
-                    logger.exception(f"Could not download daily Augmented SBOM from Silk. Continuing...")
-
-            upload_sbom_lite_to_silk(directory, sbom_lite_file_name, asset_group_daily_id)
-            upload_to_s3(directory, sbom_lite_file_name, S3_BUCKET, s3_daily_sbom_lite_path)
+            # produce Augmented SBOM with Silkbomb and upload SBOM Lite and Augmented SBOM to S3
+            if augment_sbom(
+                silkbomb_env_file,
+                directory,
+                sbom_lite_file_name,
+                sbom_augmented_file_name,
+                kondukto_project_repo,
+                daily_project_branch_id,
+            ):
+                upload_to_s3(directory, sbom_lite_file_name, S3_BUCKET, s3_daily_sbom_lite_path)
+                upload_to_s3(directory, sbom_augmented_file_name, S3_BUCKET, s3_daily_sbom_augmented_path)
+            else:
+                logger.exception(f"Could not augment daily SBOM with Kondukto scan results. Continuing...")
 
             ### Release SBOM generation ###
             # Then checking for path, we don't want to include SHA Digest.
             # We just want to keep there the initial one. Nothing more.
-            s3_release_sbom_lite_path_for_specific_tag = (
-                f"sboms/release/lite/{registry}/{organization}/{image_name}/{tag}/{platform_sanitized}/"
-            )
             s3_release_sbom_augmented_path_for_specific_tag = (
                 f"sboms/release/augmented/{registry}/{organization}/{image_name}/{tag}/{platform_sanitized}/"
             )
@@ -386,22 +345,22 @@ def generate_sbom(image_pull_spec: str, platform: str = "linux/amd64"):
                 f"sboms/release/augmented/{registry}/{organization}/{image_name}/{tag}/{platform_sanitized}/{sha}"
             )
 
-            create_asset_group_in_silk_if_needed(asset_group_release_id, asset_group_description, asset_group_project)
-
             # This path is only executed when there's a first rebuild of the release artifacts.
-            # Then, we upload the SBOM Lite this single time only.
-            if not s3_path_exists(s3_release_sbom_lite_path_for_specific_tag):
-                logger.info("Uploading SBOM Lite for the first release")
-                upload_sbom_lite_to_silk(directory, sbom_lite_file_name, asset_group_release_id)
-                upload_to_s3(directory, sbom_lite_file_name, S3_BUCKET, s3_release_sbom_lite_path)
-            # This condition is evaluated at Release Date +1 day and here we check if we got the latest
-            # Augmented SBOM for the release
-            elif not s3_path_exists(s3_release_sbom_augmented_path_for_specific_tag):
-                logger.info("Uploading Augmented SBOM for the first release")
-                if download_augmented_sbom_from_silk(directory, sbom_augmented_file_name, asset_group_release_id):
+            # Then, we upload the SBOM Lite and Augmented SBOM this single time only.
+            if not s3_path_exists(s3_release_sbom_augmented_path_for_specific_tag):
+                logger.info("Augmenting SBOM Lite and uploading SBOM Lite and Augmented SBOM for the first release")
+                if augment_sbom(
+                    silkbomb_env_file,
+                    directory,
+                    sbom_lite_file_name,
+                    sbom_augmented_file_name,
+                    kondukto_project_repo,
+                    release_project_branch_id,
+                ):
+                    upload_to_s3(directory, sbom_lite_file_name, S3_BUCKET, s3_release_sbom_lite_path)
                     upload_to_s3(directory, sbom_augmented_file_name, S3_BUCKET, s3_release_sbom_augmented_path)
                 else:
-                    logger.exception(f"Could not download release Augmented SBOM from Silk")
+                    logger.exception(f"Could not augment release SBOM with Kondukto scan results")
 
     except Exception as err:
         logger.exception(f"Skipping SBOM Generation because of an error: {err}")
