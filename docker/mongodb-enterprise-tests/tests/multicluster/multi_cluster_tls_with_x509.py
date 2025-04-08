a41b14376dff52f555b88f7174e1e6f5cfbf560d
mircea-cosbuc <mircea-cosbuc@users.noreply.github.com>
2025-04-08 09:51:07 +0200

    CLOUDP-306450: Simplify multi-cluster X509 test with note to revisit (#4251)
    # Summary
    
    Opening this in place of
    https://github.com/10gen/ops-manager-kubernetes/pull/4227 .
    Additional investigation with the agent team is required to understand
    the race condition when enabling cluster authentication. Opened
    https://jira.mongodb.org/browse/CLOUDP-311366 for this investigation.
    This patch also removes the assertions for certificate rotation as they
    were incorrectly waiting for the resource to leave `Running` state. The
    assertion was only accidentally correct, because the resource would
    occasionally transition to `Failed` state when waiting for the agents to
    be ready would time-out.
    
    Re-enabling certificate rotation check should use a different mechanism
    for checking certificates were rotated and consumed correctly (for
    example by checking automation config + agents goal state).
    
    ## Proof of Work
    
    (Hopefully passing) test here:
    https://spruce.mongodb.com/task/ops_manager_kubernetes_e2e_multi_cluster_kind_e2e_multi_cluster_tls_with_x509_patch_aadda155316f40bada5be3c13c9674e143be62f6_67f3958c729c090007eb2875_25_04_07_09_06_22/logs?execution=0
    
    ## Checklist
    - [x] Have you linked a jira ticket and/or is the ticket in the title?
    - [x] Have you checked whether your jira ticket required DOCSP changes?
    - [x] Have you checked for release_note changes?
    
    ## Reminder (Please remove this when merging)
    - Please try to Approve or Reject Changes the PR, keep PRs in review as
    short as possible
    - Our Short Guide for PRs:
    [Link](REDACTED)
    - Remember the following Communication Standards - use comment prefixes
    for clarity:
      * **blocking**: Must be addressed before approval.
      * **follow-up**: Can be addressed in a later PR or ticket.
      * **q**: Clarifying question.
      * **nit**: Non-blocking suggestions.
      * **note**: Side-note, non-actionable. Example: Praise
      * --> no prefix is considered a question

--- docker/mongodb-enterprise-tests/tests/multicluster/multi_cluster_tls_with_x509.py
+++ docker/mongodb-enterprise-tests/tests/multicluster/multi_cluster_tls_with_x509.py
@@ -4,7 +4,6 @@ from typing import List
 import kubernetes
 from kubetester.automation_config_tester import AutomationConfigTester
 from kubetester.certs import (
-    Certificate,
     create_multi_cluster_mongodb_x509_tls_certs,
     create_multi_cluster_x509_agent_certs,
     create_multi_cluster_x509_user_cert,
@@ -19,6 +18,9 @@ from kubetester.operator import Operator
 from pytest import fixture, mark
 from tests.multicluster.conftest import cluster_spec_list
 
+# TODO This test needs to re-introduce certificate rotation and enabling authentication step by step
+# See https://jira.mongodb.org/browse/CLOUDP-311366
+
 CERT_SECRET_PREFIX = "clustercert"
 MDB_RESOURCE = "multi-cluster-replica-set"
 BUNDLE_SECRET_NAME = f"{CERT_SECRET_PREFIX}-{MDB_RESOURCE}-cert"
@@ -32,7 +34,7 @@ def mongodb_multi_unmarshalled(namespace: str, member_cluster_names, custom_mdb_
     resource.set_version(ensure_ent_version(custom_mdb_version))
 
     resource["spec"]["clusterSpecList"] = cluster_spec_list(member_cluster_names, [2, 1, 2])
-    resource["spec"]["additionalMongodConfig"] = {"net": {"tls": {"mode": "preferTLS"}}}
+    resource["spec"]["additionalMongodConfig"] = {"net": {"tls": {"mode": "requireTLS"}}}
 
     return resource
 
@@ -90,6 +92,8 @@ def agent_certs(
 def mongodb_multi(
     central_cluster_client: kubernetes.client.ApiClient,
     server_certs: str,
+    agent_certs: str,
+    cluster_certs: str,
     mongodb_multi_unmarshalled: MongoDBMulti,
     multi_cluster_issuer_ca_configmap: str,
     member_cluster_names,
@@ -100,6 +104,12 @@ def mongodb_multi(
         "certsSecretPrefix": CERT_SECRET_PREFIX,
         "tls": {
             "ca": multi_cluster_issuer_ca_configmap,
+        },
+        "authentication": {
+            "enabled": True,
+            "modes": ["X509", "SCRAM"],
+            "agents": {"mode": "X509"},
+            "internalCluster": "X509",
         },
     }
 
@@ -126,55 +136,8 @@ def test_deploy_operator(multi_cluster_operator: Operator):
 
 
 @mark.e2e_multi_cluster_tls_with_x509
-def test_deploy_mongodb_multi_with_tls(mongodb_multi: MongoDBMulti, namespace: str):
+def test_deploy_mongodb_multi_with_tls_and_authentication(mongodb_multi: MongoDBMulti, namespace: str):
     mongodb_multi.assert_reaches_phase(Phase.Running, timeout=1200)
-
-
-@mark.e2e_multi_cluster_tls_with_x509
-def test_rotate_cert_and_assert_mdb_running(
-    namespace: str,
-    mongodb_multi: MongoDBMulti,
-    central_cluster_client: kubernetes.client.ApiClient,
-):
-    assert_certificate_rotation(central_cluster_client, mongodb_multi, namespace, BUNDLE_SECRET_NAME)
-
-
-@mark.e2e_multi_cluster_tls_with_x509
-def test_mongodb_multi_tls_enable_x509(
-    mongodb_multi: MongoDBMulti,
-    namespace: str,
-    agent_certs: str,
-):
-    mongodb_multi.load()
-
-    mongodb_multi["spec"]["security"] = {
-        "authentication": {
-            "enabled": True,
-            "modes": ["X509", "SCRAM"],
-            "agents": {"mode": "X509"},
-        }
-    }
-    mongodb_multi.update()
-
-    mongodb_multi.assert_reaches_phase(Phase.Running, timeout=1500)
-
-
-@mark.e2e_multi_cluster_tls_with_x509
-def test_mongodb_multi_tls_enable_internal_cluster_x509(
-    mongodb_multi: MongoDBMulti,
-    namespace: str,
-    cluster_certs: str,
-    agent_certs: str,
-):
-    mongodb_multi.load()
-
-    mongodb_multi["spec"]["security"]["authentication"]["internalCluster"] = "X509"
-    mongodb_multi.update()
-
-    # sometimes the agents need more time to register than the time we wait ->
-    # "Failed to enable Authentication for MongoDB Multi Replicaset"
-    # after this the agents eventually succeed.
-    mongodb_multi.assert_reaches_phase(Phase.Running, timeout=5000)
 
 
 @mark.e2e_multi_cluster_tls_with_x509
@@ -183,25 +146,6 @@ def test_ops_manager_state_was_updated_correctly(mongodb_multi: MongoDBMulti):
     ac_tester.assert_authentication_enabled(expected_num_deployment_auth_mechanisms=2)
     ac_tester.assert_authentication_mechanism_enabled("MONGODB-X509")
     ac_tester.assert_internal_cluster_authentication_enabled()
-
-
-@mark.e2e_multi_cluster_tls_with_x509
-def test_rotate_certfile_and_assert_mdb_running(
-    mongodb_multi: MongoDBMulti,
-    namespace: str,
-    central_cluster_client: kubernetes.client.ApiClient,
-):
-    assert_certificate_rotation(central_cluster_client, mongodb_multi, namespace, CLUSTER_BUNDLE_SECRET_NAME)
-
-
-def assert_certificate_rotation(central_cluster_client, mongodb_multi, namespace, certificate_name):
-    cert = Certificate(name=certificate_name, namespace=namespace)
-    cert.api = kubernetes.client.CustomObjectsApi(api_client=central_cluster_client)
-    cert.load()
-    cert["spec"]["dnsNames"].append("foo")  # Append DNS to cert to rotate the certificate
-    cert.update()
-    mongodb_multi.assert_abandons_phase(Phase.Running, timeout=100)
-    mongodb_multi.assert_reaches_phase(Phase.Running, timeout=1500)
 
 
 @mark.e2e_multi_cluster_tls_with_x509
@@ -228,3 +172,17 @@ def test_x509_user_connectivity(
         )
         tester = mongodb_multi.tester()
         tester.assert_x509_authentication(cert_file_name=cert_file.name, tlsCAFile=ca_path)
+
+
+# TODO Replace and use this method to check that certificate rotation after enabling TLS and authentication mechanisms
+# keeps the resources reachable and in Running state.
+def assert_certificate_rotation(central_cluster_client, mongodb_multi, namespace, certificate_name):
+    cert = Certificate(name=certificate_name, namespace=namespace)
+    cert.api = kubernetes.client.CustomObjectsApi(api_client=central_cluster_client)
+    cert.load()
+    cert["spec"]["dnsNames"].append("foo")  # Append DNS to cert to rotate the certificate
+    cert.update()
+    # FIXME the assertions below need to be replaced with a robust check that the agents are ready
+    # and the TLS certificates are rotated.
+    mongodb_multi.assert_abandons_phase(Phase.Running, timeout=100)
+    mongodb_multi.assert_reaches_phase(Phase.Running, timeout=1500)
