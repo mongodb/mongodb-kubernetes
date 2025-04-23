b3e5ba53d22e5d9f6bf372d83f50c5dae1bf393e
Julien Benhaim <julien.benhaim@mongodb.com>
2025-04-23 16:42:42 +0200

    Wait for downscale

--- docker/mongodb-enterprise-tests/tests/upgrades/__init__.py
+++ docker/mongodb-enterprise-tests/tests/upgrades/__init__.py
@@ -1,3 +1,5 @@
+import time
+
 from kubernetes import client
 from kubernetes.client import ApiException
 from tests import test_logger
@@ -9,11 +11,12 @@ logger = test_logger.get_test_logger(__name__)
 # Scale down a deployment to 0. This is useful for upgrade tests as long as they install two different operators in
 # parallel (MEKO and MCK)
 def downscale_operator_deployment(deployment_name: str, namespace: str):
+    DOWNSCALE_TO = 0
     log_deployments_info(namespace)
     logger.info(f"Attempting to downscale deployment '{deployment_name}' in namespace '{namespace}'")
 
     apps_v1 = client.AppsV1Api()
-    body = {"spec": {"replicas": 0}}
+    body = {"spec": {"replicas": DOWNSCALE_TO}}
     # We need to catch not found exception to be future-proof
     try:
         # Attempt to patch the deployment scale
@@ -25,3 +28,25 @@ def downscale_operator_deployment(deployment_name: str, namespace: str):
         else:
             logger.error(f"Unexpected error: {e}")
             raise
+
+    # We should wait for scale down to avoid flakes
+    logger.info(f"Waiting for '{deployment_name}' to scale down to {DOWNSCALE_TO}...")
+    for i in range(10):
+        try:
+            deployment = apps_v1.read_namespaced_deployment(name=deployment_name, namespace=namespace)
+            replicas = deployment.status.replicas or 0
+            ready_replicas = deployment.status.ready_replicas or 0
+            if replicas == 0 and ready_replicas == DOWNSCALE_TO:
+                logger.info(f"'{deployment_name}' successfully downscaled")
+                return
+        except ApiException as e:
+            if e.status == 404:
+                logger.warning(f"'{deployment_name}' not found while waiting for downscale")
+                return
+            else:
+                logger.error(f"Error while waiting for downscale: {e}")
+                raise
+
+        time.sleep(2)
+
+    logger.warning(f"Timeout while waiting for '{deployment_name}' to downscale")
