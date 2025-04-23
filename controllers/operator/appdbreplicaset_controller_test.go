fd533a6d8f5f202ca4268f14904837784fb75a97
Łukasz Sierant <lukasz.sierant@mongodb.com>
2025-04-23 15:04:14 +0200

    Renamed hardcoded repo paths

--- controllers/operator/appdbreplicaset_controller_test.go
+++ controllers/operator/appdbreplicaset_controller_test.go
@@ -54,7 +54,7 @@ func init() {
 
 // getReleaseJsonPath searches for a specified target directory by traversing the directory tree backwards from the current working directory
 func getReleaseJsonPath() (string, error) {
-	repositoryRootDirName := "ops-manager-kubernetes"
+	repositoryRootDirName := "mongodb-kubernetes"
 	releaseFileName := "release.json"
 
 	currentDir, err := os.Getwd()
