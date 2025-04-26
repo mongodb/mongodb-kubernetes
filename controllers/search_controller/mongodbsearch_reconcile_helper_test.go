e57f034f85ada066c1e5b99594c16dc654453277
Yavor Georgiev <yavor.georgiev@mongodb.com>
2025-04-26 16:37:27 +0300

    fix broken commit

--- controllers/search_controller/mongodbsearch_reconcile_helper_test.go
+++ controllers/search_controller/mongodbsearch_reconcile_helper_test.go
@@ -4,8 +4,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	searchv1 "github.com/mongodb/mongodb-kubernetes/api/v1/search"
 	userv1 "github.com/mongodb/mongodb-kubernetes/api/v1/user"
@@ -73,7 +74,6 @@ func TestMongoDBSearchReconcileHelper_ValidateSearchSource(t *testing.T) {
 			}
 		})
 	}
-
 }
 
 func TestMongoDBSearchReconcileHelper_ValidateSingleMongoDBSearchForSearchSource(t *testing.T) {
