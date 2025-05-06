70e1884ed121c00b811440426338434015ef4d01
Nam Nguyen <nam.nguyen@mongodb.com>
2025-05-06 09:33:53 +0200

    fix snippets again

--- scripts/dev/update_docs_snippets.sh
+++ scripts/dev/update_docs_snippets.sh
@@ -54,7 +54,7 @@ function copy_files() {
   mkdir -p "${dst_dir}"
 
   cp -r "${src_dir}/code_snippets" "${dst_dir}"
-  cp -r "${src_dir}/output" "${dst_dir}"
+  cp -r "${src_dir}/output" "${dst_dir}" || true
   cp "${src_dir}/env_variables.sh" "${dst_dir}" || true
 }
 
