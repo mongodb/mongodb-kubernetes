7e53ccde9c96c6a452808b601e7a56e22bdd1c66
Yavor Georgiev <yavor.georgiev@mongodb.com>
2025-05-07 16:41:33 +0200

    revert replica_set_config_map.py change

--- docker/mongodb-kubernetes-tests/tests/replicaset/replica_set_config_map.py
+++ docker/mongodb-kubernetes-tests/tests/replicaset/replica_set_config_map.py
@@ -5,15 +5,21 @@ from kubetester.kubetester import fixture as load_fixture
 from kubetester.mongodb import MongoDB, Phase
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def mdb(namespace: str, custom_mdb_version: str) -> MongoDB:
     resource = MongoDB.from_yaml(load_fixture("replica-set-single.yaml"), namespace=namespace)
     resource.set_version(custom_mdb_version)
-    resource["spec"]["persistent"] = True
-    return resource
+    return resource.update()
 
 
 @pytest.mark.e2e_replica_set_config_map
 def test_create_replica_set(mdb: MongoDB):
-    mdb.update()
     mdb.assert_reaches_phase(Phase.Running)
+
+
+@pytest.mark.e2e_replica_set_config_map
+def test_patch_config_map(namespace: str, mdb: MongoDB):
+    config_map = V1ConfigMap(data={"orgId": "wrongId"})
+    KubernetesTester.clients("corev1").patch_namespaced_config_map("my-project", namespace, config_map)
+    print('Patched the ConfigMap - changed orgId to "wrongId"')
+    mdb.assert_reaches_phase(Phase.Failed, timeout=100)
