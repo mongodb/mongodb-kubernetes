09409f1fe862c6e795edd8de6a7b797d8e51af35
Anand Singh <anand.singh@mongodb.com>
2025-05-27 17:02:01 +0200

    lint fix

--- docker/mongodb-kubernetes-tests/kubetester/oidc.py
+++ docker/mongodb-kubernetes-tests/kubetester/oidc.py
@@ -1,6 +1,5 @@
 import os
 
-
 # Note: The project uses AWS Cognito in the mongodb-mms-testing AWS account to facilitate OIDC authentication testing.
 # This setup includes:
 
@@ -13,6 +12,7 @@ import os
 # are stored in Evergreen and fetched from there during test execution.
 
 # A session explaining the setup can be found here: http://go/k8s-oidc-session
+
 
 def get_cognito_workload_client_id() -> str:
     return os.getenv("cognito_workload_federation_client_id", "")
