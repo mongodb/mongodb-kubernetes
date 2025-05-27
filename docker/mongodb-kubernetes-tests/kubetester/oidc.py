a29f7371040c5ed015ee2430078d779d04b4da90
Anand Singh <anand.singh@mongodb.com>
2025-05-27 17:01:08 +0200

    add explanation for manual aws setup

--- docker/mongodb-kubernetes-tests/kubetester/oidc.py
+++ docker/mongodb-kubernetes-tests/kubetester/oidc.py
@@ -1,6 +1,19 @@
 import os
 
 
+# Note: The project uses AWS Cognito in the mongodb-mms-testing AWS account to facilitate OIDC authentication testing.
+# This setup includes:
+
+# User Pool: A user pool in Cognito manages the identities.
+# Users: We use the user credentials to do authentication.
+# App Client: An app client is configured for machine-to-machine (M2M) authentication.
+# Groups: Cognito groups are used to manage users from the user pool for GroupMembership access.
+
+# Environment variables and secrets required for these tests (like client IDs, URLs, and user IDs, as seen in the Python code)
+# are stored in Evergreen and fetched from there during test execution.
+
+# A session explaining the setup can be found here: http://go/k8s-oidc-session
+
 def get_cognito_workload_client_id() -> str:
     return os.getenv("cognito_workload_federation_client_id", "")
 
