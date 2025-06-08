9589509b1eb5f71897481ab195e3574931762855
Maciej Karaś <maciej.karas@mongodb.com>
2025-06-08 22:11:52 +0200

    Combined Dockerfiles for Init Database and Init AppDB

---
+++ docker/mongodb-kubernetes-init-database/README.md
@@ -0,0 +1,12 @@
+### Building locally
+
+For building the MongoDB Init AppDB image locally use the example command:
+
+```bash
+TAG="1.0.1"
+VERSION="1.0.1"
+MONGODB_TOOLS_URL_UBI="https://downloads.mongodb.org/tools/db/mongodb-database-tools-rhel93-x86_64-100.12.0.tgz"
+docker buildx build --load --progress plain . -f docker/mongodb-kubernetes-init-database/Dockerfile -t "${TAG}" \
+ --build-arg version="${VERSION}" \
+ --build-arg mongodb_tools_url_ubi="${MONGODB_TOOLS_URL_UBI}"
+```
--- lib/sonar/test/test_final_dockerfiles.py
+++ lib/sonar/test/test_final_dockerfiles.py
@@ -18,7 +18,7 @@ def test_build_om_dockerfile():
         inventory="inventories/om.yaml",
     )
 
-#skip("This test case is only used to generate the final Dockerfile for database")
+@skip("This test case is only used to generate the final Dockerfile for database")
 def test_build_database_dockerfile():
     process_image(
         image_name="database",
@@ -30,4 +30,35 @@ def test_build_database_dockerfile():
         },
         build_options={},
         inventory="inventories/database.yaml",
+    )
+
+@skip("This test case is only used to generate the final Dockerfile for init appdb")
+def test_build_init_appdb_dockerfile():
+    process_image(
+        image_name="init-appdb",
+        skip_tags=["release"],
+        include_tags=["final_dockerfile"],
+        build_args={
+            "registry": "localhost:5000",
+            "version": "1.1.0",
+            "is_appdb": True,
+            "mongodb_tools_url_ubi": "https://downloads.mongodb.org/tools/db/mongodb-database-tools-rhel93-x86_64-100.12.0.tgz",
+        },
+        build_options={},
+        inventory="inventories/init_appdb.yaml",
+    )
+
+def test_build_init_database_dockerfile():
+    process_image(
+        image_name="init-database",
+        skip_tags=["release"],
+        include_tags=["final_dockerfile"],
+        build_args={
+            "registry": "localhost:5000",
+            "version": "1.1.0",
+            "is_appdb": False,
+            "mongodb_tools_url_ubi": "https://downloads.mongodb.org/tools/db/mongodb-database-tools-rhel93-x86_64-100.12.0.tgz",
+        },
+        build_options={},
+        inventory="inventories/init_database.yaml",
     )
