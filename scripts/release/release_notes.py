64abb4ad438237b5638e77bf6ccd11f03677b62e
Maciej Karaś <maciej.karas@mongodb.com>
2025-06-11 11:52:05 +0200

    Added tests for release notes generation

--- scripts/release/release_notes.py
+++ scripts/release/release_notes.py
@@ -1,4 +1,3 @@
-import jinja2
 from jinja2 import Template
 
 from scripts.release.changelog import CHANGELOG_PATH, get_changelog_entries, ChangeType
@@ -12,20 +11,21 @@ def generate_release_notes(
 ) -> str:
     """Generate a release notes based on the changes since the previous version tag."""
 
-    with open('scripts/release/release_notes_tpl.md', "r") as f:
-        release_notes = f.read()
-
-    changelog = get_changelog_entries(previous_version, repository_path, changelog_sub_path)
+    changelog: list = get_changelog_entries(previous_version, repository_path, changelog_sub_path)
 
     changelog_entries = list[ChangeType](map(lambda x: x[0], changelog))
     version = calculate_next_release_version(previous_version, changelog_entries)
 
-    with open('scripts/release/release_notes_tpl.md') as f:
+    with open('scripts/release/release_notes_tpl.md', "r") as f:
         template = Template(f.read())
 
     parameters = {
         'version': version,
-        'breaking_changes': [c[1] for c in changelog if c[0] == ChangeType.FEATURE],
+        'prelude': [c[1] for c in changelog if c[0] == ChangeType.PRELUDE],
+        'breaking_changes': [c[1] for c in changelog if c[0] == ChangeType.BREAKING],
+        'features': [c[1] for c in changelog if c[0] == ChangeType.FEATURE],
+        'fixes': [c[1] for c in changelog if c[0] == ChangeType.FIX],
+        'others': [c[1] for c in changelog if c[0] == ChangeType.OTHER],
     }
 
     return template.render(parameters)
