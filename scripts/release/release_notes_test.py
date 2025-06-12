4ebf3ab0c9a1edefa75a62ff8542dbd598b63089
Maciej Karaś <maciej.karas@mongodb.com>
2025-06-12 15:56:14 +0200

    Get the previous version based on current HEAD

--- scripts/release/release_notes_test.py
+++ scripts/release/release_notes_test.py
@@ -12,14 +12,15 @@ def test_generate_release_notes_1_0_0(git_repo: Repo):
 def test_generate_release_notes_1_0_1(git_repo: Repo):
     repo_path = git_repo.working_dir
     git_repo.git.checkout("1.0.1")
-    release_notes = generate_release_notes("1.0.0", repo_path)
+    release_notes = generate_release_notes(repo_path)
     with open("scripts/release/testdata/release_notes_1.0.1.md") as file:
         assert release_notes == file.read()
+
 
 def test_generate_release_notes_1_1_0(git_repo: Repo):
     repo_path = git_repo.working_dir
     git_repo.git.checkout("1.1.0")
-    release_notes = generate_release_notes("1.0.1", repo_path)
+    release_notes = generate_release_notes(repo_path)
     with open("scripts/release/testdata/release_notes_1.1.0.md") as file:
         assert release_notes == file.read()
 
@@ -27,7 +28,7 @@ def test_generate_release_notes_1_1_0(git_repo: Repo):
 def test_generate_release_notes_1_2_0(git_repo: Repo):
     repo_path = git_repo.working_dir
     git_repo.git.checkout("1.2.0")
-    release_notes = generate_release_notes("1.1.0", repo_path)
+    release_notes = generate_release_notes(repo_path)
     with open("scripts/release/testdata/release_notes_1.2.0.md") as file:
         assert release_notes == file.read()
 
@@ -35,62 +36,70 @@ def test_generate_release_notes_1_2_0(git_repo: Repo):
 def test_generate_release_notes_2_0_0(git_repo: Repo):
     repo_path = git_repo.working_dir
     git_repo.git.checkout("2.0.0")
-    release_notes = generate_release_notes("1.2.0", repo_path)
+    release_notes = generate_release_notes(repo_path)
     with open("scripts/release/testdata/release_notes_2.0.0.md") as file:
         assert release_notes == file.read()
+
 
 def test_generate_release_notes_1_2_1(git_repo: Repo):
     repo_path = git_repo.working_dir
     git_repo.git.checkout("1.2.1")
-    release_notes = generate_release_notes("1.2.0", repo_path)
+    release_notes = generate_release_notes(repo_path)
     with open("scripts/release/testdata/release_notes_1.2.1.md") as file:
         assert release_notes == file.read()
+
 
 def test_generate_release_notes_2_0_1(git_repo: Repo):
     repo_path = git_repo.working_dir
     git_repo.git.checkout("2.0.1")
-    release_notes = generate_release_notes("2.0.0", repo_path)
+    release_notes = generate_release_notes(repo_path)
     with open("scripts/release/testdata/release_notes_2.0.1.md") as file:
         assert release_notes == file.read()
+
 
 def test_generate_release_notes_1_2_2(git_repo: Repo):
     repo_path = git_repo.working_dir
     git_repo.git.checkout("1.2.2")
-    release_notes = generate_release_notes("1.2.1", repo_path)
+    release_notes = generate_release_notes(repo_path)
     with open("scripts/release/testdata/release_notes_1.2.2.md") as file:
         assert release_notes == file.read()
+
 
 def test_generate_release_notes_2_0_2(git_repo: Repo):
     repo_path = git_repo.working_dir
     git_repo.git.checkout("2.0.2")
-    release_notes = generate_release_notes("2.0.1", repo_path)
+    release_notes = generate_release_notes(repo_path)
     with open("scripts/release/testdata/release_notes_2.0.2.md") as file:
         assert release_notes == file.read()
+
 
 def test_generate_release_notes_1_2_3(git_repo: Repo):
     repo_path = git_repo.working_dir
     git_repo.git.checkout("1.2.3")
-    release_notes = generate_release_notes("1.2.2", repo_path)
+    release_notes = generate_release_notes(repo_path)
     with open("scripts/release/testdata/release_notes_1.2.3.md") as file:
         assert release_notes == file.read()
+
 
 def test_generate_release_notes_3_0_0(git_repo: Repo):
     repo_path = git_repo.working_dir
     git_repo.git.checkout("3.0.0")
-    release_notes = generate_release_notes("2.0.2", repo_path)
+    release_notes = generate_release_notes(repo_path)
     with open("scripts/release/testdata/release_notes_3.0.0.md") as file:
         assert release_notes == file.read()
+
 
 def test_generate_release_notes_2_0_3(git_repo: Repo):
     repo_path = git_repo.working_dir
     git_repo.git.checkout("2.0.3")
-    release_notes = generate_release_notes("2.0.2", repo_path)
+    release_notes = generate_release_notes(repo_path)
     with open("scripts/release/testdata/release_notes_2.0.3.md") as file:
         assert release_notes == file.read()
+
 
 def test_generate_release_notes_1_2_4(git_repo: Repo):
     repo_path = git_repo.working_dir
     git_repo.git.checkout("1.2.4")
-    release_notes = generate_release_notes("1.2.3", repo_path)
+    release_notes = generate_release_notes(repo_path)
     with open("scripts/release/testdata/release_notes_1.2.4.md") as file:
         assert release_notes == file.read()
--- scripts/release/versioning.py
+++ scripts/release/versioning.py
@@ -1,7 +1,24 @@
 import semver
+from git import Commit, Repo
 
 from scripts.release.changelog import ChangeType
 
+def find_previous_version(initial_version: str, initial_commit_sha: str, repository_path: str = '.', ) -> tuple[str, Commit]:
+    repo = Repo(repository_path)
+    head_commit = repo.head.commit
+
+    # Filter tags that are ancestors of the current HEAD commit
+    ancestor_tags = filter(lambda t: repo.is_ancestor(t.commit, head_commit) and t.commit != head_commit, repo.tags)
+
+    # Filter valid SemVer tags and sort them
+    valid_tags = filter(lambda t: semver.VersionInfo.is_valid(t.name), ancestor_tags)
+    sorted_tags: list = sorted(valid_tags, key=lambda t: semver.VersionInfo.parse(t.name), reverse=True)
+
+    if not sorted_tags:
+        # Find the initial commit by traversing to the earliest commit reachable from HEAD
+        return initial_version, repo.git.rev_parse(initial_commit_sha)
+
+    return sorted_tags[0].name, sorted_tags[0].commit
 
 def calculate_next_release_version(previous_version_str: str, changelog: list[ChangeType]) -> str:
     previous_version = semver.VersionInfo.parse(previous_version_str)
