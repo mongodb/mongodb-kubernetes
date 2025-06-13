b55b7488976634f6180c049144ab4c08a4cc06f1
Maciej Karaś <maciej.karas@mongodb.com>
2025-06-13 16:15:48 +0200

    Added tests, gitgraph, docs and cmd input

--- scripts/release/changelog_test.py
+++ scripts/release/changelog_test.py
@@ -1,10 +1,26 @@
-from git import Repo
-
-import changelog
-from conftest import git_repo
-from scripts.release.changelog import CHANGELOG_PATH
+from changelog import get_change_type, ChangeType
 
 
-def test_get_changelog_entries(git_repo: Repo):
-    repo_path = git_repo.working_dir
-    entries = changelog.get_changelog_entries("1.0.0", repo_path, CHANGELOG_PATH)
+def test_get_change_type():
+
+    # Test prelude
+    assert get_change_type("20250502_prelude_release_notes.md") == ChangeType.PRELUDE
+
+    # Test breaking changes
+    assert get_change_type("20250101_breaking_change_api_update.md") == ChangeType.BREAKING
+    assert get_change_type("20250508_breaking_remove_deprecated.md") == ChangeType.BREAKING
+    assert get_change_type("20250509_major_schema_change.md") == ChangeType.BREAKING
+
+    # Test features
+    assert get_change_type("20250509_feature_new_dashboard.md") == ChangeType.FEATURE
+    assert get_change_type("20250511_feat_add_metrics.md") == ChangeType.FEATURE
+
+    # Test fixes
+    assert get_change_type("20251210_fix_olm_missing_images.md") == ChangeType.FIX
+    assert get_change_type("20251010_bugfix_memory_leak.md") == ChangeType.FIX
+    assert get_change_type("20250302_hotfix_security_issue.md") == ChangeType.FIX
+    assert get_change_type("20250301_patch_typo_correction.md") == ChangeType.FIX
+
+    # Test other
+    assert get_change_type("20250520_docs_update_readme.md") == ChangeType.OTHER
+    assert get_change_type("20250610_refactor_codebase.md") == ChangeType.OTHER
--- scripts/release/release_notes_test.py
+++ scripts/release/release_notes_test.py
@@ -4,102 +4,66 @@ from conftest import git_repo
 from scripts.release.release_notes import generate_release_notes
 
 
+def test_generate_release_notes_before_1_0_0(git_repo: Repo):
+    initial_commit = list(git_repo.iter_commits(reverse=True))[0]
+    git_repo.git.checkout(initial_commit)
+    release_notes = generate_release_notes(git_repo.working_dir)
+    with open("scripts/release/testdata/release_notes_1.0.0_empty.md") as file:
+        assert release_notes == file.read()
+
 def test_generate_release_notes_1_0_0(git_repo: Repo):
-    ## TODO: Does not work for the initial commit
-    assert True
+    checkout_and_assert_release_notes(git_repo, "1.0.0")
 
 
 def test_generate_release_notes_1_0_1(git_repo: Repo):
-    repo_path = git_repo.working_dir
-    git_repo.git.checkout("1.0.1")
-    release_notes = generate_release_notes(repo_path)
-    with open("scripts/release/testdata/release_notes_1.0.1.md") as file:
-        assert release_notes == file.read()
+    checkout_and_assert_release_notes(git_repo, "1.0.1")
 
 
 def test_generate_release_notes_1_1_0(git_repo: Repo):
-    repo_path = git_repo.working_dir
-    git_repo.git.checkout("1.1.0")
-    release_notes = generate_release_notes(repo_path)
-    with open("scripts/release/testdata/release_notes_1.1.0.md") as file:
-        assert release_notes == file.read()
+    checkout_and_assert_release_notes(git_repo, "1.1.0")
 
 
 def test_generate_release_notes_1_2_0(git_repo: Repo):
-    repo_path = git_repo.working_dir
-    git_repo.git.checkout("1.2.0")
-    release_notes = generate_release_notes(repo_path)
-    with open("scripts/release/testdata/release_notes_1.2.0.md") as file:
-        assert release_notes == file.read()
+    checkout_and_assert_release_notes(git_repo, "1.2.0")
 
 
 def test_generate_release_notes_2_0_0(git_repo: Repo):
-    repo_path = git_repo.working_dir
-    git_repo.git.checkout("2.0.0")
-    release_notes = generate_release_notes(repo_path)
-    with open("scripts/release/testdata/release_notes_2.0.0.md") as file:
-        assert release_notes == file.read()
+    checkout_and_assert_release_notes(git_repo, "2.0.0")
 
 
 def test_generate_release_notes_1_2_1(git_repo: Repo):
-    repo_path = git_repo.working_dir
-    git_repo.git.checkout("1.2.1")
-    release_notes = generate_release_notes(repo_path)
-    with open("scripts/release/testdata/release_notes_1.2.1.md") as file:
-        assert release_notes == file.read()
+    checkout_and_assert_release_notes(git_repo, "1.2.1")
 
 
 def test_generate_release_notes_2_0_1(git_repo: Repo):
-    repo_path = git_repo.working_dir
-    git_repo.git.checkout("2.0.1")
-    release_notes = generate_release_notes(repo_path)
-    with open("scripts/release/testdata/release_notes_2.0.1.md") as file:
-        assert release_notes == file.read()
+    checkout_and_assert_release_notes(git_repo, "2.0.1")
 
 
 def test_generate_release_notes_1_2_2(git_repo: Repo):
-    repo_path = git_repo.working_dir
-    git_repo.git.checkout("1.2.2")
-    release_notes = generate_release_notes(repo_path)
-    with open("scripts/release/testdata/release_notes_1.2.2.md") as file:
-        assert release_notes == file.read()
+    checkout_and_assert_release_notes(git_repo, "1.2.2")
 
 
 def test_generate_release_notes_2_0_2(git_repo: Repo):
-    repo_path = git_repo.working_dir
-    git_repo.git.checkout("2.0.2")
-    release_notes = generate_release_notes(repo_path)
-    with open("scripts/release/testdata/release_notes_2.0.2.md") as file:
-        assert release_notes == file.read()
+    checkout_and_assert_release_notes(git_repo, "2.0.2")
 
 
 def test_generate_release_notes_1_2_3(git_repo: Repo):
-    repo_path = git_repo.working_dir
-    git_repo.git.checkout("1.2.3")
-    release_notes = generate_release_notes(repo_path)
-    with open("scripts/release/testdata/release_notes_1.2.3.md") as file:
-        assert release_notes == file.read()
+    checkout_and_assert_release_notes(git_repo, "1.2.3")
 
 
 def test_generate_release_notes_3_0_0(git_repo: Repo):
-    repo_path = git_repo.working_dir
-    git_repo.git.checkout("3.0.0")
-    release_notes = generate_release_notes(repo_path)
-    with open("scripts/release/testdata/release_notes_3.0.0.md") as file:
-        assert release_notes == file.read()
+    checkout_and_assert_release_notes(git_repo, "3.0.0")
 
 
 def test_generate_release_notes_2_0_3(git_repo: Repo):
-    repo_path = git_repo.working_dir
-    git_repo.git.checkout("2.0.3")
-    release_notes = generate_release_notes(repo_path)
-    with open("scripts/release/testdata/release_notes_2.0.3.md") as file:
-        assert release_notes == file.read()
+    checkout_and_assert_release_notes(git_repo, "2.0.3")
 
 
 def test_generate_release_notes_1_2_4(git_repo: Repo):
-    repo_path = git_repo.working_dir
-    git_repo.git.checkout("1.2.4")
-    release_notes = generate_release_notes(repo_path)
-    with open("scripts/release/testdata/release_notes_1.2.4.md") as file:
+    checkout_and_assert_release_notes(git_repo, "1.2.4")
+
+def checkout_and_assert_release_notes(git_repo: Repo, tag: str):
+    git_repo.git.checkout(tag)
+    release_notes = generate_release_notes(git_repo.working_dir)
+    with open(f"scripts/release/testdata/release_notes_{tag}.md") as file:
         assert release_notes == file.read()
