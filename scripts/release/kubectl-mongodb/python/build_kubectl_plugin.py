e669527143503a02450d906f139fb2618e01b8d0
Vivek Singh <vivek.s@mongodb.com>
2025-08-08 19:52:35 +0200

    Make file executable after downloading it from s3

--- scripts/release/kubectl-mongodb/python/build_kubectl_plugin.py
+++ scripts/release/kubectl-mongodb/python/build_kubectl_plugin.py
@@ -4,10 +4,14 @@ import sys
 
 import boto3
 from botocore.exceptions import ClientError, NoCredentialsError, PartialCredentialsError
+from mypy.types import ExtraAttrs
 
 # from lib.base_logger import logger
 
-S3_BUCKET_NAME = "mongodb-kubernetes-dev"
+DEV_S3_BUCKET_NAME = "mongodb-kubernetes-dev"
+STAGING_S3_BUCKET_NAME = "mongodb-kubernetes-staging"
+RELEASE_S3_BUCKET_NAME = "mongodb-kubernetes-release"
+
 AWS_REGION = "eu-north-1"
 S3_BUCKET_KUBECTL_PLUGIN_SUBPATH = "kubectl-mongodb"
 
@@ -15,6 +19,8 @@ COMMIT_SHA_ENV_VAR = "github_commit"
 
 GORELEASER_DIST_DIR = "dist"
 
+# LOCAL_FILE_PATHis the full filename where tests image expects the kuebctl-mongodb binary to be available
+LOCAL_FILE_PATH = "docker/mongodb-kubernetes-tests/multi-cluster-kube-config-creator_linux"
 
 def run_goreleaser():
     try:
@@ -35,7 +41,7 @@ def run_goreleaser():
         print("GoReleaser build completed successfully!")
 
     except FileNotFoundError:
-        print("ERROR: 'goreleaser' command not found. Please ensure GoReleaser is installed and in your system's PATH.")
+        print("ERROR: 'goreleaser' command not found. Please ensure goreleaser is installed and in your system's PATH.")
         sys.exit(1)
     except Exception as e:
         print(f"An unexpected error occurred while running `goreleaser build`: {e}")
@@ -62,10 +68,17 @@ def upload_artifacts_to_s3():
             local_path = os.path.join(root, filename)
             s3_key = s3_path(local_path)
 
-            print(f"Uploading artifact {local_path} to s3://{S3_BUCKET_NAME}/{s3_key}")
+            print(f"Uploading artifact {local_path} to s3://{DEV_S3_BUCKET_NAME}/{s3_key}")
+
+            stat = os.stat(local_path)
+            permissions = str(oct(stat.st_mode)[-3:])
 
             try:
-                s3_client.upload_file(local_path, S3_BUCKET_NAME, s3_key)
+                s3_client.upload_file(local_path, DEV_S3_BUCKET_NAME, s3_key, ExtraArgs={
+                    "Metadata":{
+                        "posix-permissions": permissions
+                    },
+                })
                 print(f"Successfully uploaded the artifact {filename}")
                 uploaded_files += 1
             except Exception as e:
@@ -75,6 +88,9 @@ def upload_artifacts_to_s3():
         print(f"Successfully uploaded {uploaded_files} kubectl-mongodb plugin artifacts to S3.")
 
 
+# s3_path returns the path where the artifacts should be uploaded to in S3 obect store.
+# For dev workflows it's going to be `kubectl-mongodb/{evg-patch-id}/{goreleaser-artifact}`,
+# for staging workflows it would be `kubectl-mongodb/{commit-sha}/{goreleaser-artifact}`.
 def s3_path(local_path: str):
     commit_sha = os.environ.get(COMMIT_SHA_ENV_VAR, "").strip()
     if commit_sha == "":
@@ -97,17 +113,18 @@ def download_plugin_for_tests_image():
         print("Error: The commit sha environment variable is not set. It's required to form the S3 Path.")
         sys.exit(1)
 
-    local_file_path = "docker/mongodb-kubernetes-tests/multi-cluster-kube-config-creator_linux"
+    plugin_path = f"{S3_BUCKET_KUBECTL_PLUGIN_SUBPATH}/{commit_sha}/dist/kubectl-mongodb_linux_amd64_v1/kubectl-mongodb"
 
-    bucket_path = f"{S3_BUCKET_KUBECTL_PLUGIN_SUBPATH}/{commit_sha}/dist/kubectl-mongodb_linux_amd64_v1/kubectl-mongodb"
-    print(f"Downloading s3://{S3_BUCKET_NAME}/{bucket_path} to {local_file_path}")
-
+    print(f"Downloading s3://{DEV_S3_BUCKET_NAME}/{plugin_path} to {LOCAL_FILE_PATH}")
     try:
-        s3_client.download_file(S3_BUCKET_NAME, bucket_path, local_file_path)
-        print(f"Successfully downloaded artifact to {local_file_path}")
+        s3_client.download_file(DEV_S3_BUCKET_NAME, plugin_path, LOCAL_FILE_PATH)
+        # change the file's permissions so that it can be executed
+        os.chmod(LOCAL_FILE_PATH, 0o755)
+
+        print(f"Successfully downloaded artifact to {LOCAL_FILE_PATH}")
     except ClientError as e:
         if e.response['Error']['Code'] == '404':
-            print(f"ERROR: Artifact not found at s3://{S3_BUCKET_NAME}/{bucket_path} ")
+            print(f"ERROR: Artifact not found at s3://{DEV_S3_BUCKET_NAME}/{plugin_path} ")
         else:
             print(f"ERROR: Failed to download artifact. S3 Client Error: {e}")
     except Exception as e:
