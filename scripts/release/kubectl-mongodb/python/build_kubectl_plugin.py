21267428256dcb9af1afd1c1aa80b7ed0ad5bff5
Vivek Singh <vivek.s@mongodb.com>
2025-08-08 20:18:26 +0200

    Precommit

--- scripts/release/kubectl-mongodb/python/build_kubectl_plugin.py
+++ scripts/release/kubectl-mongodb/python/build_kubectl_plugin.py
@@ -22,6 +22,7 @@ GORELEASER_DIST_DIR = "dist"
 # LOCAL_FILE_PATHis the full filename where tests image expects the kuebctl-mongodb binary to be available
 LOCAL_FILE_PATH = "docker/mongodb-kubernetes-tests/multi-cluster-kube-config-creator_linux"
 
+
 def run_goreleaser():
     try:
         command = ["./goreleaser", "build", "--snapshot", "--clean"]
@@ -74,11 +75,14 @@ def upload_artifacts_to_s3():
             permissions = str(oct(stat.st_mode)[-3:])
 
             try:
-                s3_client.upload_file(local_path, DEV_S3_BUCKET_NAME, s3_key, ExtraArgs={
-                    "Metadata":{
-                        "posix-permissions": permissions
+                s3_client.upload_file(
+                    local_path,
+                    DEV_S3_BUCKET_NAME,
+                    s3_key,
+                    ExtraArgs={
+                        "Metadata": {"posix-permissions": permissions},
                     },
-                })
+                )
                 print(f"Successfully uploaded the artifact {filename}")
                 uploaded_files += 1
             except Exception as e:
@@ -94,7 +98,9 @@ def upload_artifacts_to_s3():
 def s3_path(local_path: str):
     commit_sha = os.environ.get(COMMIT_SHA_ENV_VAR, "").strip()
     if commit_sha == "":
-        print(f"Error: The commit sha environment variable {COMMIT_SHA_ENV_VAR} is not set. It's required to form the S3 Path.")
+        print(
+            f"Error: The commit sha environment variable {COMMIT_SHA_ENV_VAR} is not set. It's required to form the S3 Path."
+        )
         sys.exit(1)
 
     return f"{S3_BUCKET_KUBECTL_PLUGIN_SUBPATH}/{commit_sha}/{local_path}"
@@ -106,7 +112,6 @@ def download_plugin_for_tests_image():
     except Exception as e:
         print(f"An error occurred connecting to S3 to download kubectl plugin for tests image: {e}")
         return
-
 
     commit_sha = os.environ.get(COMMIT_SHA_ENV_VAR, "").strip()
     if commit_sha == "":
@@ -123,12 +128,13 @@ def download_plugin_for_tests_image():
 
         print(f"Successfully downloaded artifact to {LOCAL_FILE_PATH}")
     except ClientError as e:
-        if e.response['Error']['Code'] == '404':
+        if e.response["Error"]["Code"] == "404":
             print(f"ERROR: Artifact not found at s3://{DEV_S3_BUCKET_NAME}/{plugin_path} ")
         else:
             print(f"ERROR: Failed to download artifact. S3 Client Error: {e}")
     except Exception as e:
         print(f"An unexpected error occurred during download: {e}")
+
 
 def main():
     run_goreleaser()
