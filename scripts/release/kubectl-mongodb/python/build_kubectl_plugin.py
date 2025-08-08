4f09f41e93ec6d20570f01d9e30dc5a8cb890b8d
Vivek Singh <vivek.s@mongodb.com>
2025-08-08 13:38:16 +0200

    Add evergreen function to promote and release kubectl plugin

--- scripts/release/kubectl-mongodb/python/build_kubectl_plugin.py
+++ scripts/release/kubectl-mongodb/python/build_kubectl_plugin.py
@@ -72,12 +72,23 @@ def upload_artifacts_to_s3(s3_bucket: str, version: str):
     for root, _, files in os.walk(GORELEASER_DIST_DIR):
         for filename in files:
             local_path = os.path.join(root, filename)
+<<<<<<< HEAD
             s3_key = s3_path(local_path, version)
+=======
+            s3_key = s3_path(local_path)
+
+            print(f"Uploading artifact {local_path} to s3://{DEV_S3_BUCKET_NAME}/{s3_key}")
+>>>>>>> a23bed690 (Add evergreen function to promote and release kubectl plugin)
 
             logger.info(f"Uploading artifact {local_path} to s3://{s3_bucket}/{s3_key}")
             try:
+<<<<<<< HEAD
                 s3_client.upload_file(local_path, s3_bucket, s3_key)
                 logger.info(f"Successfully uploaded the artifact {filename}")
+=======
+                s3_client.upload_file(local_path, DEV_S3_BUCKET_NAME, s3_key)
+                print(f"Successfully uploaded the artifact {filename}")
+>>>>>>> a23bed690 (Add evergreen function to promote and release kubectl plugin)
                 uploaded_files += 1
             except Exception as e:
                 logger.debug(f"ERROR: Failed to upload file {filename}: {e}")
@@ -86,12 +97,24 @@ def upload_artifacts_to_s3(s3_bucket: str, version: str):
         logger.info(f"Successfully uploaded {uploaded_files} kubectl-mongodb plugin artifacts to S3.")
 
 
+<<<<<<< HEAD
 # s3_path returns the path where the artifacts should be uploaded to in S3 object store.
 # For dev workflows it's going to be `kubectl-mongodb/{evg-patch-id}/{goreleaser-artifact}`,
 # for staging workflows it would be `kubectl-mongodb/{commit-sha}/{goreleaser-artifact}`.
 # The `version` string has the correct version (either patch id or commit sha), based on the BuildScenario.
 def s3_path(local_path: str, version: str):
     return f"{S3_BUCKET_KUBECTL_PLUGIN_SUBPATH}/{version}/{local_path}"
+=======
+def s3_path(local_path: str):
+    commit_sha = os.environ.get(COMMIT_SHA_ENV_VAR, "").strip()
+    if commit_sha == "":
+        print(
+            f"Error: The commit sha environment variable {COMMIT_SHA_ENV_VAR} is not set. It's required to form the S3 Path."
+        )
+        sys.exit(1)
+
+    return f"{S3_BUCKET_KUBECTL_PLUGIN_SUBPATH}/{commit_sha}/{local_path}"
+>>>>>>> a23bed690 (Add evergreen function to promote and release kubectl plugin)
 
 
 # download_plugin_for_tests_image downloads just the linux amd64 version of the binary and places it
@@ -103,6 +126,7 @@ def download_plugin_for_tests_image(build_scenario: BuildScenario, s3_bucket: st
         logger.debug(f"An error occurred connecting to S3 to download kubectl plugin for tests image: {e}")
         return
 
+<<<<<<< HEAD
     plugin_path = f"{S3_BUCKET_KUBECTL_PLUGIN_SUBPATH}/{version}/dist/kubectl-mongodb_linux_amd64_v1/kubectl-mongodb"
 
     logger.info(f"Downloading s3://{s3_bucket}/{plugin_path} to {LOCAL_KUBECTL_PLUGIN_PATH}")
@@ -115,10 +139,29 @@ def download_plugin_for_tests_image(build_scenario: BuildScenario, s3_bucket: st
     except ClientError as e:
         if e.response["Error"]["Code"] == "404":
             logger.debug(f"ERROR: Artifact not found at s3://{s3_bucket}/{plugin_path} ")
+=======
+    commit_sha = os.environ.get(COMMIT_SHA_ENV_VAR, "").strip()
+    if commit_sha == "":
+        print("Error: The commit sha environment variable is not set. It's required to form the S3 Path.")
+        sys.exit(1)
+
+    local_file_path = "docker/mongodb-kubernetes-tests/multi-cluster-kube-config-creator_linux"
+
+    bucket_path = f"{S3_BUCKET_KUBECTL_PLUGIN_SUBPATH}/{commit_sha}/dist/kubectl-mongodb_linux_amd64_v1/kubectl-mongodb"
+    print(f"Downloading s3://{DEV_S3_BUCKET_NAME}/{bucket_path} to {local_file_path}")
+
+    try:
+        s3_client.download_file(DEV_S3_BUCKET_NAME, bucket_path, local_file_path)
+        print(f"Successfully downloaded artifact to {local_file_path}")
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "404":
+            print(f"ERROR: Artifact not found at s3://{DEV_S3_BUCKET_NAME}/{bucket_path} ")
+>>>>>>> a23bed690 (Add evergreen function to promote and release kubectl plugin)
         else:
             logger.debug(f"ERROR: Failed to download artifact. S3 Client Error: {e}")
     except Exception as e:
         logger.debug(f"An unexpected error occurred during download: {e}")
+
 
 
 def main():
