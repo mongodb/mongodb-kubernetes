0c2ab1de76519fc7d9b67e184189219cafb54326
Nam Nguyen <nam.nguyen@mongodb.com>
2025-08-12 16:15:47 +0200

    try minikube setup

--- scripts/evergreen/e2e/build_e2e_image_ibm.sh
+++ scripts/evergreen/e2e/build_e2e_image_ibm.sh
@@ -8,7 +8,9 @@ cp -rf helm_chart docker/mongodb-kubernetes-tests/helm_chart
 
 echo "Building mongodb-kubernetes-tests image with tag: ${BASE_REPO_URL}/mongodb-kubernetes-tests:${version_id}"
 cd docker/mongodb-kubernetes-tests
-sudo podman buildx build --progress plain . -f Dockerfile -t "${BASE_REPO_URL}/mongodb-kubernetes-tests:${version_id}-$(arch)" --build-arg PYTHON_VERSION="${PYTHON_VERSION}"
-sudo podman push --authfile="/root/.config/containers/auth.json" "${BASE_REPO_URL}/mongodb-kubernetes-tests:${version_id}-$(arch)"
+#sudo podman buildx build --progress plain . -f Dockerfile -t "${BASE_REPO_URL}/mongodb-kubernetes-tests:${version_id}-$(arch)" --build-arg PYTHON_VERSION="${PYTHON_VERSION}"
+#sudo podman push --authfile="/root/.config/containers/auth.json" "${BASE_REPO_URL}/mongodb-kubernetes-tests:${version_id}-$(arch)"
+docker buildx build --progress plain . -f Dockerfile -t "${BASE_REPO_URL}/mongodb-kubernetes-tests:${version_id}-$(arch)" --build-arg PYTHON_VERSION="${PYTHON_VERSION}"
+docker push "${BASE_REPO_URL}/mongodb-kubernetes-tests:${version_id}-$(arch)"
 
 # docker buildx imagetools create "${BASE_REPO_URL}mongodb-kubernetes-tests:${version_id}" --append "${BASE_REPO_URL}mongodb-kubernetes-tests:${version_id}-$(arch)" -t "${BASE_REPO_URL}mongodb-kubernetes-tests:${version_id}"
