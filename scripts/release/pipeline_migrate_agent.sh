c06aaef12ba5a543b425be7fa5b2a1d29735f0b8
Lucian Tosa <lucian.tosa@mongodb.com>
2025-08-25 17:36:03 +0200

    Update registry

--- scripts/release/pipeline_migrate_agent.sh
+++ scripts/release/pipeline_migrate_agent.sh
@@ -5,4 +5,4 @@ set -Eeou pipefail
 scripts/dev/run_python.sh scripts/release/pipeline_main.py --parallel agent \
     --all-agents \
     --build-scenario manual_release \
-    -r 268558157000.dkr.ecr.us-east-1.amazonaws.com/lucian.tosa/mongodb-agent
+    -r quay.io/mongodb/mongodb-agent
