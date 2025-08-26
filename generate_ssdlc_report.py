59ee174b07e2ca9779c88e060b5e398ce2d1730a
Lucian Tosa <lucian.tosa@mongodb.com>
2025-08-26 15:56:06 +0200

    Cleanup

--- generate_ssdlc_report.py
+++ generate_ssdlc_report.py
@@ -97,7 +97,7 @@ def get_supported_images(release: Dict) -> dict[str, SupportedImage]:
     supported_images = convert_to_image_names(supported_images)
     supported_images["mongodb-agent-ubi"] = SupportedImage(
         get_supported_version_for_image("mongodb-agent"),
-        "mongodb-agent-ubi",
+        "mongodb-agent",
         "quay.io/mongodb/mongodb-agent",
         release["supportedImages"]["mongodb-agent"]["ssdlc_name"],
         list(),
