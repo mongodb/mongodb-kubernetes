7d8024f8e6fe82a8348905f5e54b6445c49c7602
Łukasz Sierant <lukasz.sierant@mongodb.com>
2025-09-08 22:36:19 +0200

    CLOUDP-312564: MongoDB Search - Public Preview
    Co-authored-by: Anand Singh <anand.singh@mongodb.com>
    Co-authored-by: Yavor Georgiev <fealebenpae@users.noreply.github.com>

--- controllers/search_controller/mongodbsearch_reconcile_helper.go
+++ controllers/search_controller/mongodbsearch_reconcile_helper.go
@@ -7,30 +7,42 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/blang/semver"
 	"github.com/ghodss/yaml"
 	"go.uber.org/zap"
 	"golang.org/x/xerrors"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	mdbv1 "github.com/mongodb/mongodb-kubernetes/api/v1/mdb"
 	searchv1 "github.com/mongodb/mongodb-kubernetes/api/v1/search"
 	"github.com/mongodb/mongodb-kubernetes/controllers/operator/workflow"
+	"github.com/mongodb/mongodb-kubernetes/mongodb-community-operator/pkg/automationconfig"
 	kubernetesClient "github.com/mongodb/mongodb-kubernetes/mongodb-community-operator/pkg/kube/client"
+	"github.com/mongodb/mongodb-kubernetes/mongodb-community-operator/pkg/kube/container"
+	"github.com/mongodb/mongodb-kubernetes/mongodb-community-operator/pkg/kube/podtemplatespec"
 	"github.com/mongodb/mongodb-kubernetes/mongodb-community-operator/pkg/kube/service"
 	"github.com/mongodb/mongodb-kubernetes/mongodb-community-operator/pkg/mongot"
+	"github.com/mongodb/mongodb-kubernetes/mongodb-community-operator/pkg/tls"
+	"github.com/mongodb/mongodb-kubernetes/pkg/kube"
 	"github.com/mongodb/mongodb-kubernetes/pkg/kube/commoncontroller"
 	"github.com/mongodb/mongodb-kubernetes/pkg/statefulset"
 )
 
 const (
-	MongoDBSearchIndexFieldName = "mdbsearch-for-mongodbresourceref-index"
+	MongoDBSearchIndexFieldName      = "mdbsearch-for-mongodbresourceref-index"
+	unsupportedSearchVersion         = "1.47.0"
+	unsupportedSearchVersionErrorFmt = "MongoDBSearch version %s is not supported because of breaking changes. " +
+		"The operator will ignore this resource: it will not reconcile or reconfigure the workload. " +
+		"Existing deployments will continue to run, but cannot be managed by the operator. " +
+		"To regain operator management, you must delete and recreate the MongoDBSearch resource."
 )
 
 type OperatorSearchConfig struct {
@@ -72,7 +84,11 @@ func (r *MongoDBSearchReconcileHelper) reconcile(ctx context.Context, log *zap.S
 	log = log.With("MongoDBSearch", r.mdbSearch.NamespacedName())
 	log.Infof("Reconciling MongoDBSearch")
 
-	if err := ValidateSearchSource(r.db); err != nil {
+	if err := r.db.Validate(); err != nil {
+		return workflow.Failed(err)
+	}
+
+	if err := r.ValidateSearchImageVersion(); err != nil {
 		return workflow.Failed(err)
 	}
 
@@ -80,39 +96,79 @@ func (r *MongoDBSearchReconcileHelper) reconcile(ctx context.Context, log *zap.S
 		return workflow.Failed(err)
 	}
 
+	keyfileStsModification, err := r.ensureSourceKeyfile(ctx, log)
+	if apierrors.IsNotFound(err) {
+		return workflow.Pending("Waiting for keyfile secret to be created")
+	} else if err != nil {
+		return workflow.Failed(err)
+	}
+
 	if err := r.ensureSearchService(ctx, r.mdbSearch); err != nil {
 		return workflow.Failed(err)
 	}
 
-	mongotConfig := createMongotConfig(r.mdbSearch, r.db)
-	configHash, err := r.ensureMongotConfig(ctx, mongotConfig)
+	ingressTlsMongotModification, ingressTlsStsModification, err := r.ensureIngressTlsConfig(ctx)
 	if err != nil {
 		return workflow.Failed(err)
 	}
 
-	if err := r.createOrUpdateStatefulSet(ctx, log, configHash); err != nil {
+	egressTlsMongotModification, egressTlsStsModification, err := r.ensureEgressTlsConfig(ctx)
+	if err != nil {
 		return workflow.Failed(err)
 	}
 
-	if statefulSetStatus := statefulset.GetStatefulSetStatus(ctx, r.db.NamespacedName().Namespace, r.mdbSearch.StatefulSetNamespacedName().Name, r.client); !statefulSetStatus.IsOK() {
+	configHash, err := r.ensureMongotConfig(ctx, log, createMongotConfig(r.mdbSearch, r.db), ingressTlsMongotModification, egressTlsMongotModification)
+	if err != nil {
+		return workflow.Failed(err)
+	}
+
+	configHashModification := statefulset.WithPodSpecTemplate(podtemplatespec.WithAnnotations(
+		map[string]string{
+			"mongotConfigHash": configHash,
+		},
+	))
+
+	if err := r.createOrUpdateStatefulSet(ctx, log, CreateSearchStatefulSetFunc(r.mdbSearch, r.db, r.buildImageString()), configHashModification, keyfileStsModification, ingressTlsStsModification, egressTlsStsModification); err != nil {
+		return workflow.Failed(err)
+	}
+
+	if statefulSetStatus := statefulset.GetStatefulSetStatus(ctx, r.mdbSearch.Namespace, r.mdbSearch.StatefulSetNamespacedName().Name, r.client); !statefulSetStatus.IsOK() {
 		return statefulSetStatus
 	}
 
 	return workflow.OK()
 }
 
-func (r *MongoDBSearchReconcileHelper) createOrUpdateStatefulSet(ctx context.Context, log *zap.SugaredLogger, mongotConfigHash string) error {
+func (r *MongoDBSearchReconcileHelper) ensureSourceKeyfile(ctx context.Context, log *zap.SugaredLogger) (statefulset.Modification, error) {
+	keyfileSecretName := kube.ObjectKey(r.mdbSearch.GetNamespace(), r.db.KeyfileSecretName())
+	keyfileSecret := &corev1.Secret{}
+	if err := r.client.Get(ctx, keyfileSecretName, keyfileSecret); err != nil {
+		return nil, err
+	}
+
+	return statefulset.Apply(
+		// make sure mongot pods get restarted if the keyfile changes
+		statefulset.WithPodSpecTemplate(podtemplatespec.WithAnnotations(
+			map[string]string{
+				"keyfileHash": hashBytes(keyfileSecret.Data["keyfile"]),
+			},
+		)),
+	), nil
+}
+
+func (r *MongoDBSearchReconcileHelper) buildImageString() string {
 	imageVersion := r.mdbSearch.Spec.Version
 	if imageVersion == "" {
 		imageVersion = r.operatorSearchConfig.SearchVersion
 	}
-	searchImage := fmt.Sprintf("%s/%s:%s", r.operatorSearchConfig.SearchRepo, r.operatorSearchConfig.SearchName, imageVersion)
+	return fmt.Sprintf("%s/%s:%s", r.operatorSearchConfig.SearchRepo, r.operatorSearchConfig.SearchName, imageVersion)
+}
 
+func (r *MongoDBSearchReconcileHelper) createOrUpdateStatefulSet(ctx context.Context, log *zap.SugaredLogger, modifications ...statefulset.Modification) error {
 	stsName := r.mdbSearch.StatefulSetNamespacedName()
 	sts := &appsv1.StatefulSet{ObjectMeta: metav1.ObjectMeta{Name: stsName.Name, Namespace: stsName.Namespace}}
 	op, err := controllerutil.CreateOrUpdate(ctx, r.client, sts, func() error {
-		stsModification := CreateSearchStatefulSetFunc(r.mdbSearch, r.db, searchImage, mongotConfigHash)
-		stsModification(sts)
+		statefulset.Apply(modifications...)(sts)
 		return nil
 	})
 	if err != nil {
@@ -142,7 +198,9 @@ func (r *MongoDBSearchReconcileHelper) ensureSearchService(ctx context.Context, 
 	return nil
 }
 
-func (r *MongoDBSearchReconcileHelper) ensureMongotConfig(ctx context.Context, mongotConfig mongot.Config) (string, error) {
+func (r *MongoDBSearchReconcileHelper) ensureMongotConfig(ctx context.Context, log *zap.SugaredLogger, modifications ...mongot.Modification) (string, error) {
+	mongotConfig := mongot.Config{}
+	mongot.Apply(modifications...)(&mongotConfig)
 	configData, err := yaml.Marshal(mongotConfig)
 	if err != nil {
 		return "", err
@@ -163,13 +221,89 @@ func (r *MongoDBSearchReconcileHelper) ensureMongotConfig(ctx context.Context, m
 		return "", err
 	}
 
-	zap.S().Debugf("Updated mongot config yaml config map: %v (%s) with the following configuration: %s", cmName, op, string(configData))
+	log.Debugf("Updated mongot config yaml config map: %v (%s) with the following configuration: %s", cmName, op, string(configData))
 
-	return hashMongotConfig(configData), nil
+	return hashBytes(configData), nil
 }
 
-func hashMongotConfig(mongotConfigYaml []byte) string {
-	hashBytes := sha256.Sum256(mongotConfigYaml)
+func (r *MongoDBSearchReconcileHelper) ensureIngressTlsConfig(ctx context.Context) (mongot.Modification, statefulset.Modification, error) {
+	if !r.mdbSearch.Spec.Security.TLS.Enabled {
+		mongotModification := func(config *mongot.Config) {
+			config.Server.Wireproto.TLS.Mode = mongot.ConfigTLSModeDisabled
+		}
+		return mongotModification, statefulset.NOOP(), nil
+	}
+
+	// TODO: validate that the certificate in the user-provided Secret in .spec.security.tls.certificateKeySecret is issued by the CA in the operator's CA Secret
+
+	certFileName, err := tls.EnsureTLSSecret(ctx, r.client, r.mdbSearch)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	mongotModification := func(config *mongot.Config) {
+		certPath := tls.OperatorSecretMountPath + certFileName
+		config.Server.Wireproto.TLS.Mode = mongot.ConfigTLSModeTLS
+		config.Server.Wireproto.TLS.CertificateKeyFile = ptr.To(certPath)
+	}
+
+	tlsSecret := r.mdbSearch.TLSOperatorSecretNamespacedName()
+	tlsVolume := statefulset.CreateVolumeFromSecret("tls", tlsSecret.Name)
+	tlsVolumeMount := statefulset.CreateVolumeMount("tls", tls.OperatorSecretMountPath, statefulset.WithReadOnly(true))
+	statefulsetModification := statefulset.WithPodSpecTemplate(podtemplatespec.Apply(
+		podtemplatespec.WithVolume(tlsVolume),
+		podtemplatespec.WithContainer(MongotContainerName, container.Apply(
+			container.WithVolumeMounts([]corev1.VolumeMount{tlsVolumeMount}),
+		)),
+	))
+
+	return mongotModification, statefulsetModification, nil
+}
+
+func (r *MongoDBSearchReconcileHelper) ensureEgressTlsConfig(ctx context.Context) (mongot.Modification, statefulset.Modification, error) {
+	tlsSourceConfig := r.db.TLSConfig()
+	if tlsSourceConfig == nil {
+		return mongot.NOOP(), statefulset.NOOP(), nil
+	}
+
+	mongotModification := func(config *mongot.Config) {
+		config.SyncSource.ReplicaSet.TLS = ptr.To(true)
+	}
+
+	_, containerSecurityContext := podtemplatespec.WithDefaultSecurityContextsModifications()
+	caVolume := tlsSourceConfig.CAVolume
+	trustStoreVolume := statefulset.CreateVolumeFromEmptyDir("cacerts")
+	statefulsetModification := statefulset.WithPodSpecTemplate(podtemplatespec.Apply(
+		podtemplatespec.WithVolume(caVolume),
+		podtemplatespec.WithVolume(trustStoreVolume),
+		podtemplatespec.WithInitContainer("init-cacerts", container.Apply(
+			container.WithImage(r.buildImageString()),
+			containerSecurityContext,
+			container.WithVolumeMounts([]corev1.VolumeMount{
+				statefulset.CreateVolumeMount(caVolume.Name, tls.CAMountPath, statefulset.WithReadOnly(true)),
+				statefulset.CreateVolumeMount(trustStoreVolume.Name, "/java/trust-store", statefulset.WithReadOnly(false)),
+			}),
+			container.WithCommand([]string{"sh"}),
+			container.WithArgs([]string{
+				"-c",
+				fmt.Sprintf(`
+cp /mongot-community/bin/jdk/lib/security/cacerts /java/trust-store/cacerts
+/mongot-community/bin/jdk/bin/keytool -keystore /java/trust-store/cacerts -storepass changeit -noprompt -trustcacerts -importcert -alias mongodcert -file %s/%s
+							`, tls.CAMountPath, tlsSourceConfig.CAFileName),
+			}),
+		)),
+		podtemplatespec.WithContainer(MongotContainerName, container.Apply(
+			container.WithVolumeMounts([]corev1.VolumeMount{
+				statefulset.CreateVolumeMount(trustStoreVolume.Name, "/mongot-community/bin/jdk/lib/security/cacerts", statefulset.WithReadOnly(true), statefulset.WithSubPath("cacerts")),
+			}),
+		)),
+	))
+
+	return mongotModification, statefulsetModification, nil
+}
+
+func hashBytes(bytes []byte) string {
+	hashBytes := sha256.Sum256(bytes)
 	return base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(hashBytes[:])
 }
 
@@ -203,30 +337,67 @@ func buildSearchHeadlessService(search *searchv1.MongoDBSearch) corev1.Service {
 		TargetPort: intstr.FromInt32(search.GetMongotMetricsPort()),
 	})
 
+	serviceBuilder.AddPort(&corev1.ServicePort{
+		Name:       "healthcheck",
+		Protocol:   corev1.ProtocolTCP,
+		Port:       search.GetMongotHealthCheckPort(),
+		TargetPort: intstr.FromInt32(search.GetMongotHealthCheckPort()),
+	})
+
 	return serviceBuilder.Build()
 }
 
-func createMongotConfig(search *searchv1.MongoDBSearch, db SearchSourceDBResource) mongot.Config {
-	return mongot.Config{CommunityPrivatePreview: mongot.CommunityPrivatePreview{
-		MongodHostAndPort:  fmt.Sprintf("%s.%s.svc.cluster.local:%d", db.DatabaseServiceName(), db.GetNamespace(), db.DatabasePort()),
-		QueryServerAddress: fmt.Sprintf("localhost:%d", search.GetMongotPort()),
-		KeyFilePath:        "/mongot/keyfile/keyfile",
-		DataPath:           "/mongot/data/config.yml",
-		Metrics: mongot.Metrics{
+func createMongotConfig(search *searchv1.MongoDBSearch, db SearchSourceDBResource) mongot.Modification {
+	return func(config *mongot.Config) {
+		hostAndPorts := db.HostSeeds()
+
+		config.SyncSource = mongot.ConfigSyncSource{
+			ReplicaSet: mongot.ConfigReplicaSet{
+				HostAndPort:    hostAndPorts,
+				Username:       search.SourceUsername(),
+				PasswordFile:   "/tmp/sourceUserPassword",
+				TLS:            ptr.To(false),
+				ReadPreference: ptr.To("secondaryPreferred"),
+				AuthSource:     ptr.To("admin"),
+			},
+		}
+		config.Storage = mongot.ConfigStorage{
+			DataPath: "/mongot/data/config.yml",
+		}
+		config.Server = mongot.ConfigServer{
+			Wireproto: &mongot.ConfigWireproto{
+				Address: "0.0.0.0:27027",
+				Authentication: &mongot.ConfigAuthentication{
+					Mode:    "keyfile",
+					KeyFile: "/tmp/keyfile",
+				},
+			},
+		}
+		config.Metrics = mongot.ConfigMetrics{
 			Enabled: true,
-			Address: fmt.Sprintf("localhost:%d", search.GetMongotMetricsPort()),
-		},
-		Logging: mongot.Logging{
-			Verbosity: "DEBUG",
-		},
-	}}
+			Address: fmt.Sprintf("0.0.0.0:%d", search.GetMongotMetricsPort()),
+		}
+		config.HealthCheck = mongot.ConfigHealthCheck{
+			Address: fmt.Sprintf("0.0.0.0:%d", search.GetMongotHealthCheckPort()),
+		}
+		config.Logging = mongot.ConfigLogging{
+			Verbosity: "TRACE",
+			LogPath:   nil,
+		}
+	}
 }
 
-func GetMongodConfigParameters(search *searchv1.MongoDBSearch) map[string]interface{} {
-	return map[string]interface{}{
-		"setParameter": map[string]interface{}{
-			"mongotHost":                       mongotHostAndPort(search),
-			"searchIndexManagementHostAndPort": mongotHostAndPort(search),
+func GetMongodConfigParameters(search *searchv1.MongoDBSearch) map[string]any {
+	searchTLSMode := automationconfig.TLSModeDisabled
+	if search.Spec.Security.TLS.Enabled {
+		searchTLSMode = automationconfig.TLSModeRequired
+	}
+	return map[string]any{
+		"setParameter": map[string]any{
+			"mongotHost":                                      mongotHostAndPort(search),
+			"searchIndexManagementHostAndPort":                mongotHostAndPort(search),
+			"skipAuthenticationToSearchIndexManagementServer": false,
+			"searchTLSMode":                                   string(searchTLSMode),
 		},
 	}
 }
@@ -236,27 +407,17 @@ func mongotHostAndPort(search *searchv1.MongoDBSearch) string {
 	return fmt.Sprintf("%s.%s.svc.cluster.local:%d", svcName.Name, svcName.Namespace, search.GetMongotPort())
 }
 
-func ValidateSearchSource(db SearchSourceDBResource) error {
-	version, err := semver.ParseTolerant(db.GetMongoDBVersion())
-	if err != nil {
-		return xerrors.Errorf("error parsing MongoDB version '%s': %w", db.GetMongoDBVersion(), err)
-	} else if version.Major < 8 {
-		return xerrors.New("MongoDB version must be 8.0 or higher")
-	}
-
-	if db.IsSecurityTLSConfigEnabled() {
-		return xerrors.New("MongoDBSearch does not support TLS-enabled sources")
-	}
-
-	return nil
-}
-
 func (r *MongoDBSearchReconcileHelper) ValidateSingleMongoDBSearchForSearchSource(ctx context.Context) error {
+	if r.mdbSearch.Spec.Source != nil && r.mdbSearch.Spec.Source.ExternalMongoDBSource != nil {
+		return nil
+	}
+
+	ref := r.mdbSearch.GetMongoDBResourceRef()
 	searchList := &searchv1.MongoDBSearchList{}
 	if err := r.client.List(ctx, searchList, &client.ListOptions{
-		FieldSelector: fields.OneTermEqualSelector(MongoDBSearchIndexFieldName, r.db.GetNamespace()+"/"+r.db.Name()),
+		FieldSelector: fields.OneTermEqualSelector(MongoDBSearchIndexFieldName, ref.Namespace+"/"+ref.Name),
 	}); err != nil {
-		return xerrors.Errorf("Error listing MongoDBSearch resources for search source '%s': %w", r.db.Name(), err)
+		return xerrors.Errorf("Error listing MongoDBSearch resources for search source '%s': %w", ref.Name, err)
 	}
 
 	if len(searchList.Items) > 1 {
@@ -264,8 +425,92 @@ func (r *MongoDBSearchReconcileHelper) ValidateSingleMongoDBSearchForSearchSourc
 		for i, search := range searchList.Items {
 			resourceNames[i] = search.Name
 		}
-		return xerrors.Errorf("Found multiple MongoDBSearch resources for search source '%s': %s", r.db.Name(), strings.Join(resourceNames, ", "))
+		return xerrors.Errorf(
+			"Found multiple MongoDBSearch resources for search source '%s': %s", ref.Name,
+			strings.Join(resourceNames, ", "),
+		)
 	}
 
 	return nil
+}
+
+func (r *MongoDBSearchReconcileHelper) ValidateSearchImageVersion() error {
+	version := r.getMongotImage()
+
+	if strings.Contains(version, unsupportedSearchVersion) {
+		return xerrors.Errorf(unsupportedSearchVersionErrorFmt, unsupportedSearchVersion)
+	}
+
+	return nil
+}
+
+func (r *MongoDBSearchReconcileHelper) getMongotImage() string {
+	version := strings.TrimSpace(r.mdbSearch.Spec.Version)
+	if version != "" {
+		return version
+	}
+
+	if r.operatorSearchConfig.SearchVersion != "" {
+		return r.operatorSearchConfig.SearchVersion
+	}
+
+	if r.mdbSearch.Spec.StatefulSetConfiguration == nil {
+		return ""
+	}
+
+	for _, container := range r.mdbSearch.Spec.StatefulSetConfiguration.SpecWrapper.Spec.Template.Spec.Containers {
+		if container.Name == MongotContainerName {
+			return container.Image
+		}
+	}
+
+	return ""
+}
+
+func SearchCoordinatorRole() mdbv1.MongoDBRole {
+	// direct translation of https://github.com/10gen/mongo/blob/6f8d95a513eea8f91ea9f5d895dd8a288dfcf725/src/mongo/db/auth/builtin_roles.yml#L652
+	return mdbv1.MongoDBRole{
+		Role: "searchCoordinator",
+		Db:   "admin",
+		Roles: []mdbv1.InheritedRole{
+			{
+				Role: "clusterMonitor",
+				Db:   "admin",
+			},
+			{
+				Role: "directShardOperations",
+				Db:   "admin",
+			},
+			{
+				Role: "readAnyDatabase",
+				Db:   "admin",
+			},
+		},
+		Privileges: []mdbv1.Privilege{
+			{
+				Resource: mdbv1.Resource{
+					Db: "__mdb_internal_search",
+				},
+				Actions: []string{
+					"changeStream", "collStats", "dbHash", "dbStats", "find",
+					"killCursors", "listCollections", "listIndexes", "listSearchIndexes",
+					// performRawDataOperations is available only on mongod master
+					// "performRawDataOperations",
+					"planCacheRead", "cleanupStructuredEncryptionData",
+					"compactStructuredEncryptionData", "convertToCapped", "createCollection",
+					"createIndex", "createSearchIndexes", "dropCollection", "dropIndex",
+					"dropSearchIndex", "insert", "remove", "renameCollectionSameDB",
+					"update", "updateSearchIndex",
+				},
+			},
+			// TODO: this causes the error "(BadValue) resource: {cluster: true} conflicts with resource type 'db'"
+			// {
+			// 	Resource: mdbv1.Resource{
+			// 		Cluster: ptr.To(true),
+			// 	},
+			// 	Actions: []string{"bypassDefaultMaxTimeMS"},
+			// },
+		},
+		AuthenticationRestrictions: nil,
+	}
 }
--- docker/mongodb-kubernetes-tests/tests/search/search_community_external_mongod_basic.py
+++ docker/mongodb-kubernetes-tests/tests/search/search_community_external_mongod_basic.py
@@ -16,18 +16,19 @@ ADMIN_USER_NAME = "mdb-admin-user"
 ADMIN_USER_PASSWORD = "mdb-admin-user-pass"
 
 MONGOT_USER_NAME = "search-sync-source"
-MONGOT_USER_PASSWORD = "search-sync-source-user-password"
+MONGOT_USER_PASSWORD = "search-sync-source-password"
 
 USER_NAME = "mdb-user"
 USER_PASSWORD = "mdb-user-pass"
 
 MDBC_RESOURCE_NAME = "mdbc-rs"
+MDBS_RESOURCE_NAME = "mdbs"
 
 
 @fixture(scope="function")
 def mdbc(namespace: str) -> MongoDBCommunity:
     resource = MongoDBCommunity.from_yaml(
-        yaml_fixture("community-replicaset-sample-mflix.yaml"),
+        yaml_fixture("community-replicaset-sample-mflix-external.yaml"),
         name=MDBC_RESOURCE_NAME,
         namespace=namespace,
     )
@@ -35,52 +36,84 @@ def mdbc(namespace: str) -> MongoDBCommunity:
     if try_load(resource):
         return resource
 
+    mongot_host = f"{MDBS_RESOURCE_NAME}-search-svc.{namespace}.svc.cluster.local:27027"
+    if "additionalMongodConfig" not in resource["spec"]:
+        resource["spec"]["additionalMongodConfig"] = {}
+    if "setParameter" not in resource["spec"]["additionalMongodConfig"]:
+        resource["spec"]["additionalMongodConfig"]["setParameter"] = {}
+
+    resource["spec"]["additionalMongodConfig"]["setParameter"].update(
+        {
+            "mongotHost": mongot_host,
+            "searchIndexManagementHostAndPort": mongot_host,
+            "skipAuthenticationToSearchIndexManagementServer": False,
+            "searchTLSMode": "disabled",
+        }
+    )
+
     return resource
 
 
 @fixture(scope="function")
-def mdbs(namespace: str) -> MongoDBSearch:
+def mdbs(namespace: str, mdbc: MongoDBCommunity) -> MongoDBSearch:
     resource = MongoDBSearch.from_yaml(
         yaml_fixture("search-minimal.yaml"),
+        name=MDBS_RESOURCE_NAME,
         namespace=namespace,
     )
 
-    if try_load(resource):
-        return resource
+    seeds = [
+        f"{mdbc.name}-{i}.{mdbc.name}-svc.{namespace}.svc.cluster.local:27017" for i in range(mdbc["spec"]["members"])
+    ]
+
+    resource["spec"] = {
+        "source": {
+            "external": {
+                "hostAndPorts": seeds,
+                "keyFileSecretRef": {"name": f"{mdbc.name}-keyfile", "key": "keyfile"},
+                "tls": {"enabled": False},
+            },
+            "passwordSecretRef": {"name": f"{MDBC_RESOURCE_NAME}-{MONGOT_USER_NAME}-password", "key": "password"},
+            "username": MONGOT_USER_NAME,
+        }
+    }
 
     return resource
 
 
-@mark.e2e_search_community_basic
+@mark.e2e_search_external_basic
 def test_install_operator(namespace: str, operator_installation_config: dict[str, str]):
     operator = get_default_operator(namespace, operator_installation_config=operator_installation_config)
     operator.assert_is_running()
 
 
-@mark.e2e_search_community_basic
+@mark.e2e_search_external_basic
 def test_install_secrets(namespace: str, mdbs: MongoDBSearch):
     create_or_update_secret(namespace=namespace, name=f"{USER_NAME}-password", data={"password": USER_PASSWORD})
     create_or_update_secret(
         namespace=namespace, name=f"{ADMIN_USER_NAME}-password", data={"password": ADMIN_USER_PASSWORD}
     )
+
     create_or_update_secret(
-        namespace=namespace, name=f"{mdbs.name}-{MONGOT_USER_NAME}-password", data={"password": MONGOT_USER_PASSWORD}
+        namespace=namespace,
+        name=f"{MDBC_RESOURCE_NAME}-{MONGOT_USER_NAME}-password",
+        data={"password": MONGOT_USER_PASSWORD},
     )
 
 
-@mark.e2e_search_community_basic
+@mark.e2e_search_external_basic
 def test_create_database_resource(mdbc: MongoDBCommunity):
     mdbc.update()
     mdbc.assert_reaches_phase(Phase.Running, timeout=1000)
 
 
-@mark.e2e_search_community_basic
-def test_create_search_resource(mdbs: MongoDBSearch):
+@mark.e2e_search_external_basic
+def test_create_search_resource(mdbs: MongoDBSearch, mdbc: MongoDBCommunity):
     mdbs.update()
-    mdbs.assert_reaches_phase(Phase.Running, timeout=300)
+    mdbs.assert_reaches_phase(Phase.Running, timeout=1000)
 
 
-@mark.e2e_search_community_basic
+@mark.e2e_search_external_basic
 def test_wait_for_community_resource_ready(mdbc: MongoDBCommunity):
     mdbc.assert_reaches_phase(Phase.Running, timeout=1800)
 
@@ -92,17 +125,17 @@ def sample_movies_helper(mdbc: MongoDBCommunity) -> SampleMoviesSearchHelper:
     )
 
 
-@mark.e2e_search_community_basic
+@mark.e2e_search_external_basic
 def test_search_restore_sample_database(sample_movies_helper: SampleMoviesSearchHelper):
     sample_movies_helper.restore_sample_database()
 
 
-@mark.e2e_search_community_basic
+@mark.e2e_search_external_basic
 def test_search_create_search_index(sample_movies_helper: SampleMoviesSearchHelper):
     sample_movies_helper.create_search_index()
 
 
-@mark.e2e_search_community_basic
+@mark.e2e_search_external_basic
 def test_search_assert_search_query(sample_movies_helper: SampleMoviesSearchHelper):
     sample_movies_helper.assert_search_query(retry_timeout=60)
 
