af0930e36958b06e1b86428f1d61a9f75acdaa69
Łukasz Sierant <lukasz.sierant@mongodb.com>
2025-09-08 22:43:34 +0200

    Search snippets

---
+++ scripts/code_snippets/tests/test_kind_search_enterprise_snippets.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+test "${MDB_BASH_DEBUG:-0}" -eq 1 && set -x
+
+source scripts/dev/set_env_context.sh
+
+script_name=$(readlink -f "${BASH_SOURCE[0]}")
+
+_SNIPPETS_OUTPUT_DIR="$(dirname "${script_name}")/outputs/$(basename "${script_name%.*}")"
+export _SNIPPETS_OUTPUT_DIR
+mkdir -p "${_SNIPPETS_OUTPUT_DIR}"
+
+dump_logs() {
+  if [[ "${SKIP_DUMP:-"false"}" != "true" ]]; then
+    scripts/evergreen/e2e/dump_diagnostic_information_from_all_namespaces.sh "${K8S_CTX}"
+  fi
+}
+trap dump_logs EXIT
+
+test_dir="./docs/search/02-search-enterprise-deploy"
+source "${test_dir}/env_variables.sh"
+echo "Sourcing env variables for ${CODE_SNIPPETS_FLAVOR} flavor"
+# shellcheck disable=SC1090
+test -f "${test_dir}/env_variables_${CODE_SNIPPETS_FLAVOR}.sh" && source "${test_dir}/env_variables_${CODE_SNIPPETS_FLAVOR}.sh"
+${test_dir}/test.sh
+
+test_dir="./docs/search/03-search-query-usage"
+echo "Sourcing env variables for ${CODE_SNIPPETS_FLAVOR} flavor"
+# shellcheck disable=SC1090
+test -f "${test_dir}/env_variables_${CODE_SNIPPETS_FLAVOR}.sh" && source "${test_dir}/env_variables_${CODE_SNIPPETS_FLAVOR}.sh"
+
+export MDB_CONNECTION_STRING="mongodb://mdb-user:${MDB_USER_PASSWORD}@${MDB_RESOURCE_NAME}-0.${MDB_RESOURCE_NAME}-svc.${MDB_NS}.svc.cluster.local:27017/?replicaSet=${MDB_RESOURCE_NAME}"
+
+${test_dir}/test.sh
