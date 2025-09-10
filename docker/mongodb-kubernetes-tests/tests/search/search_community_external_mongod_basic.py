bee8528654c2d25c2644535d1b7838d564159a19
Yavor Georgiev <yavor.georgiev@mongodb.com>
2025-09-11 02:21:48 +0300

    lower test timeouts

--- docker/mongodb-kubernetes-tests/tests/search/search_community_external_mongod_basic.py
+++ docker/mongodb-kubernetes-tests/tests/search/search_community_external_mongod_basic.py
@@ -104,18 +104,18 @@ def test_install_secrets(namespace: str, mdbs: MongoDBSearch):
 @mark.e2e_search_external_basic
 def test_create_database_resource(mdbc: MongoDBCommunity):
     mdbc.update()
-    mdbc.assert_reaches_phase(Phase.Running, timeout=1000)
+    mdbc.assert_reaches_phase(Phase.Running, timeout=300)
 
 
 @mark.e2e_search_external_basic
 def test_create_search_resource(mdbs: MongoDBSearch, mdbc: MongoDBCommunity):
     mdbs.update()
-    mdbs.assert_reaches_phase(Phase.Running, timeout=1000)
+    mdbs.assert_reaches_phase(Phase.Running, timeout=300)
 
 
 @mark.e2e_search_external_basic
 def test_wait_for_community_resource_ready(mdbc: MongoDBCommunity):
-    mdbc.assert_reaches_phase(Phase.Running, timeout=1800)
+    mdbc.assert_reaches_phase(Phase.Running, timeout=300)
 
 
 @fixture(scope="function")
