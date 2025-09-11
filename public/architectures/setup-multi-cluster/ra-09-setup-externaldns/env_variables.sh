9f64ee6460e78263997500ae3d88c1db2a068698
Łukasz Sierant <lukasz.sierant@mongodb.com>
2025-09-11 11:11:18 +0200

    Fixed DNS sa identifier

--- public/architectures/setup-multi-cluster/ra-09-setup-externaldns/env_variables.sh
+++ public/architectures/setup-multi-cluster/ra-09-setup-externaldns/env_variables.sh
@@ -12,7 +12,18 @@
 #  ${K8S_CLUSTER_2_CONTEXT_NAME}
 #  ${MDB_GKE_PROJECT}
 
-export DNS_SA_NAME="external-dns-sa-${K8S_CLUSTER_SUFFIX:-""}"
+# Example K8S_CLUSTER_SUFFIX when running in evg: "-68c1dc3ffdfb5800075ecee0-26080"
+suffix_short="${K8S_CLUSTER_SUFFIX}"
+# Ensure suffix_short is no longer than 19 characters to make DNS_SA_NAME fit max 30 characters
+if [[ -n "${suffix_short}" && ${#suffix_short} -gt 19 ]]; then
+    # Calculate positions for cutting from the middle
+    prefix_len=$(((19 - 1) / 2))  # -1 for the 'x' replacement
+    suffix_start=$((${#suffix_short} - prefix_len))
+    # Create the truncated version with 'x' in the middle
+    suffix_short="${suffix_short:0:${prefix_len}}x${suffix_short:${suffix_start}}"
+fi
+
+export DNS_SA_NAME="ext-dns-sa${suffix_short}"
 export DNS_SA_EMAIL="${DNS_SA_NAME}@${MDB_GKE_PROJECT}.iam.gserviceaccount.com"
 
 export CUSTOM_DOMAIN="mongodb.custom"
