c4502f69fe07ba9f4d710f9b0dd363a947b172e9
Łukasz Sierant <lukasz.sierant@mongodb.com>
2025-09-12 15:32:18 +0200

    CLOUDP-312564: MongoDB Search Snippets (#426)
    # Summary
    
    <!-- Enter your issue summary here.-->
    
    ## Proof of Work
    
    <!-- Enter your proof that it works here.-->
    
    
    <!-- start git-machete generated -->
    
    # Based on PR #425
    
    ## Chain of upstream PRs as of 2025-09-11
    
    * PR #425:
      `master` ← `search/public-preview-operator`
    
      * **PR #426 (THIS ONE)**:
        `search/public-preview-operator` ← `search/public-preview-snippets`
    
    <!-- end git-machete generated -->
    
    
    ## Checklist
    
    - [ ] Have you linked a jira ticket and/or is the ticket in the title?
    - [ ] Have you checked whether your jira ticket required DOCSP changes?
    - [ ] Have you added changelog file?
        - use `skip-changelog` label if not needed
    - refer to [Changelog files and Release
    Notes](https://github.com/mongodb/mongodb-kubernetes/blob/master/CONTRIBUTING.md#changelog-files-and-release-notes)
    section in CONTRIBUTING.md for more details
    
    ---------
    
    Co-authored-by: Anand Singh <anand.singh@mongodb.com>
    Co-authored-by: Yavor Georgiev <fealebenpae@users.noreply.github.com>
    Co-authored-by: Vivek Singh <vsingh.ggits.2010@gmail.com>
    Co-authored-by: Yavor Georgiev <yavor.georgiev@mongodb.com>

--- docs/community-search/quick-start/code_snippets/0335_show_running_pods.sh
+++
@@ -1,6 +0,0 @@
-echo; echo "MongoDBCommunity resource"
-kubectl --context "${K8S_CLUSTER_0_CONTEXT_NAME}" -n "${MDB_NAMESPACE}" get mdbc/mdbc-rs
-echo; echo "MongoDBSearch resource"
-kubectl --context "${K8S_CLUSTER_0_CONTEXT_NAME}" -n "${MDB_NAMESPACE}" get mdbs/mdbc-rs
-echo; echo "Pods running in cluster ${K8S_CLUSTER_0_CONTEXT_NAME}"
-kubectl --context "${K8S_CLUSTER_0_CONTEXT_NAME}" -n "${MDB_NAMESPACE}" get pods
