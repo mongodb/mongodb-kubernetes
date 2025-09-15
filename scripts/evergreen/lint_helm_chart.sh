fb69e77a99a60791db5e8a8490ca152f9357131e
Vivek Singh <vivek.s@mongodb.com>
2025-09-15 20:12:15 +0200

    Add a makefile target `make lint-chart` to lint the charts locally

--- scripts/evergreen/lint_helm_chart.sh
+++ scripts/evergreen/lint_helm_chart.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 set -Eeou pipefail
 
+if ! command -v ct &> /dev/null; then
+  echo "Error: 'ct' command not found in PATH. Please download it from here https://github.com/helm/chart-testing" >&2
+  exit 1
+fi
+
 source scripts/dev/set_env_context.sh
+
+if [ -z "$PROJECT_DIR" ]; then
+  echo "Error: PROJECT_DIR environment variable is not set. Please set a context or set it to your MCK repo manually." >&2
+  exit 1
+fi
 
 export PATH=${PROJECT_DIR}/venv/bin:${PATH}
 
