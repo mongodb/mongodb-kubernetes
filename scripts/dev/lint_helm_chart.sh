557a472ea44114048c4525047df8b84718040d94
Vivek Singh <vivek.s@mongodb.com>
2025-09-19 18:48:23 +0200

    Add a comment, fix order of statements

--- scripts/dev/lint_helm_chart.sh
+++ scripts/dev/lint_helm_chart.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 set -Eeou pipefail
 
+source scripts/dev/set_env_context.sh
+
 if ! command -v ct &> /dev/null; then
   echo "Error: 'ct' command not found in PATH. Please download it from here https://github.com/helm/chart-testing" >&2
   exit 1
 fi
-
-source scripts/dev/set_env_context.sh
 
 if [ -z "${PROJECT_DIR}" ]; then
   echo "Error: PROJECT_DIR environment variable is not set. Please set a context or set it (PROJECT_DIR var) to your local MCK repo manually." >&2
   exit 1
 fi
 
+# the binaries yamale and yamllint required by ct are available at `${PROJECT_DIR}/venv/bin`
 export PATH=${PROJECT_DIR}/venv/bin:${PATH}
 
 ct lint --charts="${PROJECT_DIR}/helm_chart/" \
