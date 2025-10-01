2eff0cf5c7422ec4704d76df74a420d3c74e7bc3
Vivek Singh <vivek.s@mongodb.com>
2025-10-01 14:10:56 +0200

    Fix linting `ct lint` issue in helm chart

--- scripts/dev/lint_helm_chart.sh
+++ scripts/dev/lint_helm_chart.sh
@@ -16,6 +16,11 @@ fi
 # the binaries yamale and yamllint required by ct are available at `${PROJECT_DIR}/venv/bin`
 export PATH=${PROJECT_DIR}/venv/bin:${PATH}
 
+helm template "${PROJECT_DIR}/helm_chart/" \
+  -f "${PROJECT_DIR}/helm_chart/values.yaml" \
+  -f "${PROJECT_DIR}/helm_chart/values-openshift.yaml" \
+  -f "${PROJECT_DIR}/helm_chart/values-multi-cluster.yaml"
+
 ct lint --charts="${PROJECT_DIR}/helm_chart/" \
     --chart-yaml-schema "${PROJECT_DIR}/helm_chart/tests/schemas/chart_schema.yaml" \
     --lint-conf "${PROJECT_DIR}/helm_chart/tests/schemas/lintconf.yaml"
