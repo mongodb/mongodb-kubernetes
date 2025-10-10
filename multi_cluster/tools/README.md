592f35d1bb9d743264d4f4718eabc4a660d2c8b0
Maciej Karaś <maciej.karas@mongodb.com>
2025-10-10 14:18:14 +0200

    Bump istio version because of the issue with CNI -> https://github.com/istio/istio/issues/29511

--- multi_cluster/tools/README.md
+++ multi_cluster/tools/README.md
@@ -5,12 +5,12 @@ The script is intended to install Istio in the multi E2E clusters that we have c
 Steps to run the script and verify it:
 
 * Install the istioctl binary:
-  `curl -sL https://istio.io/downloadIstioctl | ISTIO_VERSION=1.9.1 sh -`
+  `curl -sL https://istio.io/downloadIstioctl | ISTIO_VERSION=1.27.1 sh -`
   `export PATH=$PATH:$HOME/.istioctl/bin`
 
 * Export cluster variables:
   `export CTX_CLUSTER1=e2e.cluster1.mongokubernetes.com`
-   
+
    `export CTX_CLUSTER2=e2e.cluster2.mongokubernetes.com `
 
 
--- multi_cluster/tools/install_istio.sh
+++ multi_cluster/tools/install_istio.sh
@@ -5,7 +5,7 @@ set -eux
 export CTX_CLUSTER1=${CTX_CLUSTER1:-e2e.cluster1.mongokubernetes.com}
 export CTX_CLUSTER2=${CTX_CLUSTER2:-e2e.cluster2.mongokubernetes.com}
 export CTX_CLUSTER3=${CTX_CLUSTER3:-e2e.cluster3.mongokubernetes.com}
-export VERSION=${VERSION:-1.12.8}
+export VERSION=${VERSION:-1.27.1}
 
 IS_KIND="false"
 if [[ $CTX_CLUSTER1 = kind* ]]; then
@@ -127,9 +127,6 @@ spec:
   meshConfig:
     defaultConfig:
       terminationDrainDuration: 30s
-      proxyMetadata:
-        ISTIO_META_DNS_AUTO_ALLOCATE: "true"
-        ISTIO_META_DNS_CAPTURE: "true"
   values:
     global:
       meshID: mesh1
