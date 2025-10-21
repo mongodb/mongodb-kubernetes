344001d6d6c2035f4599834e9f47ce5a76fefb5a
Julien Benhaim <julien.benhaim@mongodb.com>
2025-10-21 15:17:12 +0200

    Retry all errors

--- scripts/evergreen/lint_code.sh
+++ scripts/evergreen/lint_code.sh
@@ -7,7 +7,7 @@ required_version="v2.0.2"
 # Install or update golangci-lint if not installed or version is incorrect
 if ! [[ -x "$(command -v golangci-lint)" ]]; then
     echo "Installing/updating golangci-lint to version ${required_version}..."
-    curl --retry 5 --retry-delay 3 --fail --show-error --max-time 180 -sSL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin "${required_version}"
+    curl --retry 5 --retry-delay 3 --retry-all-errors --fail --show-error --max-time 180 -sSL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin "${required_version}"
 else
     echo "golangci-lint is already installed"
 fi
