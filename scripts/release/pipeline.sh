18f79ff3fc1f0a9f7677deb9e7173e36692d5aa6
Maciej Karaś <6159874+MaciejKaras@users.noreply.github.com>
2025-10-23 13:57:27 +0200

    CLOUDP-295785 - simplify pipeline.sh script and use it for all images (#543)
    # Summary
    
    Combines all `pipeline.py` invocations into single `pipeline.sh` script
    which makes it easier to maintain and reduces amount of future bugs. The
    same principle was applied in `.evergreen-functions.yml`. Now we have
    only single `pipeline` evergreen function. For evergreen extensions I've
    decided to stick with lower-case variable naming. This is aligning
    better with existing evg functions + less conflicts with context
    variables.
    
    This also fixes the bug, where the `BUILD_SCENARIO: release` for OM and
    agent release tasks was not properly overriding the default one from
    `evg_private_context`.
    
    ## Proof of Work
    
    1. Custom
    [rebuild_currently_used_agents](https://spruce.mongodb.com/task/mongodb_kubernetes_manual_release_agent_currently_used_rebuild_currently_used_agents_patch_fd4450bdd5aab857c0b9570cfd1cb4f3b8bb42f8_68f7626ffca1f10007b316df_25_10_21_10_37_36/logs?execution=0)
    task with flags was successful
    2. [Image release
    tasks](https://spruce.mongodb.com/version/68f76366a6e778000773cdfd/tasks?page=0&sorts=STATUS%3AASC%3BBASE_STATUS%3ADESC&variant=%5Erelease_images%24)
    where successful and released images to quay.io with correct versions
    3. Published OM and agent images using fixed
    [tasks](http://spruce.mongodb.com/version/68f76720b122440007395905/tasks?page=0&sorts=STATUS%3AASC%3BBASE_STATUS%3ADESC&variant=%5Epublish_om80_images%24)
    
    ## Checklist
    
    - [x] Have you linked a jira ticket and/or is the ticket in the title?
    - [x] Have you checked whether your jira ticket required DOCSP changes?
    - [x] Have you added changelog file?
        - use `skip-changelog` label if not needed
    - refer to [Changelog files and Release
    Notes](https://github.com/mongodb/mongodb-kubernetes/blob/master/CONTRIBUTING.md#changelog-files-and-release-notes)
    section in CONTRIBUTING.md for more details

--- scripts/release/pipeline.sh
+++ scripts/release/pipeline.sh
@@ -4,4 +4,38 @@ set -Eeou pipefail
 
 source scripts/dev/set_env_context.sh
 
-scripts/dev/run_python.sh scripts/release/pipeline.py "${IMAGE_NAME}" --build-scenario "${BUILD_SCENARIO}" --version "${OPERATOR_VERSION}" ${FLAGS:+${FLAGS}}
+args=("${IMAGE_NAME}")
+args+=(--build-scenario "${BUILD_SCENARIO_OVERRIDE:-${BUILD_SCENARIO}}")
+
+case ${IMAGE_NAME} in
+  "agent")
+    IMAGE_VERSION=""
+    ;;
+
+  "ops-manager")
+    IMAGE_VERSION="${OM_VERSION}"
+    ;;
+
+  "readiness-probe")
+    IMAGE_VERSION="${READINESS_PROBE_VERSION}"
+    ;;
+
+  "upgrade-hook")
+    IMAGE_VERSION="${VERSION_UPGRADE_HOOK_VERSION}"
+    ;;
+
+  *)
+    IMAGE_VERSION="${OPERATOR_VERSION}"
+    ;;
+esac
+
+if [[ "${IMAGE_VERSION:-}" != "" ]]; then
+    args+=(--version "${IMAGE_VERSION}")
+fi
+
+if [[ "${FLAGS:-}" != "" ]]; then
+    IFS=" " read -ra flags <<< "${FLAGS}"
+    args+=("${flags[@]}")
+fi
+
+scripts/dev/run_python.sh scripts/release/pipeline.py "${args[@]}"
