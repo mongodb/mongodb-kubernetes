ca7881cb059a18ca61df713c3e25f7fb96d332a5
Maciej Karaś <maciej.karas@mongodb.com>
2025-10-28 15:13:30 +0100

    Fixes after merge with master

--- scripts/release/oci_chart_info.py
+++ scripts/release/oci_chart_info.py
@@ -2,13 +2,12 @@ import argparse
 import json
 from dataclasses import asdict
 
+from scripts.release.argparse_utils import get_scenario_from_arg
 from scripts.release.build.build_info import load_build_info
-from scripts.release.build.build_scenario import BuildScenario
+from scripts.release.build.build_scenario import SUPPORTED_SCENARIOS, BuildScenario
 
 
 def main():
-    supported_scenarios = list(BuildScenario)
-
     parser = argparse.ArgumentParser(
         description="""Dump the Helm chart information for the 'mongodb-kubernetes' chart as JSON.""",
         formatter_class=argparse.RawTextHelpFormatter,
@@ -20,13 +19,13 @@ def main():
         action="store",
         required=True,
         type=str,
-        choices=supported_scenarios,
+        choices=SUPPORTED_SCENARIOS,
         help=f"""Build scenario when reading configuration from 'build_info.json'.
-    Options: {", ".join(supported_scenarios)}. For '{BuildScenario.DEVELOPMENT}' the '{BuildScenario.PATCH}' scenario is used to read values from 'build_info.json'""",
+    Options: {", ".join(SUPPORTED_SCENARIOS)}. For '{BuildScenario.DEVELOPMENT}' the '{BuildScenario.PATCH}' scenario is used to read values from 'build_info.json'""",
     )
     args = parser.parse_args()
 
-    build_scenario = BuildScenario(args.build_scenario)
+    build_scenario = get_scenario_from_arg(args.build_scenario)
     build_info = load_build_info(build_scenario)
     chart_info = build_info.helm_charts["mongodb-kubernetes"]
 
