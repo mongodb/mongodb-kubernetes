c97c7a0ad61f2cbfc25fed0dcff1799fc9f180cf
Vivek Singh <vsingh.ggits.2010@gmail.com>
2025-12-05 14:51:44 +0100

    [CLOUDP-361632] Add information about latest OM and agent in release.json (#622)
    # Summary
    
    We have been getting some request from the customers where they wanted
    to know the combination of the container images that they require to run
    a specific version of operator and Mongo DB instance. As part of that
    work we are going to add a new file (release_info.json) as the assets in
    the GitHub release.
    That file is going to have details about the latest released images.
    These images are going to be the images that MCK publishes as well as
    the latest OM and Agent version mappings. To enable that, as part of
    this PR we are adding a new field in the `release.json` that has the
    mapping between OM version and agent version of latest OM releases. This
    new field in the `release.json` is going to have the opsManaget to agent
    image version mapping for every major opsmanager version.
    We are adding this information in the `release.json` so that it can
    easily be used later (in next PR), to create the release_info.json file.
    This PR also updated the precommit hook so that when new version of OM
    is released, the newly added field of release.json would properly be
    updated with newly release OM/agent.
    
    ## Proof of Work
    
    Manual: 
    - Mimic new release of OM by adding a new version (8.0.17) to
    `supportedImages.ops-manager.versions` and respective entry in
    `mongodb-agent.opsManagerMapping`. Run `make precommit` and verify that
    the `supportedImages.latestOpsManagerAgentMapping` has been updated for
    major version 8 with updated versions.
    - Mimic the new major version release of OM by adding new version
    (9.0.1) to `supportedImages.ops-manager.versions` and respective entry
    in `mongodb-agent.opsManagerMapping`. Run `make precommit` and verify
    that a new entry for major version 9 has been added to
    `supportedImages.latestOpsManagerAgentMapping`.
    
    ## Checklist
    
    - [x] Have you linked a jira ticket and/or is the ticket in the title?
    - [x] Have you checked whether your jira ticket required DOCSP changes?
    - [ ] Have you added changelog file? (will show up later)
        - use `skip-changelog` label if not needed
    - refer to [Changelog files and Release
    Notes](https://github.com/mongodb/mongodb-kubernetes/blob/master/CONTRIBUTING.md#changelog-files-and-release-notes)
    section in CONTRIBUTING.md for more details

--- scripts/evergreen/release/update_release.py
+++ scripts/evergreen/release/update_release.py
@@ -31,6 +31,9 @@ def update_release_json():
     newest_operator_version = data["mongodbOperator"]
     update_operator_related_versions(data, newest_operator_version)
 
+    # Adds mapping between latest major version of OM and agent to the release.json
+    update_latest_om_agent_mapping(data, newest_om_version)
+
     with open(release, "w") as f:
         json.dump(
             data,
@@ -38,6 +41,52 @@ def update_release_json():
             indent=2,
         )
         f.write("\n")
+
+
+def update_latest_om_agent_mapping(data, new_om_version):
+    """
+    Updates the 'latestOpsManagerAgentMapping' in release.json with
+    newly released Ops Manager version and its corresponding Agent version.
+
+    If a OM's major version entry already exists, it updates the 'opsManagerVersion'
+    for that entry. Otherwise, it adds a new entry for the major version.
+
+    Args:
+        data (dict): The complete configuration dictionary.
+        new_om_version (str): The new Ops Manager version (e.g., "8.0.11").
+    """
+
+    try:
+        om_agent_mapping = data["latestOpsManagerAgentMapping"]
+    except KeyError:
+        logger.error("Error: 'latestOpsManagerAgentMapping' field not found in the release.json data.")
+
+    new_agent_version = data["supportedImages"]["mongodb-agent"]["opsManagerMapping"]["ops_manager"][new_om_version][
+        "agent_version"
+    ]
+
+    try:
+        new_om_major_version = new_om_version.split(".")[0]
+    except IndexError:
+        logger.error(f"Error: Invalid version format for new_om_version: {new_om_version}")
+
+    new_om_agent_mapping = {"opsManagerVersion": new_om_version, "agentVersion": new_agent_version}
+
+    new_entry = {new_om_major_version: new_om_agent_mapping}
+
+    major_version_found = False
+    for mapping in om_agent_mapping:
+        if new_om_major_version in mapping:
+            # Update the existing entry
+            mapping[new_om_major_version] = new_om_agent_mapping
+            major_version_found = True
+            logger.info(f"Updated existing entry for major version '{new_om_major_version}' to {new_om_version}.")
+            break
+
+    # this is new major version of OM, a new entry will be added
+    if not major_version_found:
+        om_agent_mapping.append(new_entry)
+        logger.info(f"Added new entry for major version '{new_om_major_version}' with version {new_om_version}.")
 
 
 def update_operator_related_versions(release: dict, version: str):
