e46d5edf18c9fb9931a599e71cb44d16767e9dbf
Łukasz Sierant <lukasz.sierant@mongodb.com>
2026-01-13 13:01:16 +0100

    Debug logs to check failing license

--- scripts/evergreen/update_licenses.sh
+++ scripts/evergreen/update_licenses.sh
@@ -3,6 +3,7 @@
 set -Eeou pipefail
 source scripts/dev/set_env_context.sh
 
+go version
 go install github.com/google/go-licenses@v1.6.0
 
 # Define the root of the repo and the scripts directory
@@ -20,7 +21,11 @@ process_licenses() {
         return 1
     fi
 
+    echo "Go licenses version"
+
     PATH=$(go env GOPATH)/bin:${PATH} GOOS=linux GOARCH=amd64 GOFLAGS="-mod=mod" go-licenses report . --template "${SCRIPTS_DIR}/update_licenses.tpl" > licenses_full.csv 2> licenses_stderr  || true
+
+    cat licenses_stderr
 
     # Filter and sort the licenses report
     grep -v 10gen licenses_full.csv | grep -v "github.com/mongodb" | grep -v "^golang.org" | sort > LICENSE-THIRD-PARTY || true
