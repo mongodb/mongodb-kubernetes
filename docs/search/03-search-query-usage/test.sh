4a52de47d9b8b2801311c8d7e54f7d5da40785d3
Vivek Singh <vivek.s@mongodb.com>
2026-01-14 22:03:20 +0100

    Fix lint issue, handle env var properly

--- docs/search/03-search-query-usage/test.sh
+++ docs/search/03-search-query-usage/test.sh
@@ -17,7 +17,7 @@ run 03_0430_create_search_index.sh
 run 03_0435_create_vector_search_index.sh
 # don't create auto embedding vector search if it's not community deployment.
 # since we set EMBEDDING_MODEL env var just in community, we can check that env var.
-if [ -n "$EMBEDDING_MODEL" ]; then
+if [ -n "${EMBEDDING_MODEL:-}" ]; then
     run 03_0437_create_auto_embed_vector_search_index.sh
 else
     echo "Skipping creation of autoEmbed vector search index for non community deployments."
@@ -28,7 +28,7 @@ run_for_output 03_0445_list_vector_search_indexes.sh
 run_for_output 03_0447_list_auto_embed_vector_search_indexes.sh
 run_for_output 03_0450_execute_search_query.sh
 run_for_output 03_0455_execute_vector_search_query.sh
-if [ -n "$EMBEDDING_MODEL" ]; then
+if [ -n "${EMBEDDING_MODEL:-}" ]; then
     run_for_output 03_0456_execute_auto_embed_vector_search_query.sh
 else
     echo "Skipping running autoEmbed vector search query for non community deployments."
