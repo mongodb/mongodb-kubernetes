d460a686251e1edf8eb777e1ed47e3de8fe8987c
Vivek Singh <vsingh.ggits.2010@gmail.com>
2026-03-11 16:20:02 +0100

    Add E2E test for single search instance with external enterprise RS (#870)

---
+++ docker/mongodb-kubernetes-tests/tests/search/replicaset_external_mongodb_single_mongot.py
@@ -0,0 +1,180 @@
+from kubetester.certs import create_mongodb_tls_certs, create_tls_certs
+from kubetester.mongodb import MongoDB
+from kubetester.mongodb_search import MongoDBSearch
+from kubetester.mongodb_user import MongoDBUser
+from kubetester.omtester import skip_if_cloud_manager
+from kubetester.phase import Phase
+from pytest import fixture, mark
+from tests import test_logger
+from tests.common.mongodb_tools_pod import mongodb_tools_pod
+from tests.common.search import movies_search_helper, search_resource_names
+from tests.common.search.movies_search_helper import SampleMoviesSearchHelper
+from tests.common.search.replicaset_search_helper import verify_rs_mongod_parameters, verify_vector_search
+from tests.common.search.search_deployment_helper import SearchDeploymentHelper
+from tests.common.search.search_tester import SearchTester
+from tests.common.search.sharded_search_helper import create_sharded_ca
+from tests.conftest import get_default_operator, get_issuer_ca_filepath
+from tests.search.om_deployment import get_ops_manager
+
+logger = test_logger.get_test_logger(__name__)
+
+MDB_RESOURCE_NAME = "mdb-rs-ext"
+MDBS_RESOURCE_NAME = MDB_RESOURCE_NAME
+
+ADMIN_USER_NAME = "mdb-admin-user"
+ADMIN_USER_PASSWORD = "mdb-admin-user-pass"
+
+MONGOT_USER_NAME = "search-sync-source"
+MONGOT_USER_PASSWORD = "search-sync-source-user-password"
+
+USER_NAME = "mdb-user"
+USER_PASSWORD = "mdb-user-pass"
+
+CA_CONFIGMAP_NAME = f"{MDB_RESOURCE_NAME}-ca"
+MDBS_TLS_SECRET_NAME = search_resource_names.mongot_tls_cert_name(MDB_RESOURCE_NAME)
+
+
+@fixture(scope="module")
+def ca_configmap(issuer_ca_filepath: str, namespace: str) -> str:
+    return create_sharded_ca(issuer_ca_filepath, namespace, CA_CONFIGMAP_NAME)
+
+
+@fixture(scope="function")
+def helper(namespace: str) -> SearchDeploymentHelper:
+    return SearchDeploymentHelper(
+        namespace=namespace,
+        mdb_resource_name=MDB_RESOURCE_NAME,
+        mdbs_resource_name=MDBS_RESOURCE_NAME,
+        ca_configmap_name=CA_CONFIGMAP_NAME,
+    )
+
+
+@fixture(scope="function")
+def mdb(namespace: str, ca_configmap: str, issuer_ca_configmap: str, helper: SearchDeploymentHelper) -> MongoDB:
+    mongot_host = search_resource_names.mongot_service_host(MDBS_RESOURCE_NAME, namespace, 27028)
+    return helper.create_replicaset_mdb(
+        mongot_host=mongot_host,
+        issuer_ca_configmap=issuer_ca_configmap,
+        tls_cert_prefix="certs",
+    )
+
+
+@fixture(scope="function")
+def mdbs(namespace: str, helper: SearchDeploymentHelper) -> MongoDBSearch:
+    resource = helper.mdbs_for_ext_rs_source(mongot_user_name=MONGOT_USER_NAME)
+    resource["spec"]["security"] = {"tls": {"certificateKeySecretRef": {"name": MDBS_TLS_SECRET_NAME}}}
+    return resource
+
+
+@fixture(scope="function")
+def admin_user(helper: SearchDeploymentHelper) -> MongoDBUser:
+    return helper.admin_user_resource(ADMIN_USER_NAME)
+
+
+@fixture(scope="function")
+def user(helper: SearchDeploymentHelper) -> MongoDBUser:
+    return helper.user_resource(USER_NAME)
+
+
+@fixture(scope="function")
+def mongot_user(helper: SearchDeploymentHelper, mdbs: MongoDBSearch) -> MongoDBUser:
+    return helper.mongot_user_resource(mdbs, MONGOT_USER_NAME)
+
+
+@mark.e2e_search_external_rs_single_mongot
+def test_install_operator(namespace: str, operator_installation_config: dict[str, str]):
+    operator = get_default_operator(namespace, operator_installation_config=operator_installation_config)
+    operator.assert_is_running()
+
+
+@mark.e2e_search_external_rs_single_mongot
+@skip_if_cloud_manager
+def test_create_ops_manager(namespace: str):
+    ops_manager = get_ops_manager(namespace)
+    ops_manager.update()
+    ops_manager.om_status().assert_reaches_phase(Phase.Running, timeout=1200)
+    ops_manager.appdb_status().assert_reaches_phase(Phase.Running, timeout=600)
+
+
+@mark.e2e_search_external_rs_single_mongot
+def test_install_tls_certificates(namespace: str, mdb: MongoDB, mdbs: MongoDBSearch, issuer: str):
+    create_mongodb_tls_certs(issuer, namespace, mdb.name, f"certs-{mdb.name}-cert", mdb.get_members())
+
+    search_service_name = search_resource_names.mongot_service_name(mdbs.name)
+    create_tls_certs(
+        issuer,
+        namespace,
+        search_resource_names.mongot_statefulset_name(mdbs.name),
+        replicas=1,
+        service_name=search_service_name,
+        additional_domains=[f"{search_service_name}.{namespace}.svc.cluster.local"],
+        secret_name=MDBS_TLS_SECRET_NAME,
+    )
+
+
+@mark.e2e_search_external_rs_single_mongot
+def test_create_database_resource(mdb: MongoDB):
+    mdb.update()
+    mdb.assert_reaches_phase(Phase.Running, timeout=300)
+
+
+@mark.e2e_search_external_rs_single_mongot
+def test_create_users(
+    helper: SearchDeploymentHelper, admin_user: MongoDBUser, user: MongoDBUser, mongot_user: MongoDBUser, mdb: MongoDB
+):
+    helper.deploy_users(
+        admin_user,
+        ADMIN_USER_PASSWORD,
+        user,
+        USER_PASSWORD,
+        mongot_user,
+        MONGOT_USER_PASSWORD,
+    )
+
+
+@mark.e2e_search_external_rs_single_mongot
+def test_create_search_resource(mdbs: MongoDBSearch):
+    mdbs.update()
+    mdbs.assert_reaches_phase(Phase.Running, timeout=300)
+
+
+@mark.e2e_search_external_rs_single_mongot
+def test_wait_for_database_resource_ready(mdb: MongoDB):
+    mdb.get_om_tester().wait_agents_ready()
+    mdb.assert_reaches_phase(Phase.Running, timeout=300)
+
+
+@mark.e2e_search_external_rs_single_mongot
+def test_wait_for_mongod_parameters(mdb: MongoDB):
+    verify_rs_mongod_parameters(mdb.namespace, mdb.name, mdb.get_members())
+
+
+@fixture(scope="function")
+def sample_movies_helper(mdb: MongoDB, namespace: str) -> SampleMoviesSearchHelper:
+    return movies_search_helper.SampleMoviesSearchHelper(
+        SearchTester.for_replicaset(mdb, USER_NAME, USER_PASSWORD, use_ssl=True, ca_path=get_issuer_ca_filepath()),
+        tools_pod=mongodb_tools_pod.get_tools_pod(namespace),
+    )
+
+
+@mark.e2e_search_external_rs_single_mongot
+def test_search_restore_sample_database(sample_movies_helper: SampleMoviesSearchHelper):
+    sample_movies_helper.restore_sample_database()
+
+
+@mark.e2e_search_external_rs_single_mongot
+def test_search_create_search_index(sample_movies_helper: SampleMoviesSearchHelper):
+    sample_movies_helper.create_search_index()
+
+
+@mark.e2e_search_external_rs_single_mongot
+def test_search_assert_search_query(sample_movies_helper: SampleMoviesSearchHelper):
+    sample_movies_helper.assert_search_query(retry_timeout=60)
+
+
+@mark.e2e_search_external_rs_single_mongot
+def test_vector_search(mdb: MongoDB):
+    search_tester = SearchTester.for_replicaset(
+        mdb, USER_NAME, USER_PASSWORD, use_ssl=True, ca_path=get_issuer_ca_filepath()
+    )
+    verify_vector_search(search_tester)
