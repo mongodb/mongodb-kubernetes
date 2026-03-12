5d5fb3ce25c0d7f4a22fda634a9fd79d8027ed94
Łukasz Sierant <lukasz.sierant@mongodb.com>
2026-03-12 20:12:39 +0100

    Search: sharded cluster operator support (#816)
    <!-- start git-machete generated -->
    
    # Based on PR #806
    
    ## Chain of upstream PRs as of 2026-03-03
    
    * PR #806:
      `master` ← `search/base`
    
      * **PR #816 (THIS ONE)**:
        `search/base` ← `search/sharded-cluster`
    
    <!-- end git-machete generated -->
    
    # Summary
    
    MCK already supported deploying single instance of mongot process using
    MongoDBSearch resource, with the MongoDB replicaset as source. That
    meant the customers can run search queries/use search against a
    ReplicaSet deployment.
    In this PR, we are adding support to have sharded cluster as source to
    the MongoDBSearch resource. So that search can be used with sharded
    clusters as well.
    
    To achieve this we have added the new field in the
    `ExternalMongoDBSource` type that can be used to configure the details
    of the sharded cluster that is going to be used as source.
    
    ```go
    type ExternalMongoDBSource struct {
    	// ShardedCluster contains configuration for external sharded MongoDB clusters.
    	// Mutually exclusive with HostAndPorts.
    	// +optional
    	ShardedCluster *ExternalShardedClusterConfig `json:"shardedCluster,omitempty"`
    ```
    
    The `ExternalShardedClusterConfig` is mainly used to generate the mongot
    config, so that mongot correctly knows how to talk to the mongod
    processes. Rest of the changes revolve around this.
    
    ## Proof of Work
    
    TBD
    
    ---------
    
    Co-authored-by: Vivek Singh <vsingh.ggits.2010@gmail.com>
    Co-authored-by: Julien-Ben <33035980+Julien-Ben@users.noreply.github.com>
    Co-authored-by: Julien Benhaim <julien.benhaim@mongodb.com>
    Co-authored-by: Vivek Singh <vivek.s@mongodb.com>

---
+++ controllers/searchcontroller/sharded_external_search_source.go
@@ -0,0 +1,125 @@
+package searchcontroller
+
+import (
+	"golang.org/x/xerrors"
+	"k8s.io/apimachinery/pkg/types"
+
+	searchv1 "github.com/mongodb/mongodb-kubernetes/api/v1/search"
+	"github.com/mongodb/mongodb-kubernetes/controllers/operator/watch"
+	"github.com/mongodb/mongodb-kubernetes/pkg/statefulset"
+)
+
+// ShardedExternalSearchSource implements ShardedSearchSourceDBResource for external sharded MongoDB clusters.
+type ShardedExternalSearchSource struct {
+	namespace string
+	spec      *searchv1.ExternalMongoDBSource
+}
+
+func NewShardedExternalSearchSource(namespace string, spec *searchv1.ExternalMongoDBSource) *ShardedExternalSearchSource {
+	return &ShardedExternalSearchSource{
+		namespace: namespace,
+		spec:      spec,
+	}
+}
+
+func (r *ShardedExternalSearchSource) Validate() error {
+	if r.spec.ShardedCluster == nil {
+		return xerrors.New("sharded configuration is required for ShardedExternalSearchSource")
+	}
+
+	if len(r.spec.ShardedCluster.Router.Hosts) == 0 {
+		return xerrors.New("router.hosts must have at least one host")
+	}
+
+	if len(r.spec.ShardedCluster.Shards) == 0 {
+		return xerrors.New("at least one shard must be configured")
+	}
+
+	seenShards := make(map[string]struct{}, len(r.spec.ShardedCluster.Shards))
+	for i, shard := range r.spec.ShardedCluster.Shards {
+		if shard.ShardName == "" {
+			return xerrors.Errorf("shard[%d].shardName is required", i)
+		}
+
+		if _, ok := seenShards[shard.ShardName]; ok {
+			return xerrors.Errorf("shardNames can not be duplicate, shard name %s is duplicate", shard.ShardName)
+		}
+		seenShards[shard.ShardName] = struct{}{}
+
+		if len(shard.Hosts) == 0 {
+			return xerrors.Errorf("shard[%d].hosts must have at least one host", i)
+		}
+	}
+
+	return nil
+}
+
+func (r *ShardedExternalSearchSource) TLSConfig() *TLSSourceConfig {
+	// Prioritize router-specific TLS if present, otherwise fall back to top-level TLS
+	var tlsConfig *searchv1.ExternalMongodTLS
+	if r.spec.ShardedCluster != nil && r.spec.ShardedCluster.Router.TLS != nil {
+		tlsConfig = r.spec.ShardedCluster.Router.TLS
+	} else if r.spec.TLS != nil {
+		tlsConfig = r.spec.TLS
+	}
+
+	if tlsConfig == nil {
+		return nil
+	}
+
+	return &TLSSourceConfig{
+		CAFileName: tlsCACertName,
+		CAVolume:   statefulset.CreateVolumeFromSecret("ca", tlsConfig.CA.Name),
+		ResourcesToWatch: map[watch.Type][]types.NamespacedName{
+			watch.Secret: {
+				{Namespace: r.namespace, Name: tlsConfig.CA.Name},
+			},
+		},
+	}
+}
+
+func (r *ShardedExternalSearchSource) KeyfileSecretName() string {
+	if r.spec.KeyFileSecretKeyRef != nil {
+		return r.spec.KeyFileSecretKeyRef.Name
+	}
+	return ""
+}
+
+func (r *ShardedExternalSearchSource) HostSeeds() []string {
+	if r.spec.ShardedCluster != nil && len(r.spec.ShardedCluster.Shards) > 0 {
+		return r.spec.ShardedCluster.Shards[0].Hosts
+	}
+	return nil
+}
+
+func (r *ShardedExternalSearchSource) GetShardCount() int {
+	if r.spec.ShardedCluster == nil {
+		return 0
+	}
+	return len(r.spec.ShardedCluster.Shards)
+}
+
+func (r *ShardedExternalSearchSource) GetShardNames() []string {
+	if r.spec.ShardedCluster == nil {
+		return nil
+	}
+	names := make([]string, len(r.spec.ShardedCluster.Shards))
+	for i, shard := range r.spec.ShardedCluster.Shards {
+		names[i] = shard.ShardName
+	}
+	return names
+}
+
+func (r *ShardedExternalSearchSource) HostSeedsForShard(shardIdx int) []string {
+	if r.spec.ShardedCluster == nil || shardIdx < 0 || shardIdx >= len(r.spec.ShardedCluster.Shards) {
+		return nil
+	}
+	return r.spec.ShardedCluster.Shards[shardIdx].Hosts
+}
+
+func (r *ShardedExternalSearchSource) MongosHostAndPort() string {
+	if r.spec.ShardedCluster == nil || len(r.spec.ShardedCluster.Router.Hosts) == 0 {
+		return ""
+	}
+	return r.spec.ShardedCluster.Router.Hosts[0]
+}
