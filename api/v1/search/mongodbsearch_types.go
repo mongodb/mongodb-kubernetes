3a338565aa9055f5ad21ebcc024fca9dc8d0ba7a
Vivek Singh <vsingh.ggits.2010@gmail.com>
2026-03-17 22:01:22 +0100

    Add support to configure mongot jvm flags using MongoDBSearch CR (#907)

--- api/v1/search/mongodbsearch_types.go
+++ api/v1/search/mongodbsearch_types.go
@@ -93,6 +93,10 @@ type MongoDBSearchSpec struct {
 	// LoadBalancer configures how mongod/mongos connect to mongot (Managed vs Unmanaged/BYO Load Balancer).
 	// +optional
 	LoadBalancer *LoadBalancerConfig `json:"lb,omitempty"`
+	// JVMFlags can be used to set the `--jvm-flags` option for the search (mongot) processes.
+	// https://www.mongodb.com/docs/manual/tutorial/mongot-sizing/advanced-guidance/hardware/#jvm-heap-sizing
+	// +optional
+	JVMFlags []string `json:"jvmFlags,omitempty"`
 }
 
 // LBMode defines the load balancer mode for Search
