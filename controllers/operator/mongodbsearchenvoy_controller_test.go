64de52e1f699bbd78e3547acc3fde8067e6d9958
Vivek Singh <vsingh.ggits.2010@gmail.com>
2026-03-23 12:13:56 +0100

    Handle `search.spec.lb.endpoint` for external source and managed LB (#922)
    # Summary
    
    Test steps:
    https://docs.google.com/document/d/1hyTQnvJQHOiKXtZZoMf8x0_h-CYsK8k9YUnC4YNeET8/edit?tab=t.hx0n5tq7mm10
    
    ## Proof of Work
    
    <!-- Enter your proof that it works here.-->
    
    ## Checklist
    
    - [ ] Have you linked a jira ticket and/or is the ticket in the title?
    - [ ] Have you checked whether your jira ticket required DOCSP changes?
    - [ ] Have you added changelog file?
        - use `skip-changelog` label if not needed
    - refer to [Changelog files and Release
    Notes](https://github.com/mongodb/mongodb-kubernetes/blob/master/CONTRIBUTING.md#changelog-files-and-release-notes)
    section in CONTRIBUTING.md for more details

--- controllers/operator/mongodbsearchenvoy_controller_test.go
+++ controllers/operator/mongodbsearchenvoy_controller_test.go
@@ -11,18 +11,109 @@ import (
 )
 
 func TestBuildReplicaSetRoute(t *testing.T) {
-	search := &searchv1.MongoDBSearch{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "mdb-search",
-			Namespace: "test-ns",
+	tests := []struct {
+		name          string
+		endpoint      string
+		expectedSNI   string
+		expectedProxy string
+	}{
+		{
+			name:          "no endpoint uses proxy service FQDN",
+			endpoint:      "",
+			expectedSNI:   "mdb-search-search-lb-svc.test-ns.svc.cluster.local",
+			expectedProxy: "mdb-search-search-lb-svc",
+		},
+		{
+			name:          "endpoint with port uses endpoint hostname",
+			endpoint:      "lb.example.com:443",
+			expectedSNI:   "lb.example.com",
+			expectedProxy: "mdb-search-search-lb-svc",
+		},
+		{
+			name:          "endpoint without port uses endpoint as-is",
+			endpoint:      "lb.example.com",
+			expectedSNI:   "lb.example.com",
+			expectedProxy: "mdb-search-search-lb-svc",
 		},
 	}
 
-	route := buildReplicaSetRoute(search)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			search := &searchv1.MongoDBSearch{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "mdb-search",
+					Namespace: "test-ns",
+				},
+			}
+			if tt.endpoint != "" {
+				search.Spec.LoadBalancer = &searchv1.LoadBalancerConfig{
+					Mode:     searchv1.LBModeManaged,
+					Endpoint: tt.endpoint,
+				}
+			}
 
-	assert.Equal(t, "rs", route.Name)
-	assert.Equal(t, "rs", route.NameSafe)
-	assert.Equal(t, "mdb-search-search-lb-svc.test-ns.svc.cluster.local", route.SNIHostname)
-	assert.Equal(t, "mdb-search-search-svc.test-ns.svc.cluster.local", route.UpstreamHost)
-	assert.Equal(t, int32(27028), route.UpstreamPort)
+			route := buildReplicaSetRoute(search)
+
+			assert.Equal(t, "rs", route.Name)
+			assert.Equal(t, "rs", route.NameSafe)
+			assert.Equal(t, tt.expectedSNI, route.SNIHostname)
+			assert.Equal(t, tt.expectedProxy, route.ProxyServiceName)
+			assert.Equal(t, "mdb-search-search-svc.test-ns.svc.cluster.local", route.UpstreamHost)
+			assert.Equal(t, int32(27028), route.UpstreamPort)
+		})
+	}
+}
+
+func TestBuildShardRoutes(t *testing.T) {
+	shardNames := []string{"mdb-sh-0", "mdb-sh-1"}
+
+	tests := []struct {
+		name         string
+		endpoint     string
+		expectedSNIs []string
+	}{
+		{
+			name:     "no endpoint uses proxy service FQDNs",
+			endpoint: "",
+			expectedSNIs: []string{
+				"mdb-search-search-0-mdb-sh-0-proxy-svc.test-ns.svc.cluster.local",
+				"mdb-search-search-0-mdb-sh-1-proxy-svc.test-ns.svc.cluster.local",
+			},
+		},
+		{
+			name:     "endpoint template resolves per shard",
+			endpoint: "mongot-{shardName}-ns.apps.example.com:443",
+			expectedSNIs: []string{
+				"mongot-mdb-sh-0-ns.apps.example.com",
+				"mongot-mdb-sh-1-ns.apps.example.com",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			search := &searchv1.MongoDBSearch{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "mdb-search",
+					Namespace: "test-ns",
+				},
+			}
+			if tt.endpoint != "" {
+				search.Spec.LoadBalancer = &searchv1.LoadBalancerConfig{
+					Mode:     searchv1.LBModeManaged,
+					Endpoint: tt.endpoint,
+				}
+			}
+
+			routes := buildShardRoutes(search, shardNames)
+
+			assert.Len(t, routes, 2)
+			for i, route := range routes {
+				assert.Equal(t, shardNames[i], route.Name)
+				assert.Equal(t, tt.expectedSNIs[i], route.SNIHostname)
+				expectedProxy := "mdb-search-search-0-" + shardNames[i] + "-proxy-svc"
+				assert.Equal(t, expectedProxy, route.ProxyServiceName)
+			}
+		})
+	}
 }
