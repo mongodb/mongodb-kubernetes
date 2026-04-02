022038bb78312535417f772cd45737c2b03073ce
Anand Singh <anand.singh@mongodb.com>
2026-04-02 16:10:45 +0200

    Extract shared e2e env variables for 10 into common file
    Move Ops Manager overrides into env_variables_e2e_common.sh, sourced by
    both public and private e2e env files.

--- docs/search/10-search-external-rs-mongod-managed-lb/env_variables_e2e_private.sh
+++ docs/search/10-search-external-rs-mongod-managed-lb/env_variables_e2e_private.sh
@@ -14,10 +14,4 @@ OPERATOR_ADDITIONAL_HELM_VALUES="$(get_operator_helm_values | tr ' ' ',')"
 export OPERATOR_ADDITIONAL_HELM_VALUES
 export OPERATOR_HELM_CHART="${PROJECT_DIR}/helm_chart"
 
-# we need project name with a timestamp (NAMESPACE in evg is randomized) to allow for cloud-qa cleanups
-# NOTE: This snippet uses MDB_EXTERNAL_CLUSTER_NAME instead of MDB_RESOURCE_NAME
-export OPS_MANAGER_PROJECT_NAME="${NAMESPACE}-${MDB_EXTERNAL_CLUSTER_NAME}"
-export OPS_MANAGER_API_URL="${OM_BASE_URL}"
-export OPS_MANAGER_API_USER="${OM_USER}"
-export OPS_MANAGER_API_KEY="${OM_API_KEY}"
-export OPS_MANAGER_ORG_ID="${OM_ORGID}"
+source "$(dirname "${BASH_SOURCE[0]}")/env_variables_e2e_common.sh"
