49f9d98ac07f15abf736f797001d197c72467c20
filipcirtog <filip.cirtog@mongodb.com>
2026-04-15 16:24:10 +0200

    lint

--- docker/mongodb-kubernetes-tests/tests/authentication/replica_set_scram_sha_1_connectivity.py
+++ docker/mongodb-kubernetes-tests/tests/authentication/replica_set_scram_sha_1_connectivity.py
@@ -1,13 +1,6 @@
 from kubetester.mongotester import ReplicaSetTester
 from pytest import fixture, mark
-<<<<<<< HEAD
 from tests.authentication.sha1_connectivity_tests import SHA1ConnectivityTests
-=======
-from tests.authentication.sha1_connectivity_tests import (
-    SHA1ConnectivityTests,
-    run_authentication_disabled_after_resource_deleted,
-)
->>>>>>> 75aca20a3 (lint)
 
 
 @mark.e2e_replica_set_scram_sha_1_user_connectivity
@@ -23,13 +16,3 @@ class TestReplicaSetSHA1Connectivity(SHA1ConnectivityTests):
     @fixture
     def mongo_tester(self, mdb_resource_name: str):
         return ReplicaSetTester(mdb_resource_name, 3)
-
-    def test_ops_manager_state_correctly_updated(self):
-        tester = AutomationConfigTester(KubernetesTester.get_automation_config())
-        tester.assert_authentication_mechanism_enabled("MONGODB-CR")
-        tester.assert_authoritative_set(True)
-        tester.assert_authentication_enabled(3)
-        tester.assert_expected_users(0)
-
-    def test_authentication_is_disabled_once_resource_is_deleted(self, mdb: MongoDB):
-        run_authentication_disabled_after_resource_deleted(mdb)
