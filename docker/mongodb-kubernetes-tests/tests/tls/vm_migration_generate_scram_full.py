454c4de07b703069160210ce2cbc149d711cdfde
filipcirtog <filip.cirtog@mongodb.com>
2026-04-17 11:50:03 +0200

    fix: test

--- docker/mongodb-kubernetes-tests/tests/tls/vm_migration_generate_scram_full.py
+++ docker/mongodb-kubernetes-tests/tests/tls/vm_migration_generate_scram_full.py
@@ -87,6 +87,7 @@ def _configure_ac(namespace: str, om_tester: OMTester, vm_sts: dict, vm_service:
                 "user": "app-user",
                 "db": "admin",
                 "roles": [
+                    {"role": "readWrite", "db": "admin"},
                     {"role": "readWrite", "db": "myapp"},
                     {"role": "read", "db": "reporting"},
                 ],
