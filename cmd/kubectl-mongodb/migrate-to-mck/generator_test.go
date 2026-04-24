ca291fe25d0b917a103dbf05ab0d8e1abad52b99
filipcirtog <filip.cirtog@mongodb.com>
2026-04-24 10:50:56 +0200

    fix: remove multicluster code

--- cmd/kubectl-mongodb/migrate-to-mck/generator_test.go
+++ cmd/kubectl-mongodb/migrate-to-mck/generator_test.go
@@ -1,7 +1,6 @@
 package migratetomck
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -60,28 +59,24 @@ func TestGenerateMongoDBCR_CustomResourceName(t *testing.T) {
 	assert.Contains(t, yamlOutput, "replicaSetNameOverride: my-rs")
 }
 
-func TestGenerateMongoDBCR_MultiCluster_CustomResourceName(t *testing.T) {
-	members := make([]any, 5)
-	processes := make([]any, 5)
-	for i := 0; i < 5; i++ {
-		name := fmt.Sprintf("geo-rs-%d", i)
-		members[i] = map[string]any{"_id": i, "host": name, "priority": 1, "votes": 1}
-		processes[i] = map[string]any{
-			"name":                        name,
-			"hostname":                    fmt.Sprintf("mongo-%d.example.com", i),
-			"version":                     "8.0.4-ent",
-			"featureCompatibilityVersion": "8.0",
-			"processType":                 "mongod",
-			"args2_6": map[string]any{
-				"net":         map[string]any{"port": 27017},
-				"replication": map[string]any{"replSetName": "geo-rs"},
-			},
-		}
-	}
+func TestGenerateMongoDBCR_MultiCluster_NotSupported(t *testing.T) {
 	ac := om.NewAutomationConfig(om.Deployment{
-		"processes":   processes,
-		"replicaSets": []any{map[string]any{"_id": "geo-rs", "members": members}},
-		"sharding":    []any{},
+		"processes": []any{
+			map[string]any{
+				"name": "geo-rs-0", "hostname": "mongo-0.example.com",
+				"version": "8.0.4-ent", "featureCompatibilityVersion": "8.0",
+				"processType": "mongod",
+				"args2_6": map[string]any{
+					"net":         map[string]any{"port": 27017},
+					"replication": map[string]any{"replSetName": "geo-rs"},
+				},
+			},
+		},
+		"replicaSets": []any{map[string]any{
+			"_id":     "geo-rs",
+			"members": []any{map[string]any{"_id": 0, "host": "geo-rs-0", "priority": 1, "votes": 1}},
+		}},
+		"sharding": []any{},
 	})
 
 	opts := withDeploymentData(ac, GenerateOptions{
@@ -91,13 +86,9 @@ func TestGenerateMongoDBCR_MultiCluster_CustomResourceName(t *testing.T) {
 		MultiClusterNames:     []string{"east1", "west1"},
 	})
 
-	obj, resourceName, err := GenerateMongoDBCR(ac, opts)
-	require.NoError(t, err)
-	assert.Equal(t, "custom-mc-name", resourceName)
-	yamlOutput, err := marshalCRToYAML(obj)
-	require.NoError(t, err)
-	assert.Contains(t, yamlOutput, "name: custom-mc-name")
-	assert.Contains(t, yamlOutput, "replicaSetNameOverride: geo-rs")
+	_, _, err := GenerateMongoDBCR(ac, opts)
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "not yet supported")
 }
 
 func TestGenerateMongoDBCR_AutoNormalizesRSName(t *testing.T) {
