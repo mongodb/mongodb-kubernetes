2ada43b2ca865f38e31e4e7152fc0fdaa1a04263
filipcirtog <filip.cirtog@mongodb.com>
2026-04-24 11:02:19 +0200

    fix: remove multicluster code

--- cmd/kubectl-mongodb/migrate-to-mck/generator_test.go
+++ cmd/kubectl-mongodb/migrate-to-mck/generator_test.go
@@ -59,37 +59,6 @@ func TestGenerateMongoDBCR_CustomResourceName(t *testing.T) {
 	assert.Contains(t, yamlOutput, "replicaSetNameOverride: my-rs")
 }
 
-func TestGenerateMongoDBCR_MultiCluster_NotSupported(t *testing.T) {
-	ac := om.NewAutomationConfig(om.Deployment{
-		"processes": []any{
-			map[string]any{
-				"name": "geo-rs-0", "hostname": "mongo-0.example.com",
-				"version": "8.0.4-ent", "featureCompatibilityVersion": "8.0",
-				"processType": "mongod",
-				"args2_6": map[string]any{
-					"net":         map[string]any{"port": 27017},
-					"replication": map[string]any{"replSetName": "geo-rs"},
-				},
-			},
-		},
-		"replicaSets": []any{map[string]any{
-			"_id":     "geo-rs",
-			"members": []any{map[string]any{"_id": 0, "host": "geo-rs-0", "priority": 1, "votes": 1}},
-		}},
-		"sharding": []any{},
-	})
-
-	opts := withDeploymentData(ac, GenerateOptions{
-		ResourceNameOverride:  "custom-mc-name",
-		CredentialsSecretName: "mc-credentials",
-		ConfigMapName:         "mc-om-config",
-		MultiClusterNames:     []string{"east1", "west1"},
-	})
-
-	_, _, err := GenerateMongoDBCR(ac, opts)
-	require.Error(t, err)
-	assert.Contains(t, err.Error(), "not yet supported")
-}
 
 func TestGenerateMongoDBCR_AutoNormalizesRSName(t *testing.T) {
 	ac := om.NewAutomationConfig(om.Deployment{
