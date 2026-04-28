e5db287d11c8f0cd1ecfd30fa96d5b196e7b6f67
Łukasz Sierant <lukasz.sierant@mongodb.com>
2026-04-28 08:51:10 +0200

    Use ty for statically check python types (#897)
    # Summary
    
    Expand static Python type checking in `docker/mongodb-kubernetes-tests/`
    from the search tests to every `tests/` file. Before this PR the `ty`
    pre-commit hook only ran against `tests/search/` and
    `tests/common/search/`; everything else was unchecked.
    
    ## Proof of Work
    
    Green [evg](https://spruce.mongodb.com/version/69e9a71e8b720100074d7b76)

--- docker/mongodb-kubernetes-tests/kubetester/mongodb_user.py
+++ docker/mongodb-kubernetes-tests/kubetester/mongodb_user.py
@@ -16,6 +16,8 @@ TRACER = trace.get_tracer("evergreen-agent")
 
 
 class MongoDBUser(CustomObject, MongoDBCommon):
+    _password: Optional[str] = None
+
     def __init__(self, *args, **kwargs):
         with_defaults = {
             "plural": "mongodbusers",
@@ -80,6 +82,7 @@ class MongoDBUser(CustomObject, MongoDBCommon):
     def add_role(self, role: Role) -> MongoDBUser:
         self["spec"]["roles"] = self["spec"].get("roles", [])
         self["spec"]["roles"].append({"db": role.db, "name": role.role})
+        return self
 
     def add_roles(self, roles: List[Role]):
         for role in roles:
