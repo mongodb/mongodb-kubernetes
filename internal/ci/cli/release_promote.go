364cb97636ab2eb6137060b3558b0a68c78d05dd
Jose Vazquez <jose.vazquez@mongodb.com>
2026-05-05 17:22:51 +0200

    Add mckctl release promote
    Signed-off-by: Jose Vazquez <jose.vazquez@mongodb.com>

---
+++ internal/ci/cli/release_promote.go
@@ -0,0 +1,59 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/mongodb/mongodb-kubernetes/internal/ci/release"
+)
+
+func newReleasePromoteCmd() *cobra.Command {
+	var (
+		image       string
+		commit      string
+		version     string
+		registryURL string
+		repo        string
+		dryRun      bool
+	)
+
+	cmd := &cobra.Command{
+		Use:   "promote",
+		Short: "Promote a candidate image by applying promoted-latest and promoted-{commit}-{version} tags",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			promoter := release.NewOCIPromoter(registryURL, repo)
+			tags, err := release.Promote(release.PromoteInputs{
+				Image:   image,
+				Commit:  commit,
+				Version: version,
+				DryRun:  dryRun,
+			}, promoter)
+			if err != nil {
+				return err
+			}
+			for _, tag := range tags {
+				if dryRun {
+					fmt.Fprintf(cmd.OutOrStdout(), "dry-run: would copy %s → %s/%s:%s\n", image, registryURL, repo, tag)
+				} else {
+					fmt.Fprintf(cmd.OutOrStdout(), "promoted: %s/%s:%s\n", registryURL, repo, tag)
+				}
+			}
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVar(&image, "image", "", "source image reference to promote (required)")
+	cmd.Flags().StringVar(&commit, "commit", "", "commit SHA to encode in the promoted tag (required)")
+	cmd.Flags().StringVar(&version, "version", "", "version to encode in the promoted tag (required)")
+	cmd.Flags().StringVar(&registryURL, "registry", "https://quay.io", "target OCI registry base URL")
+	cmd.Flags().StringVar(&repo, "repo", "mongodb/mongodb-kubernetes-operator", "target image repository")
+
+	cmd.Flags().BoolVar(&dryRun, "dry-run", false, "print what would happen without copying any images")
+
+	_ = cmd.MarkFlagRequired("image")
+	_ = cmd.MarkFlagRequired("commit")
+	_ = cmd.MarkFlagRequired("version")
+
+	return cmd
+}
