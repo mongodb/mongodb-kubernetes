d22ff1e09b1df8390944a6055e1524a2f8cdc82c
Maciej Karaś <maciej.karas@mongodb.com>
2026-06-08 12:03:13 +0200

    revert helm.py changes

--- docker/mongodb-kubernetes-tests/kubetester/helm.py
+++ docker/mongodb-kubernetes-tests/kubetester/helm.py
@@ -36,8 +36,7 @@ def helm_template(
     args = ("helm", "template", *command_args, helm_chart_path)
     logger.info(" ".join(args))
 
-    home = os.getenv("HOME")
-    yaml_file_name = os.path.join(home, "{}.yaml".format(str(uuid.uuid4())))
+    yaml_file_name = "{}.yaml".format(str(uuid.uuid4()))
     with open(yaml_file_name, "w") as output:
         process_run_and_check(" ".join(args), stdout=output, check=True, shell=True)
 
