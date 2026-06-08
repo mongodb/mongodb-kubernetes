a0baa4c768fa71a3145660570d465b5921c804a9
Maciej Karaś <maciej.karas@mongodb.com>
2026-06-08 20:49:38 +0200

    nit: remove comment from database_construction.go

--- controllers/operator/construct/database_construction.go
+++ controllers/operator/construct/database_construction.go
@@ -1111,7 +1111,6 @@ func GetNonPersistentAgentVolumeMounts(volumes []corev1.Volume, volumeMounts []c
 
 	volumeMounts = append(volumeMounts, statefulset.CreateVolumeMount(util.PvMms, util.PvcMountPathTmp, statefulset.WithSubPath(util.PvcNameTmp)))
 
-	// Agent cache directory — must be writable even with readOnlyRootFilesystem: true
 	volumeMounts = append(volumeMounts, statefulset.CreateVolumeMount(util.PvMms, util.PvcMountPathAgentCache, statefulset.WithSubPath(util.PvcAgentCache)))
 	return volumes, volumeMounts
 }
