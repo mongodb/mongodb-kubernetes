6296d3a93fa6d86ddf76a129b443dc60517c6120
filipcirtog <filip.cirtog@mongodb.com>
2026-06-12 17:58:17 +0200

    KUBE-116: add unit tests for EncodeUserinfoComponent and GetLoginString

--- pkg/util/stringutil/stringutil_test.go
+++ pkg/util/stringutil/stringutil_test.go
@@ -6,6 +6,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestEncodeUserinfoComponent(t *testing.T) {
+	cases := []struct{ in, want string }{
+		{"pass word", "pass%20word"},
+		{"pass+word", "pass%2Bword"},
+		{"p@ss", "p%40ss"},
+		{"p%25x", "p%2525x"},
+		{"my:p@ss/w?rd# %[+]!$&'()*,;=~-._", "my%3Ap%40ss%2Fw%3Frd%23%20%25%5B%2B%5D%21%24%26%27%28%29%2A%2C%3B%3D~-._"},
+		{"plain", "plain"},
+	}
+	for _, c := range cases {
+		assert.Equal(t, c.want, EncodeUserinfoComponent(c.in), c.in)
+	}
+}
+
 func TestContainsAny(t *testing.T) {
 	assert.True(t, ContainsAny([]string{"one", "two"}, "one"))
 	assert.True(t, ContainsAny([]string{"one", "two"}, "two"))
