d6f1dffcb495eb993dd15e8ff8d63349796c41f0
Vivek Singh <vsingh.ggits.2010@gmail.com>
2026-06-23 15:57:53 +0200

    [KUBE-127] Add support for mTLS with enc keyfile for scram auth and E2E (#1240)
    # Summary
    
    When mongot connects to its sync source over TLS while authenticating
    with SCRAM (username/password), it used to connect as a plain TLS client
    which means it didn't present a client certificate during the handshake.
    This is fine when the source mongod accepts any TLS client, but it
    breaks when the source is configured to require mutual TLS (i.e. the
    mongod demands that every connecting client present a certificate signed
    by a trusted CA). In that setup mongot's SCRAM connection is rejected at
    the TLS layer, before SCRAM authentication even gets a chance to run.
    
    It is important to understand that authentication and transport are two
    separate concerns here. SCRAM (username+password) is still the
    authentication mechanism, but mTLS is a transport-level requirement
    (mongot may present a client cert just to establish the connection). So
    we need a way for a SCRAM-authenticated mongot to also present a client
    certificate during the TLS handshake.
    
    This PR adds that. Users can now point mongot at a client certificate
    for the SCRAM connection via a new field on the source spec:
    
    ```
      spec:
        source:
          username: <scram-user>
          passwordSecretRef:
            name: <password-secret>
          tls:
            clientCertificateSecretRef:
              name: <client-cert-secret>   # tls.crt, tls.key (+ optional tls.keyFilePassword)
    ```
    
    When `spec.source.tls.clientCertificateSecretRef` is set, the operator
    wires the certificate into mongot's `scramAuth.tls` block so mongot
    presents it during the handshake while still authenticating via SCRAM:
    
    ```
      syncSource:
        replicaSet:
          scramAuth:
            username: <scram-user>
            passwordFile: <path>
            tls:
              enabled: true
              caFile: /var/lib/tls/ca/ca-pem
              tlsCertificateKeyFile: /var/lib/tls/scram-client/<hash>.pem
              tlsCertificateKeyFilePasswordFile: /tmp/scram-key-password   # only if encrypted
    ```
    
    Two more things to note about the behavior:
    
    1. Just like the gRPC keyfile change in #1237, the client certificate's
    private key can be password-encrypted. If the user adds a
    `tls.keyFilePassword` key to the client cert secret, the operator mounts
    it and sets `tlsCertificateKeyFilePasswordFile` so mongot can decrypt
    the key.
    2. For sharded clusters the same client cert and CA are also applied to
    the router (mongos) connection, so mongot presents the certificate
    consistently whether it is talking to a shard's replica set or to
    mongos.
    
    
    ## Proof of Work
    
    E2E is part of the
    [PR](https://github.com/mongodb/mongodb-kubernetes/pull/1248).
    
    ## Checklist
    
    - [x] Have you linked a jira ticket and/or is the ticket in the title?
    - [x] Have you checked whether your jira ticket required DOCSP changes?
    - [x] Have you added changelog file?
        - use `skip-changelog` label if not needed
    - refer to [Changelog files and Release
    Notes](https://github.com/mongodb/mongodb-kubernetes/blob/master/CONTRIBUTING.md#changelog-files-and-release-notes)
    section in CONTRIBUTING.md for more details

--- docker/mongodb-kubernetes-tests/tests/search/search_enterprise_enc_mtls.py
+++ docker/mongodb-kubernetes-tests/tests/search/search_enterprise_enc_mtls.py
@@ -1,5 +1,5 @@
 from kubetester import get_service, try_load
-from kubetester.certs import create_mongodb_tls_certs, create_tls_certs
+from kubetester.certs import create_mongodb_tls_certs, create_tls_certs, generate_cert
 from kubetester.kubetester import fixture as yaml_fixture
 from kubetester.mongodb import MongoDB
 from kubetester.mongodb_search import MongoDBSearch
@@ -36,6 +36,10 @@ MDBS_TLS_SECRET_NAME = search_resource_names.mongot_tls_cert_name(MDB_RESOURCE_N
 # Password used to encrypt the gRPC server certificate private key
 GRPC_KEY_PASSWORD = "test-grpc-key-password"
 
+# SCRAM mTLS client cert for mongot -> mongod connection
+SCRAM_CLIENT_CERT_SECRET_NAME = f"{MDB_RESOURCE_NAME}-scram-tls-user"
+SCRAM_KEY_PASSWORD = "test-scram-key-password"
+
 
 @fixture(scope="function")
 def helper(namespace: str) -> SearchDeploymentHelper:
@@ -67,6 +71,7 @@ def mdbs(namespace: str) -> MongoDBSearch:
         resource["spec"] = {}
     resource["spec"]["security"] = {"tls": {"certificateKeySecretRef": {"name": MDBS_TLS_SECRET_NAME}}}
     resource["spec"]["source"] = {"passwordSecretRef": {"name": f"{resource.name}-{MONGOT_USER_NAME}-password"}}
+    resource["spec"]["source"]["tls"] = {"clientCertificateSecretRef": {"name": SCRAM_CLIENT_CERT_SECRET_NAME}}
     try_load(resource)
     return resource
 
@@ -86,13 +91,13 @@ def mongot_user(helper: SearchDeploymentHelper, mdbs: MongoDBSearch) -> MongoDBU
     return helper.mongot_user_resource(mdbs.name, MONGOT_USER_NAME)
 
 
-@mark.e2e_search_enterprise_tls
+@mark.e2e_search_enterprise_enc_mtls
 def test_install_operator(namespace: str, operator_installation_config: dict[str, str]):
     operator = get_default_operator(namespace, operator_installation_config=operator_installation_config)
     operator.assert_is_running()
 
 
-@mark.e2e_search_enterprise_tls
+@mark.e2e_search_enterprise_enc_mtls
 @skip_if_cloud_manager
 def test_create_ops_manager(namespace: str):
     ops_manager = get_ops_manager(namespace)
@@ -102,7 +107,7 @@ def test_create_ops_manager(namespace: str):
     ops_manager.appdb_status().assert_reaches_phase(Phase.Running, timeout=600)
 
 
-@mark.e2e_search_enterprise_tls
+@mark.e2e_search_enterprise_enc_mtls
 def test_install_tls_secrets_and_configmaps(namespace: str, mdb: MongoDB, mdbs: MongoDBSearch, issuer: str):
     create_mongodb_tls_certs(issuer, namespace, mdb.name, f"certs-{mdb.name}-cert", mdb.get_members())
 
@@ -125,14 +130,28 @@ def test_install_tls_secrets_and_configmaps(namespace: str, mdb: MongoDB, mdbs: 
     # that mongot can handle password-protected keys via tls.keyFilePassword
     encrypt_tls_key_with_password(namespace, MDBS_TLS_SECRET_NAME, GRPC_KEY_PASSWORD)
 
+    # SCRAM client cert for mongot mTLS to mongod
+    generate_cert(
+        namespace=namespace,
+        pod="",
+        dns="",
+        issuer=issuer,
+        spec={
+            "usages": ["digital signature", "key encipherment", "client auth"],
+            "dnsNames": ["scram-client"],
+        },
+        secret_name=SCRAM_CLIENT_CERT_SECRET_NAME,
+    )
+    encrypt_tls_key_with_password(namespace, SCRAM_CLIENT_CERT_SECRET_NAME, SCRAM_KEY_PASSWORD)
 
-@mark.e2e_search_enterprise_tls
+
+@mark.e2e_search_enterprise_enc_mtls
 def test_create_database_resource(mdb: MongoDB):
     mdb.update()
     mdb.assert_reaches_phase(Phase.Running, timeout=300)
 
 
-@mark.e2e_search_enterprise_tls
+@mark.e2e_search_enterprise_enc_mtls
 def test_create_users(
     helper: SearchDeploymentHelper, admin_user: MongoDBUser, user: MongoDBUser, mongot_user: MongoDBUser, mdb: MongoDB
 ):
@@ -146,7 +165,7 @@ def test_create_users(
     )
 
 
-@mark.e2e_search_enterprise_tls
+@mark.e2e_search_enterprise_enc_mtls
 def test_create_search_resource(mdbs: MongoDBSearch):
     mdbs.update()
     mdbs.assert_reaches_phase(Phase.Running, timeout=300)
@@ -155,47 +174,47 @@ def test_create_search_resource(mdbs: MongoDBSearch):
 # After picking up MongoDBSearch CR, MongoDB reconciler will add mongod parameters to each process.
 # Due to how MongoDB reconciler works (blocking on waiting for agents and not changing the status to pending)
 # the phase won't be updated to Pending and we need to wait by checking agents' status directly in OM.
-@mark.e2e_search_enterprise_tls
+@mark.e2e_search_enterprise_enc_mtls
 def test_wait_for_agents_ready(mdb: MongoDB):
     mdb.get_om_tester().wait_agents_ready()
     mdb.assert_reaches_phase(Phase.Running, timeout=300)
 
 
-@mark.e2e_search_enterprise_tls
+@mark.e2e_search_enterprise_enc_mtls
 def test_wait_for_mongod_parameters(mdb: MongoDB):
     expected_host = search_resource_names.mongot_pod_fqdn(mdb.name, mdb.namespace, 27028)
     verify_rs_mongod_parameters(mdb.namespace, mdb.name, mdb.get_members(), expected_host)
 
 
-@mark.e2e_search_enterprise_tls
+@mark.e2e_search_enterprise_enc_mtls
 # def test_search_verify_prometheus_disabled_initially(namespace: str, mdbs: MongoDBSearch):
 #     tools_pod = mongodb_tools_pod.get_tools_pod(namespace)
 #     assert_search_service_prometheus_port(mdbs, should_exist=False)
 #     assert_search_pod_prometheus_endpoint(mdbs, tools_pod, should_be_accessible=False)
 
 
-@mark.e2e_search_enterprise_tls
+@mark.e2e_search_enterprise_enc_mtls
 def test_search_enable_prometheus_on_default_port(mdbs: MongoDBSearch):
     mdbs["spec"]["prometheus"] = {}
     mdbs.update()
     mdbs.assert_reaches_phase(Phase.Running, timeout=300)
 
 
-@mark.e2e_search_enterprise_tls
+@mark.e2e_search_enterprise_enc_mtls
 def test_search_verify_prometheus_enabled(namespace: str, mdbs: MongoDBSearch):
     tools_pod = mongodb_tools_pod.get_tools_pod(namespace)
     assert_search_service_prometheus_port(mdbs, should_exist=True, expected_port=9946)
     assert_search_pod_prometheus_endpoint(mdbs, tools_pod, should_be_accessible=True, port=9946)
 
 
-@mark.e2e_search_enterprise_tls
+@mark.e2e_search_enterprise_enc_mtls
 def test_search_change_prometheus_to_custom_port(mdbs: MongoDBSearch):
     mdbs["spec"]["prometheus"] = {"port": 10000}
     mdbs.update()
     mdbs.assert_reaches_phase(Phase.Running, timeout=300)
 
 
-@mark.e2e_search_enterprise_tls
+@mark.e2e_search_enterprise_enc_mtls
 def test_search_verify_prometheus_enabled_on_custom_port(namespace: str, mdbs: MongoDBSearch):
     tools_pod = mongodb_tools_pod.get_tools_pod(namespace)
     assert_search_service_prometheus_port(mdbs, should_exist=True, expected_port=10000)
@@ -212,17 +231,17 @@ def sample_movies_helper(mdb: MongoDB, namespace: str) -> movies_search_helper.S
     )
 
 
-@mark.e2e_search_enterprise_tls
+@mark.e2e_search_enterprise_enc_mtls
 def test_search_restore_sample_database(sample_movies_helper: movies_search_helper.SampleMoviesSearchHelper):
     sample_movies_helper.restore_sample_database()
 
 
-@mark.e2e_search_enterprise_tls
+@mark.e2e_search_enterprise_enc_mtls
 def test_search_create_search_index(sample_movies_helper: movies_search_helper.SampleMoviesSearchHelper):
     sample_movies_helper.create_search_index()
 
 
-@mark.e2e_search_enterprise_tls
+@mark.e2e_search_enterprise_enc_mtls
 def test_search_assert_search_query(sample_movies_helper: movies_search_helper.SampleMoviesSearchHelper):
     sample_movies_helper.assert_search_query(retry_timeout=60)
 
