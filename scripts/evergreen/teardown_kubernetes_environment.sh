5485a82ac175b2bc5762660ac8e11f84c86749b6
Nam Nguyen <nam.nguyen@mongodb.com>
2026-06-26 16:07:56 +0200

    fix(e2e): enhance docker cleanup commands in environment teardown scripts

--- scripts/evergreen/teardown_kubernetes_environment.sh
+++ scripts/evergreen/teardown_kubernetes_environment.sh
@@ -4,13 +4,10 @@ set -Eeou pipefail
 
 source scripts/dev/set_env_context.sh
 
-if [ "${KUBE_ENVIRONMENT_NAME}" = "kind" ]; then
-    docker system prune -a -f
-    echo "Deleting Kind cluster"
-    kind delete clusters --all
-fi
-
 if [ "${KUBE_ENVIRONMENT_NAME}" = "minikube" ]; then
     echo "Deleting minikube cluster"
     "${PROJECT_DIR:-.}/bin/minikube" delete
+else
+    kind delete clusters --all 2>/dev/null || true
+    docker system prune --all --volumes --force
 fi
