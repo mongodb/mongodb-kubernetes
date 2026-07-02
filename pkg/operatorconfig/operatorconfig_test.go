c951606f27a88cbc8f9562965884697cd1e63c64
Mikalai Radchuk <509198+m1kola@users.noreply.github.com>
2026-07-02 09:27:08 +0200

    CLOUDP-411215: Move watchedResources setting to OperatorConfig (#1310)
    Replace the -watch-resource operator flag and operator.watchedResources
    Helm value with OperatorConfig.spec.watchedResources, defaulting to all
    known CRDs. Member-cluster discovery now tolerates a missing member-list
    ConfigMap so single-cluster operators run even with mongodbmulticluster
    watched.

--- pkg/operatorconfig/operatorconfig_test.go
+++ pkg/operatorconfig/operatorconfig_test.go
@@ -34,6 +34,7 @@ func TestLoad_AbsentCR(t *testing.T) {
 	assert.Empty(t, cfg.ResourceVersion)
 	assert.Equal(t, operatorv1.ArchitectureNonStatic, cfg.Spec.DefaultArchitecture)
 	assert.Equal(t, 1, cfg.Spec.MaxConcurrentReconciles)
+	assert.Equal(t, operatorv1.AllWatchedResources, cfg.Spec.WatchedResources)
 }
 
 func TestLoad_PresentCR(t *testing.T) {
@@ -54,7 +55,31 @@ func TestLoad_PresentCR(t *testing.T) {
 	assert.Equal(t, 4, cfg.Spec.MaxConcurrentReconciles)
 	// DefaultArchitecture was omitted in the CR; withDefaults fills it in
 	assert.Equal(t, operatorv1.ArchitectureNonStatic, cfg.Spec.DefaultArchitecture)
+	// WatchedResources was omitted in the CR; withDefaults fills in all known CRDs
+	assert.Equal(t, operatorv1.AllWatchedResources, cfg.Spec.WatchedResources)
 	assert.NotEmpty(t, cfg.ResourceVersion)
+}
+
+func TestLoad_WatchedResourcesSubsetPreserved(t *testing.T) {
+	subset := []operatorv1.WatchedResource{
+		operatorv1.WatchedResourceMongoDB,
+		operatorv1.WatchedResourceMongoDBUsers,
+	}
+	cr := &operatorv1.OperatorConfig{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      util.DefaultOperatorConfigName,
+			Namespace: testNamespace,
+		},
+		Spec: operatorv1.OperatorConfigSpec{
+			WatchedResources: subset,
+		},
+	}
+	c := fake.NewClientBuilder().WithScheme(testScheme()).WithObjects(cr).Build()
+
+	cfg, err := Load(context.Background(), c, testNamespace, util.DefaultOperatorConfigName)
+
+	require.NoError(t, err)
+	assert.Equal(t, subset, cfg.Spec.WatchedResources)
 }
 
 func TestLoad_ClientError(t *testing.T) {
