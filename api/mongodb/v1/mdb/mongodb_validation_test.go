d361d3cb6981b9845d49cc0296adef7a7474f315
Lucian Tosa <lucian.tosa@mongodb.com>
2026-08-13 16:07:06 +0200

    linter

--- api/mongodb/v1/mdb/mongodb_validation_test.go
+++ api/mongodb/v1/mdb/mongodb_validation_test.go
@@ -9,6 +9,7 @@ import (
 	"k8s.io/utils/ptr"
 
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	v1 "github.com/mongodb/mongodb-kubernetes/api/mongodb/v1"
 	"github.com/mongodb/mongodb-kubernetes/api/mongodb/v1/status"
