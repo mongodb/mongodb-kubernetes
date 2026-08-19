5f44eb1aa3edda9daae1ff0820f720e4a45f30e4
Jose Vazquez <jose.vazquez@mongodb.com>
2026-08-19 10:29:01 +0200

    Simplify branch defaulting

--- scripts/dev/regenerate_multicluster_rbac.sh
+++ scripts/dev/regenerate_multicluster_rbac.sh
@@ -7,8 +7,9 @@ set -Eeou pipefail
 
 source scripts/dev/set_env_context.sh
 source scripts/funcs/printing
+BRANCH="${branch_name:-master}"
 
-git_last_changed=$(git ls-tree -r "origin/${branch_name:-master}" --name-only)
+git_last_changed=$(git ls-tree -r "origin/${BRANCH}" --name-only)
 
 if echo "${git_last_changed}" | grep -q -e 'cmd/kubectl-mongodb' -e 'pkg/kubectl-mongodb'; then
   echo 'regenerating multicluster RBAC public example'
