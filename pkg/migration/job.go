c4c61a26255a99d15c35f2ecb9333791aec38003
Lucian Tosa <lucian.tosa@mongodb.com>
2026-08-29 11:32:50 +0200

    fix linter

--- pkg/migration/job.go
+++ pkg/migration/job.go
@@ -12,7 +12,6 @@ import (
 	mdbv1 "github.com/mongodb/mongodb-kubernetes/api/mongodb/v1/mdb"
 	"github.com/mongodb/mongodb-kubernetes/controllers/operator/authentication"
 	"github.com/mongodb/mongodb-kubernetes/pkg/kube"
-	"github.com/mongodb/mongodb-kubernetes/pkg/tls"
 	"github.com/mongodb/mongodb-kubernetes/pkg/kube/container"
 	"github.com/mongodb/mongodb-kubernetes/pkg/kube/podtemplatespec"
 	"github.com/mongodb/mongodb-kubernetes/pkg/tls"
