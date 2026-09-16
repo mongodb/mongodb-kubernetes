18745291c79c20b615b49ffe279b8d07c9aceed0
Jose Vazquez <jose.vazquez@mongodb.com>
2026-09-16 11:27:29 +0200

    Fix dockerfile ldflags

--- scripts/release/atomic_pipeline.py
+++ scripts/release/atomic_pipeline.py
@@ -132,11 +132,13 @@ def build_operator_image(build_configuration: ImageBuildConfiguration, with_race
     # repository with a given suffix.
     test_suffix = os.getenv("test_suffix", "")
     log_automation_config_diff = os.getenv("LOG_AUTOMATION_CONFIG_DIFF", "false")
+    ldflag_version = os.getenv("RELEASE_VERSION", build_configuration.version)
 
     build_configuration.version = f"{build_configuration.version}{'-race' if with_race_detection else ''}"
 
     args = {
         "version": build_configuration.version,
+        "ldflag_version": ldflag_version,
         "log_automation_config_diff": log_automation_config_diff,
         "test_suffix": test_suffix,
         "use_race": "true" if with_race_detection else "false",
